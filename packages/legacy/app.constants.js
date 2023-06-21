6e802e569b8ffe9482a288ff828e41c79ea7708c
Zylks <tzylks@deloitte.com>
2023-06-21 12:47:52 -0700

    Include Breadcrumbs

--- packages/legacy/app.constants.js
+++ packages/legacy/app.constants.js
@@ -19,7 +19,7 @@ angular
                 pathSplit = [],
                 returnObj;
 
-            mod = "MonolithDev";
+            mod = 'Monolith_Dev';
             protocol = window.location.protocol;
             host = window.location.hostname;
             port = window.location.port;
