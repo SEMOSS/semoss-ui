fac926db42ff4a0f5c57a99a8e50a94466926565
Zylks <tzylks@deloitte.com>
2023-06-27 13:42:29 -0700

    Update LinearProgress props

--- packages/ui/src/components/LinearProgress/LinearProgress.tsx
+++ packages/ui/src/components/LinearProgress/LinearProgress.tsx
@@ -3,8 +3,44 @@ import { SxProps } from "@mui/system";
 import { LinearProgressProps as MuiLinearProgressProps } from "@mui/material";
 
 export interface LinearProgressProps extends MuiLinearProgressProps {
-    /** custom style object */
+    /**
+     * The color of the component.
+     * It supports both default and custom theme colors, which can be added as shown in the
+     * [palette customization guide](https://mui.com/material-ui/customization/palette/#adding-new-colors).
+     * @default 'primary'
+     */
+    color?:
+        | "primary"
+        | "secondary"
+        | "error"
+        | "info"
+        | "success"
+        | "warning"
+        | "inherit";
+
+    /**
+     * The system prop that allows defining system overrides as well as additional CSS styles.
+     */
     sx?: SxProps;
+
+    /**
+     * The value of the progress indicator for the determinate and buffer variants.
+     * Value between 0 and 100.
+     */
+    value?: number;
+
+    /**
+     * The value for the buffer variant.
+     * Value between 0 and 100.
+     */
+    valueBuffer?: number;
+
+    /**
+     * The variant to use.
+     * Use indeterminate or query when there is no progress value.
+     * @default 'indeterminate'
+     */
+    variant?: "determinate" | "indeterminate" | "buffer" | "query";
 }
 
 export const LinearProgress = (props: LinearProgressProps) => {
