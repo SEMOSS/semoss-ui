1fe9d2d3a329f8c1a991ae407a2bead313b3a07e
Zylks <tzylks@deloitte.com>
2023-06-27 13:48:53 -0700

    Update Link props

--- packages/ui/src/components/Link/Link.tsx
+++ packages/ui/src/components/Link/Link.tsx
@@ -1,11 +1,42 @@
-import { ReactNode } from "react";
 import MuiLink from "@mui/material/Link";
 import { SxProps } from "@mui/system";
-import { LinkProps as MuiLinkProps } from "@mui/material";
 
-export interface LinkProps extends MuiLinkProps {
-    /** custom style object */
+export interface LinkProps {
+    /**
+     * The content of the component.
+     */
+    children?: React.ReactNode;
+
+    /**
+     * The system prop that allows defining system overrides as well as additional CSS styles.
+     */
     sx?: SxProps;
+
+    /**
+     * Controls when the link should have an underline.
+     * @default 'always'
+     */
+    underline?: "none" | "hover" | "always";
+
+    /**
+     * Applies the theme typography styles.
+     * @default 'inherit'
+     */
+    variant?:
+        | "h1"
+        | "h2"
+        | "h3"
+        | "h4"
+        | "h5"
+        | "h6"
+        | "inherit"
+        | "subtitle1"
+        | "subtitle2"
+        | "body1"
+        | "body2"
+        | "caption"
+        | "button"
+        | "overline";
 }
 
 export const Link = (props: LinkProps) => {
