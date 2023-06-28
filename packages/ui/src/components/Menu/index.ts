4afba05578fc465c2e2575a11c7dca439cc25c2a
Zylks <tzylks@deloitte.com>
2023-06-28 14:33:24 -0700

    Include Menu and MenuItem components

---
+++ packages/ui/src/components/Menu/index.ts
@@ -0,0 +1,10 @@
+import { Menu, MenuProps } from "./Menu";
+import { MenuItem, MenuItemProps } from "./MenuItem";
+
+const MenuNameSpace = Object.assign(Menu, {
+    Item: MenuItem,
+});
+
+export type { MenuProps, MenuItemProps };
+
+export { MenuNameSpace as Menu };
