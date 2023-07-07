95fc1afcec66406f990a6ebdaa40fc11633d2910
Baxter <johbaxter@deloitte.com>
2023-07-07 11:11:43 -0400

    merged dev into database-permissions-continued

--- packages/legacy/app.constants.js
+++ packages/legacy/app.constants.js
@@ -19,7 +19,7 @@ angular
                 pathSplit = [],
                 returnObj;
 
-            mod = "vha-supply";
+            mod = "MonolithDev";
             protocol = window.location.protocol;
             host = window.location.hostname;
             port = window.location.port;
