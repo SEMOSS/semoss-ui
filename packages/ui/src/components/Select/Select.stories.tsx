29b835808000eb70cb689af264b1fa670755a18a
Baxter <johbaxter@deloitte.com>
2023-07-15 17:48:36 -0400

    started cards on the project permissions page; Some additions necessary for BE on Database Permissions;  Also code cleanup necessary for Permissions

--- packages/ui/src/components/Select/Select.stories.tsx
+++ packages/ui/src/components/Select/Select.stories.tsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { Select } from "../Select";
-import { Checkbox } from "../Checkbox";
-import { Input } from "../Input";
-import { MenuItem } from "../Menu";
 import { Stack } from "../Stack";
 import { Box } from "../Box";
 
@@ -61,21 +58,28 @@ const SelectComponent = (args) => {
 
     return (
         <Stack spacing={2}>
-            <Select
-                {...args}
-                sx={{ width: "100%" }}
-                onChange={onChange}
-                value={selected}
-                helperText="Select helper text"
-                placeholder="Select placeholder"
-                label="Select Label"
-            >
-                <Select.Option value={1}>1</Select.Option>
-                <Select.Option value={2}>2</Select.Option>
-                <Select.Option value={3}>3</Select.Option>
-                <Select.Option value={4}>4</Select.Option>
-                <Select.Option value={5}>5</Select.Option>
-            </Select>
+            <div sx={{}}>
+                <div sx={{ width: "70%" }}>Hey</div>
+                <div sx={{ width: "10%" }}>
+                    <Select
+                        {...args}
+                        sx={{ width: "100%" }}
+                        onChange={onChange}
+                        value={selected}
+                        helperText="Select helper text"
+                        placeholder="Select placeholder"
+                        label="Select Label"
+                    >
+                        <Select.Item value={1}>1</Select.Item>
+                        <Select.Item value={2}>2</Select.Item>
+                        <Select.Item value={3}>3</Select.Item>
+                        <Select.Item value={4}>4</Select.Item>
+                        <Select.Item value={5}>5</Select.Item>
+                    </Select>
+                </div>
+
+                <div sx={{ width: "20%" }}>Hello</div>
+            </div>
             <Box>Value: {selected}</Box>
         </Stack>
     );
