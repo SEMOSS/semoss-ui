10a2e6800f10883f1c9df4fcec0c936c6f1c6a26
Baxter <johbaxter@deloitte.com>
2023-07-25 13:10:23 -0400

    Swapped out Tabs for ToggleTabsGroup component on detail page,  work on the home tab, after merging in Theme provider changes

--- packages/client/src/pages/database/DatabaseIndexPage.tsx
+++ packages/client/src/pages/database/DatabaseIndexPage.tsx
@@ -1,10 +1,18 @@
 import { useState, useMemo } from 'react';
-import { styled, Stack, Button, Chip, Typography } from '@semoss/ui';
+import {
+    Button,
+    Card,
+    Chip,
+    Grid,
+    Stack,
+    styled,
+    Typography,
+} from '@semoss/ui';
+
 import { EditOutlined } from '@mui/icons-material';
+
 import { Link } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
-
-import Cat from '@/assets/img/cat.jpg';
 
 import { Section, LoadingScreen } from '@/components/ui';
 import { Markdown } from '@/components/common';
@@ -115,7 +123,7 @@ export const DatabaseIndexPage = observer(() => {
             {dbMetaData.markdown && (
                 <Section>
                     <Section.Header>
-                        <Typography variant={'h6'}>About</Typography>
+                        <Typography variant={'h6'}>Details</Typography>
                     </Section.Header>
                     <Markdown content={dbMetaData.markdown} />
                 </Section>
@@ -183,10 +191,38 @@ export const DatabaseIndexPage = observer(() => {
                 <Section.Header>
                     <Typography variant={'h6'}>Statistics</Typography>
                 </Section.Header>
-                <div>
-                    <p>Total Uses:</p>
-                    <p>Total Views:</p>
-                </div>
+                <Grid container spacing={3}>
+                    <Grid item sm={12} md={6} lg={4} xl={3}>
+                        <Card>
+                            <div>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="36"
+                                    height="36"
+                                    viewBox="0 0 36 36"
+                                    fill="none"
+                                >
+                                    <path
+                                        d="M18 9.75C23.685 9.75 28.755 12.945 31.23 18C28.755 23.055 23.7 26.25 18 26.25C12.3 26.25 7.245 23.055 4.77 18C7.245 12.945 12.315 9.75 18 9.75ZM18 6.75C10.5 6.75 4.095 11.415 1.5 18C4.095 24.585 10.5 29.25 18 29.25C25.5 29.25 31.905 24.585 34.5 18C31.905 11.415 25.5 6.75 18 6.75ZM18 14.25C20.07 14.25 21.75 15.93 21.75 18C21.75 20.07 20.07 21.75 18 21.75C15.93 21.75 14.25 20.07 14.25 18C14.25 15.93 15.93 14.25 18 14.25ZM18 11.25C14.28 11.25 11.25 14.28 11.25 18C11.25 21.72 14.28 24.75 18 24.75C21.72 24.75 24.75 21.72 24.75 18C24.75 14.28 21.72 11.25 18 11.25Z"
+                                        fill="#0471F0"
+                                    />
+                                </svg>
+                            </div>
+                            <Card.Content>
+                                <Typography>Views</Typography>
+                            </Card.Content>
+                        </Card>
+                    </Grid>
+                    <Grid item sm={12} md={6} lg={4} xl={3}>
+                        Total Views:
+                    </Grid>
+                    <Grid item sm={12} md={6} lg={4} xl={3}>
+                        Total Views:
+                    </Grid>
+                    <Grid item sm={12} md={6} lg={4} xl={3}>
+                        Total Views:
+                    </Grid>
+                </Grid>
             </Section>
             <Section>
                 <Section.Header>
