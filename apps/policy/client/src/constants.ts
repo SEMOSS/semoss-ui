c0ecdc2d2f0f2a7a065aea3d910f3794d14b31f8
Neel Sanghvi <>
2023-08-14 16:00:17 -0400

    feat(client/theme): adding theming variables to the ui

--- apps/policy/client/src/constants.ts
+++
@@ -1,3 +0,0 @@
-export const ENDPOINT = process.env.ENDPOINT;
-export const MODULE = process.env.MODULE;
-export const APP = process.env.APP;
--- packages/client/src/constants.ts
+++ packages/client/src/constants.ts
@@ -1,2 +1,9 @@
+import Logo from '@/assets/logo.svg';
+
 export const ENDPOINT = process.env.ENDPOINT;
 export const MODULE = process.env.MODULE;
+
+export const THEME = {
+    name: 'SEMOSS',
+    logo: Logo,
+};
