065b6e73998d9e5de1e5246401d92e17bc8de3d3
Justin Buckner <jubuckner@deloitte.com>
2023-08-18 03:29:14 -0400

    style(MetamodelNode): add style to MetamodelNode

--- packages/client/src/components/metamodel/Metamodel.tsx
+++ packages/client/src/components/metamodel/Metamodel.tsx
@@ -52,7 +52,6 @@ export const Metamodel = (props: MetamodelProps) => {
         callback,
         isInteractive,
     } = props;
-    console.log('props: ', props);
 
     // node is {
     //     data: { name: 'table', properties: [{ id: 'colid', name: 'colname', type: 'coltype' }] },
@@ -95,18 +94,31 @@ export const Metamodel = (props: MetamodelProps) => {
                 }
             }
         }
+        if (action === 'COLUMN_TYPE_CHANGE') {
+            // handle column type change
+        }
+        if (action === 'COLUMN_DESCRIPTION_CHANGE') {
+            // handle column type change
+        }
+        if (action === 'COLUMN_LOGICAL_NAME_CHANGE') {
+            // handle column type change
+        }
+
         if (action === 'TABLE_NAME_CHANGE') {
             // handle table name change
         }
-
+        if (action === 'TABLE_DESCRIPTION_CHANGE') {
+            // handle table relationship change
+        }
         if (action === 'TABLE_RELATIONSHIP_CHANGE') {
+            // handle table relationship change
+        }
+        if (action === 'TABLE_POSITION_CHANGE') {
             // handle table relationship change
         }
         // if action === 'column data type change'
     };
 
-    console.log('originalData: ', originalData);
-    console.log('data: ', data);
     // create the context
     const metamodelContext: MetamodelContextType = {
         selectedNodeId: selectedNode ? selectedNode.id : null,
@@ -127,8 +139,6 @@ export const Metamodel = (props: MetamodelProps) => {
                         }
                     }
                 }
-                console.log('node: ', node);
-                console.log('onSelectNode func: ', onSelectNode);
 
                 onSelectNode(node);
             },
@@ -139,21 +149,40 @@ export const Metamodel = (props: MetamodelProps) => {
     };
 
     const onSubmit = () => {
-        // format the data
-        const dataObj = {
-            metamodel: { relation: [], nodeProp: {} },
-            dataTypeMap: {},
+        const payloadObj = {
+            metamodel: {
+                relation: [
+                    // { fromTable: 'id', toTable: 'Drug', relName: 'id_Drug' },
+                ],
+                nodeProp: {
+                    // tableName: [ // 'colname' EXCLUDING THE FIRST COLUMN
+                    // ],
+                },
+            },
+            dataTypeMap: {
+                // colName: type,
+            },
+            newHeaders: {}, // oldColName: newColName
+            additionalDataTypes: {}, // colName: specificFormat
+            descriptionMap: {}, // colName: description
+            logicalNamesMap: {}, // colName/alias: logicalName
+            position: [{}],
         };
+        // // format the data
+        // const dataObj = {
+        //     metamodel: { relation: [], nodeProp: {} },
+        //     dataTypeMap: {},
+        // };
 
         // loop over data state
 
         // build metamodel relation for each table
         for (const edge of data.edges) {
             const relName = `${edge.source}_${edge.target}`;
-            dataObj.metamodel.relation.push({
-                relName: relName,
-                toTable: edge.target,
+            payloadObj.metamodel.relation.push({
                 fromTable: edge.source,
+                toTable: edge.target,
+                relName: relName,
             });
         }
 
@@ -161,24 +190,30 @@ export const Metamodel = (props: MetamodelProps) => {
 
         // build dataTypeMap for each table
         for (const node of data.nodes) {
+            console.log('node: ', node);
             // const temp = { [node.data.name]: [] };
-            const temp = [];
+            // const temp = [];
             for (const col of node.data.properties) {
-                dataObj.dataTypeMap[col.name] = col.type;
+                payloadObj.dataTypeMap[col.name] = col.type;
                 // temp[node.data.name].push(col.name);
-                temp.push(col.name);
+                // if (idx === 0) {
+                // no-op to skip pushing in the first column bc nodeProp does not accept the first column !!! Need to figure out why?
+                // } else {
+                // if (node.data.properties.length <= 1) {
+                //     // no-op
+                // } else {
+                //     temp.push(col.name);
+                // }
+                // }
             }
 
-            dataObj.metamodel.nodeProp[node.data.name] = temp;
+            payloadObj.metamodel.nodeProp[node.data.name] = [];
         }
 
-        console.log('dataObj: ', dataObj);
-        callback(dataObj);
+        console.log('dataObj: ', payloadObj);
+        // callback(payloadObj);
+        callback(payloadObj);
     };
-
-    useEffect(() => {
-        console.log('data changed: ', data);
-    }, [data]);
 
     return (
         <MetamodelContext.Provider value={metamodelContext}>
@@ -210,3 +245,6 @@ export const Metamodel = (props: MetamodelProps) => {
  *  removed arg / prop in CSVForm calling Metamodel component bc it was null and caused error. And the function is not needed in the CSVForm
  *
  */
+
+// OBSERVATION: metamodel in legacy gives the table a primary key column that matches the table name...
+// dataType map needs to include the primary key for all tables
--- packages/client/src/components/metamodel/MetamodelNode.tsx
+++ packages/client/src/components/metamodel/MetamodelNode.tsx
@@ -1,12 +1,210 @@
 import React, { useState } from 'react';
 import { NodeProps, Handle, Position } from 'react-flow-renderer';
 
-import { styled, Icon, Button } from '@semoss/ui';
-import { TableBar, ViewColumn, EditRounded, Delete } from '@mui/icons-material';
+import { styled, Icon, Button, Typography } from '@semoss/ui';
+import {
+    TableChartOutlined,
+    TableViewRounded,
+    ViewColumn,
+    EditRounded,
+    Delete,
+} from '@mui/icons-material';
 
 import { useMetamodel } from '@/hooks';
 
-import { EditColumnModal, EditTableModal } from '@/components/physicalDatabase';
+import { EditColumnModal } from '@/components/physicalDatabase';
+
+// one-off custom icon from figma
+const TableIcon = () => {
+    return (
+        <svg
+            width="30"
+            height="30"
+            viewBox="0 0 30 30"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <rect
+                x="0.5"
+                y="0.5"
+                width="29"
+                height="29"
+                rx="7.5"
+                fill="white"
+            />
+            <path
+                d="M8 7H22C22.5304 7 23.0391 7.21071 23.4142 7.58579C23.7893 7.96086 24 8.46957 24 9V21C24 21.5304 23.7893 22.0391 23.4142 22.4142C23.0391 22.7893 22.5304 23 22 23H8C7.46957 23 6.96086 22.7893 6.58579 22.4142C6.21071 22.0391 6 21.5304 6 21V9C6 8.46957 6.21071 7.96086 6.58579 7.58579C6.96086 7.21071 7.46957 7 8 7ZM8 11V15H14V11H8ZM16 11V15H22V11H16ZM8 17V21H14V17H8ZM16 17V21H22V17H16Z"
+                fill="#975FE4"
+            />
+            <rect
+                x="0.5"
+                y="0.5"
+                width="29"
+                height="29"
+                rx="7.5"
+                stroke="#975FE4"
+            />
+        </svg>
+    );
+};
+
+const StyledMetamodelCard = styled('div')(({ theme }) => ({
+    alignItems: 'flex-start',
+    borderRadius: theme.shape.borderRadius,
+    boxShadow: '0px 5px 22px #0000000f',
+    display: 'inline-flex',
+    flexDirection: 'column',
+    overflow: 'hidden',
+    position: 'relative',
+    width: '354px',
+}));
+const StyledMetamodelHeaderFrame = styled('div')(({ theme }) => ({
+    alignItems: 'center',
+    alignSelf: 'stetch',
+    backgroundColor: 'rgba(241, 233, 251, 1)',
+    borderRadius: '12px 12px 0px 0px',
+    boxShadow: '0px 5px 22px #0000000f',
+    display: 'flex',
+    gap: '10px',
+    height: '54px',
+    padding: '10px',
+    position: 'relative',
+    width: '100%',
+}));
+const StyledMetamodelIconButton = styled(Button)(({ theme }) => ({
+    alignItems: 'center',
+    borderColor: 'rgba(151, 95, 228, 1) !important',
+    borderRadius: theme.shape.borderRadius,
+    justifyContent: 'center !important',
+    marginBottom: '-9px !important',
+    marginTop: '-9px !important',
+}));
+
+const StyledInstanceNode = styled('div')(({ theme }) => ({
+    alignItems: 'center !important',
+    flex: '0 0 auto !important',
+    justifyContent: 'center !important',
+    marginBottom: '-1px !important',
+    marginLeft: '-1px !important',
+    marginRight: '-1px !important',
+    marginTop: '-1px !important',
+}));
+const StyledButtonSecondary = styled(Button)(({ theme }) => ({
+    alignSelf: 'stretch !important',
+    display: 'flex !important',
+    flex: '1 !important',
+    flexGrow: '1 !important',
+    justifyContent: 'center !important',
+    width: '100%',
+}));
+const StyledFrameWrapper = styled('div')(({ theme }) => ({
+    alignItems: 'flex-start',
+    display: 'flex',
+    flex: '1',
+    flexDirection: 'column',
+    flexGrow: '1',
+    gap: '4px',
+    marginBottom: '-10px',
+    marginTop: '-10px',
+    position: 'relative',
+}));
+const StyledMetamodelCardDiv = styled('div')(({ theme }) => ({
+    alignItems: 'flex-start',
+    alignSelf: 'stretch',
+    display: 'flex',
+    flex: '0 0 auto',
+    position: 'relative',
+    width: '100%',
+}));
+
+const StyledHeaderFont = styled(Typography)(() => ({
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    color: '#000000de',
+    flex: 1,
+    fontFamily: 'Inter Hevetica',
+    fontSize: '16px',
+    fontWeight: 500,
+    letterSpacing: '.15px',
+    lineHeight: '24px',
+    marginTop: '-1px',
+    marginLeft: '6px',
+    position: 'relative',
+}));
+const StyledValueFont = styled(Typography)(() => ({
+    alignSelf: 'stretch',
+    color: 'rgba(34, 164, 255, 1)',
+    flex: 1,
+    fontFamily: 'Inter-Medium, Helvetica',
+    fontSize: '16px',
+    fontWeight: 500,
+    letterSpacing: '.15px',
+    lineHeight: '24px',
+    marginTop: '-1px',
+    position: 'relative',
+}));
+
+const StyledColumnNameCell = styled('div')(() => ({
+    flex: '0 0 auto !important',
+}));
+
+const StyledTableHeader = styled('div')(() => ({
+    alignItems: 'flex-start',
+    backgroundColor: '#ffffff',
+    borderRadius: '0px 12px 12px 12px',
+    display: 'inline-flex',
+    flex: '0 0 auto',
+    flexDirection: 'column',
+    padding: '0px 0px 16px',
+    position: 'relative',
+}));
+
+const StyledTableRow = styled('div')(() => ({
+    display: 'flex',
+    alignSelf: 'stretch',
+    alignItems: 'flex-start',
+    padding: '8px 0px',
+    justifyContent: 'space-between',
+    backgroundColor: '#ffffff01',
+    // flex: '0 0 auto',
+    gap: '4',
+    position: 'relative',
+    width: '100%',
+}));
+
+const StyledTableCell = styled('div')(() => ({
+    height: '44px !important',
+    width: '114.67px !important',
+}));
+
+const StyledDivider = styled('div')(() => ({
+    height: '2px',
+    alignSelf: 'stretch !important',
+    borderColor: '#000000',
+    position: 'relative',
+    width: '100%',
+}));
+const StyledIconWrapper = styled('div')(() => ({
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    opacity: 0,
+    padding: '10px 16px',
+    position: 'relative',
+    width: '114.67px',
+}));
+
+const StyledIconKeyWrapper = styled('div')(() => ({
+    alignItems: 'flex-start',
+    display: 'inline-flex',
+    flex: '0 0 auto',
+    position: 'relative',
+}));
+const StyledRoundedKeyWrapper = styled('div')(() => ({
+    height: '24px',
+    position: 'relative',
+    width: '24px',
+}));
 
 const StyledNode = styled('div', {
     shouldForwardProp: (prop) => prop !== 'isSelected',
@@ -57,6 +255,33 @@ const StyledTitleContainer = styled('div')(() => ({
     alignItems: 'center',
     overflow: 'hidden',
 }));
+
+/**
+ * node container
+ * sub container
+ *
+ * table header row
+ *  table header icon cell
+ *  table header name cell
+ *      header name font styling
+ *  table header edit icon cell
+ *      edit table icon button
+ *  table header delete icon cell
+ *      delete table icon button
+ *
+ * divider
+ *
+ * table body container
+ *  table row container
+ *    table row
+ *      table cell container: column name
+ *      table cell container: column type
+ *      table cell container: column icon forKey/primKey
+ *      table cell container: edit column icon button
+ *
+ * relationship styling
+ *
+ */
 
 type MetamodelNodeProps = NodeProps<{
     /** Name of the node */
@@ -126,35 +351,6 @@ const _MetamodelNode = (props: MetamodelNodeProps) => {
     const [originalData, setOriginalData] = useState(data);
     const [newData, setNewData] = useState(data);
 
-    const [finalData, setFinalData] = useState({
-        newTables: [
-            {
-                id: '',
-                prevName: '',
-                newName: '',
-                columns: [{ name: '', type: '' }],
-            },
-        ],
-        newColumns: [
-            {
-                table: { id: '', name: '' },
-                prevName: '',
-                newName: '',
-                type: '',
-            },
-        ],
-    });
-    const [nodeData, setNodeData] = useState({
-        table: { id: '', prevName: '', newName: '' },
-        columns: [
-            {
-                table: { id: '', name: '' },
-                prevName: '',
-                newName: '',
-                type: '',
-            },
-        ],
-    });
     const [relations, setRelations] = useState([{ source: '', target: '' }]);
 
     const [tableOptions, setTableOptions] = useState([]);
@@ -186,76 +382,95 @@ const _MetamodelNode = (props: MetamodelNodeProps) => {
     const [openEditColumnModal, setOpenEditColumnModal] = useState(false);
     const [databaseUpdated, setDatabaseUpdated] = useState(false);
 
-    console.log('onSelectNodeId in mmnode: ', onSelectNodeId);
     return (
-        <StyledNode
+        <StyledMetamodelCard
             isSelected={selectedNodeId === id}
             onClick={() => {
                 onSelectNodeId(id);
             }}
         >
             <StyledHandle type="target" position={Position.Left} />
-            <StyledRow isHeader={true}>
-                <StyledIcon>
-                    <TableBar />
-                </StyledIcon>
-                {isInteractive ? (
-                    <StyledRow isHeader={true}>
-                        <StyledTitleContainer>
-                            <Button
+            <StyledMetamodelHeaderFrame>
+                <StyledTableRow>
+                    {/* <StyledMetamodelIconButton>
+                        <TableIcon />
+                    </StyledMetamodelIconButton> */}
+                    <StyledMetamodelIconButton
+                        variant={'text'}
+                        color={'primary'}
+                        disabled={!isInteractive}
+                        onClick={() => {
+                            setTableToEdit({
+                                tableId: id,
+                                tableName: data.name,
+                                tableDescription: '',
+                                columns: data.properties,
+                            });
+                            setOpenEditTableModal(true);
+                        }}
+                    >
+                        <TableIcon />
+                    </StyledMetamodelIconButton>
+                    {isInteractive ? (
+                        <StyledTableRow>
+                            <StyledTitleContainer>
+                                {/* <Button
+                                    variant={'text'}
+                                    color={'primary'}
+                                    onClick={() => {
+                                        setTableToEdit({
+                                            tableId: id,
+                                            tableName: data.name,
+                                            tableDescription: '',
+                                            columns: data.properties,
+                                        });
+                                        setOpenEditTableModal(true);
+                                    }}
+                                >
+                                    <EditRounded />
+                                </Button> */}
+                                <StyledHeaderFont variant="body1">
+                                    {data.name.toLowerCase()}
+                                </StyledHeaderFont>
+                            </StyledTitleContainer>
+                            <StyledButtonSecondary
                                 variant={'text'}
-                                color={'primary'}
+                                color={'error'}
                                 onClick={() => {
-                                    setTableToEdit({
-                                        tableId: id,
-                                        tableName: data.name,
-                                        tableDescription: '',
-                                        columns: data.properties,
+                                    setDataToDelete({
+                                        structureId: id,
+                                        structureName: data.name,
+                                        structureType: 'table',
                                     });
-                                    setOpenEditTableModal(true);
+                                    setOpenDeleteConfirmationModal(true);
                                 }}
                             >
-                                {/* <Icon
-                                    size="md"
-                                    color="primary"
-                                    path={EditIcon}
-                                ></Icon> */}
-                                <EditRounded />
-                            </Button>
-                            <StyledTitle>{data.name}</StyledTitle>
-                        </StyledTitleContainer>
-                        <Button
-                            variant={'text'}
-                            color={'error'}
-                            onClick={() => {
-                                setDataToDelete({
-                                    structureId: id,
-                                    structureName: data.name,
-                                    structureType: 'table',
-                                });
-                                setOpenDeleteConfirmationModal(true);
-                            }}
-                        >
-                            {/* <Icon size="md" color="red" path={mdiDelete}></Icon> */}
-                            <Delete />
-                        </Button>
-                    </StyledRow>
-                ) : (
-                    <StyledTitle>{data.name}</StyledTitle>
-                )}
-                <StyledTitle>{data.name}</StyledTitle>
-            </StyledRow>
+                                {/* <Icon size="md" color="red" path={mdiDelete}></Icon> */}
+                                <Delete />
+                            </StyledButtonSecondary>
+                        </StyledTableRow>
+                    ) : (
+                        <StyledHeaderFont variant="body1">
+                            {data.name.toLowerCase()}
+                        </StyledHeaderFont>
+                    )}
+                </StyledTableRow>
+            </StyledMetamodelHeaderFrame>
+            <StyledDivider />
             {data.properties.map((p) => (
-                <StyledRow isHeader={true} key={p.id}>
+                <StyledTableRow key={p.id}>
                     {isInteractive ? (
-                        <StyledRow isHeader={true}>
-                            <StyledTitleContainer>
-                                <Button
+                        <StyledTableRow>
+                            {/* <StyledTitleContainer> */}
+                            {/* <Button
                                     variant={'text'}
                                     color={'primary'}
                                     onClick={() => {
                                         setColumnToEdit({
-                                            table: { id: id, name: data.name },
+                                            table: {
+                                                id: id,
+                                                name: data.name,
+                                            },
                                             columnName: p.name,
                                             columnDescription: 'test',
                                             columnType: p.type,
@@ -265,16 +480,21 @@ const _MetamodelNode = (props: MetamodelNodeProps) => {
                                         });
                                         setOpenEditColumnModal(true);
                                     }}
-                                >
-                                    {/* <Icon
+                                > */}
+                            {/* <Icon
                                         path={mdiPencil}
                                         color="primary"
                                     ></Icon> */}
-                                    <EditRounded />
-                                </Button>
-                                <StyledTitle>{p.name}</StyledTitle>
-                            </StyledTitleContainer>
-                            <Button
+                            {/* <EditRounded /> */}
+                            {/* </Button> */}
+                            <StyledHeaderFont variant="body2">
+                                {p.name.toLowerCase()}
+                            </StyledHeaderFont>
+                            {/* </StyledTitleContainer> */}
+                            <StyledValueFont variant="body2">
+                                {p.type.toLowerCase()}
+                            </StyledValueFont>
+                            <StyledButtonSecondary
                                 variant={'text'}
                                 color={'error'}
                                 onClick={() => {
@@ -286,23 +506,23 @@ const _MetamodelNode = (props: MetamodelNodeProps) => {
                                     setOpenDeleteConfirmationModal(true);
                                 }}
                             >
-                                {/* <Icon
-                                    path={mdiDelete}
-                                    size="sm"
-                                    color="red"
-                                ></Icon> */}
                                 <Delete />
-                            </Button>
-                        </StyledRow>
+                            </StyledButtonSecondary>
+                        </StyledTableRow>
                     ) : (
                         <>
-                            <StyledIcon>
+                            {/* <StyledIcon>
                                 <ViewColumn />
-                            </StyledIcon>
-                            <StyledTitle>{p.name}</StyledTitle>
+                            </StyledIcon> */}
+                            <StyledHeaderFont variant="body2">
+                                {p.name.toLowerCase()}
+                            </StyledHeaderFont>
+                            <StyledValueFont variant="body2">
+                                {p.type.toLowerCase()}
+                            </StyledValueFont>
                         </>
                     )}
-                </StyledRow>
+                </StyledTableRow>
             ))}
             <StyledHandle type="source" position={Position.Right} />
             {openEditColumnModal && (
@@ -314,7 +534,7 @@ const _MetamodelNode = (props: MetamodelNodeProps) => {
                     updateState={updateData}
                 />
             )}
-        </StyledNode>
+        </StyledMetamodelCard>
     );
     // return (
     //     <StyledNode
