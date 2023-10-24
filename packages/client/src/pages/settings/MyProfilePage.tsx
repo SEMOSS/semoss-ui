82bf91f4f174bc0ea804f8f53751b905b52ead55
Betthauser <tbetthauser@deloitte.com>
2023-10-24 14:17:18 -0700

    feat: add placeholder section for profile image update and working modal partially styled

--- packages/client/src/pages/settings/MyProfilePage.tsx
+++ packages/client/src/pages/settings/MyProfilePage.tsx
@@ -1,4 +1,9 @@
-import { Add, Delete, ContentCopyOutlined } from '@mui/icons-material';
+import {
+    Add,
+    Delete,
+    ContentCopyOutlined,
+    EditRounded,
+} from '@mui/icons-material';
 import { useForm, Controller } from 'react-hook-form';
 import {
     styled,
@@ -11,11 +16,21 @@ import {
     Modal,
     Alert,
     TextField,
+    Icon,
+    Box,
 } from '@semoss/ui';
 
 import { useAPI, useRootStore } from '@/hooks';
 import { LoadingScreen } from '@/components/ui';
 import { useState } from 'react';
+
+const ProfileImagePlaceholder = styled('span')(({ theme }) => ({
+    display: 'block',
+    width: '50px',
+    height: '50px',
+    backgroundColor: '#999',
+    borderRadius: '50%',
+}));
 
 const StyledCodeBlock = styled('pre')(({ theme }) => ({
     display: 'flex',
@@ -43,8 +58,9 @@ export const MyProfilePage = () => {
     const notification = useNotification();
     const { monolithStore } = useRootStore();
 
-    // track the model
+    // track the models
     const [addModal, setAddModal] = useState(false);
+    const [profileImgModal, setProfileImgModal] = useState(false);
 
     // get the keys
     const getUserAccessKeys = useAPI(['getUserAccessKeys']);
@@ -139,8 +155,12 @@ export const MyProfilePage = () => {
             getUserAccessKeys.refresh();
         }
 
-        // rest the form
+        // reset the form
         reset({});
+    };
+
+    const closeProfileEditModel = () => {
+        setProfileImgModal(false);
     };
 
     /**
@@ -172,6 +192,28 @@ export const MyProfilePage = () => {
 
     return (
         <>
+            {/* ### ---> test stack for profileImgModal */}
+
+            <Stack direction="row" alignItems={'center'}>
+                {/* ### ---> need to add a table or grid here - does semoss-ui use Grid? */}
+                <Typography variant="h6">
+                    <strong>Edit profile picture</strong>
+                </Typography>
+
+                <ProfileImagePlaceholder />
+
+                <Button
+                    variant="text"
+                    // startIcon={<EditRounded />}
+                    sx={{ textAlign: 'right' }}
+                    onClick={() => {
+                        setProfileImgModal(true);
+                    }}
+                >
+                    Upload
+                </Button>
+            </Stack>
+
             <Stack direction="row" justifyContent={'space-between'} mb={1}>
                 <Typography variant="h6">
                     <strong>Access Keys</strong>
@@ -188,6 +230,7 @@ export const MyProfilePage = () => {
                     Add New
                 </Button>
             </Stack>
+
             <Table.Container>
                 <Table>
                     <Table.Head>
@@ -379,6 +422,178 @@ export const MyProfilePage = () => {
                     </Button>
                 </Modal.Actions>
             </Modal>
+
+            {/* ### ---> test modal */}
+            <Modal open={profileImgModal} onClose={() => closeModel()}>
+                <Modal.Title>Upload Profile Picture</Modal.Title>
+                <Modal.Content>
+                    <ProfileImagePlaceholder />
+                    {/* <form onSubmit={handleSubmit(createAccessKey)}>
+                        <Stack direction="column" spacing={2}>
+                            <Alert severity="info" variant="outlined">
+                                Note: Your private key will only be generated
+                                once
+                            </Alert>
+
+                            <Controller
+                                name={'TOKENNAME'}
+                                control={control}
+                                rules={{ required: true }}
+                                render={({ field }) => {
+                                    return (
+                                        <TextField
+                                            required
+                                            label="Name"
+                                            value={
+                                                field.value ? field.value : ''
+                                            }
+                                            disabled={isCreated}
+                                            onChange={(value) =>
+                                                field.onChange(value)
+                                            }
+                                            inputProps={{ maxLength: 255 }}
+                                        ></TextField>
+                                    );
+                                }}
+                            />
+
+                            <Controller
+                                name={'TOKENDESCRIPTION'}
+                                control={control}
+                                rules={{ required: false }}
+                                render={({ field }) => {
+                                    return (
+                                        <TextField
+                                            label="Description"
+                                            value={
+                                                field.value ? field.value : ''
+                                            }
+                                            disabled={isCreated}
+                                            onChange={(value) =>
+                                                field.onChange(value)
+                                            }
+                                            inputProps={{ maxLength: 500 }}
+                                        ></TextField>
+                                    );
+                                }}
+                            />
+
+                            <Stack direction="row" justifyContent={'start'}>
+                                <Button
+                                    disabled={isCreated}
+                                    type="submit"
+                                    variant={'outlined'}
+                                    color="primary"
+                                >
+                                    Generate
+                                </Button>
+                            </Stack>
+                            {isCreated && (
+                                <>
+                                    <Stack direction="column" spacing={1}>
+                                        <Typography variant={'subtitle2'}>
+                                            Access Key
+                                        </Typography>
+                                        <StyledCodeBlock>
+                                            <StyledCodeContent>
+                                                {ACCESSKEY}
+                                            </StyledCodeContent>
+                                            <Button
+                                                size={'medium'}
+                                                variant="outlined"
+                                                startIcon={
+                                                    <ContentCopyOutlined
+                                                        color={'inherit'}
+                                                    />
+                                                }
+                                                onClick={() => copy(ACCESSKEY)}
+                                            >
+                                                Copy
+                                            </Button>
+                                        </StyledCodeBlock>
+                                    </Stack>
+                                    <Stack direction="column" spacing={1}>
+                                        <Typography variant={'subtitle2'}>
+                                            Secret Key
+                                        </Typography>
+                                        <StyledCodeBlock>
+                                            <StyledCodeContent>
+                                                {SECRETKEY}
+                                            </StyledCodeContent>
+                                            <Button
+                                                size={'medium'}
+                                                variant="outlined"
+                                                startIcon={
+                                                    <ContentCopyOutlined
+                                                        color={'inherit'}
+                                                    />
+                                                }
+                                                onClick={() => copy(SECRETKEY)}
+                                            >
+                                                Copy
+                                            </Button>
+                                        </StyledCodeBlock>
+                                    </Stack>
+                                </>
+                            )}
+                        </Stack>
+                    </form> */}
+                </Modal.Content>
+                <Modal.Actions>
+                    <Button
+                        variant="text"
+                        onClick={() => closeProfileEditModel()}
+                    >
+                        Close
+                    </Button>
+                </Modal.Actions>
+            </Modal>
         </>
     );
 };
+
+// To-Do for ticket 144...
+
+// ---------------------------
+
+// My Profile page needs to be updated with the following items:
+//  Dependent on another branch hold off on this: Edit Profile Picture container **will require a crop window after upload, design team is working on building this out in future
+//  Edit Profile Information container with input/editable fields <-- [?] does this mean a modal
+//  Display of Monolith Endpoint and ability to copy
+//  Update current access key table to sit within a container as shown in designs and update styling accordingly
+//  Update the generate key modals according to the designs
+
+// ---------------------------
+
+// Generate Key modal may be working
+// need to test with correct user permissions
+// need to check for second stage of modal -- where Access Key and Secret Key are both shown with copy icon
+
+// Add Edit profile picture container -- save for later [?]
+// profile picture img
+// Upload blue link / text-only button
+// open profile pic edit modal
+// hide / save for later
+
+// Add Edit profile information container
+// input fields for First Name and Last Name
+// input for Username
+// input for Email
+// Blue button for Save
+// black text-only button for Reset
+// do we have a editProfileInfo reactor [?]
+
+// Add a Monolith Endpoint container
+// displays enpoint text with copy / clipboard icon
+// do we already have access to this info from existing reactor calls [?]
+
+// separate modal for -- Upload Profile Picture -- waiting on this [?]
+// text input for Name edit [?]
+// update image tool -- waiting on this still [?]
+
+// separate modal for -- Edit Access Key
+// uneditable display for Access Key: ...
+// editable text input for Name*
+// editable text input for Description
+// blue Save button
+// do we have an editAccessKey reactor [?]
