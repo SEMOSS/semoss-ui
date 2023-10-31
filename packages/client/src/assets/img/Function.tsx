368d7cb155a27384d78d58958fff2a2d09c6ea9c
Baxter <johbaxter@deloitte.com>
2023-10-31 15:48:13 -0400

    style(client): push svgs for function and vector

---
+++ packages/client/src/assets/img/Function.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { SwitchAccessShortcutOutlined } from '@mui/icons-material';
+import { styled, CustomPaletteOptions } from '@semoss/ui';
+
+const StyledContainer = styled('div')(({ theme }) => {
+    return {
+        // width: '50px', height: '50px',
+        '.MuiIcon-fontSizeLarge': {
+            width: '2em',
+            height: '2em',
+        },
+    };
+});
+
+const StyledIcon = styled(SwitchAccessShortcutOutlined)(({ theme }) => {
+    const palette = theme.palette as CustomPaletteOptions;
+
+    return {
+        color: palette.pink['300'],
+    };
+});
+
+export const Function = () => {
+    return (
+        <StyledContainer>
+            <StyledIcon fontSize="large" />
+        </StyledContainer>
+    );
+};
