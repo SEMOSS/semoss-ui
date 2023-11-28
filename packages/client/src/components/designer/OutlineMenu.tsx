98b315784d5a7f0c22bd91e731938449084807ed
Baxter <johbaxter@deloitte.com>
2023-11-28 17:16:10 -0500

    style(client): add layers and pages to outline pane, tree view

--- packages/client/src/components/designer/OutlineMenu.tsx
+++ packages/client/src/components/designer/OutlineMenu.tsx
@@ -1,27 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { observer } from 'mobx-react-lite';
 import {
-    Avatar,
-    styled,
-    List,
-    Typography,
+    Collapse,
     Divider,
     Icon,
+    IconButton,
+    List,
+    TextField,
     TreeView,
+    Typography,
+    styled,
 } from '@semoss/ui';
 import { useBlocks, useDesigner } from '@/hooks';
 import {
-    AutoAwesome,
-    ContentCopyOutlined,
-    ExpandMore,
     ChevronRight,
-    KeyboardDoubleArrowLeft,
-    KeyboardDoubleArrowRight,
-    CreateNewFolderOutlined,
-    NoteAddOutlined,
+    ExpandMore,
+    Home,
+    SearchOutlined,
 } from '@mui/icons-material/';
 
 import { getIconForBlock } from '../block-defaults';
+import { Block } from '@/stores/state/state.types';
 
 const StyledMenu = styled('div')(({ theme }) => ({
     display: 'flex',
@@ -35,12 +34,19 @@ const StyledMenuHeader = styled('div')(({ theme }) => ({
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: 'space-between',
     paddingTop: theme.spacing(1.5),
     paddingRight: theme.spacing(1),
     paddingBottom: theme.spacing(1.5),
     paddingLeft: theme.spacing(2),
     gap: theme.spacing(1),
+}));
+
+const StyledCollapseSearch = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'flex-end',
 }));
 
 const StyledMenuScroll = styled('div')(({ theme }) => ({
@@ -52,6 +58,15 @@ const StyledMenuScroll = styled('div')(({ theme }) => ({
     overflowY: 'auto',
 }));
 
+const StyledSearchedLabel = styled('span', {
+    shouldForwardProp: (prop) => prop !== 'search',
+})<{
+    /** Track if it is a search term */
+    search: boolean;
+}>(({ search, theme }) => ({
+    color: search ? theme.palette.primary.main : '',
+}));
+
 const StyledListItemButton = styled(List.ItemButton, {
     shouldForwardProp: (prop) => prop !== 'hovered',
 })<{
@@ -59,18 +74,21 @@ const StyledListItemButton = styled(List.ItemButton, {
     hovered: boolean;
 }>(({ hovered, theme }) => ({
     // outline: hovered ? '1px solid red' : '',
-    background: hovered ? theme.palette.action.hover : '',
+    background: hovered ? theme.palette.action.selected : '',
     '&:hover *[data-hover]': {
         visibility: 'visible',
     },
 }));
 
-const StyledListIndent = styled('div')(({ theme }) => ({
-    paddingLeft: theme.spacing(2),
-}));
-
 const StyledIcon = styled(Icon)(({ theme }) => ({
     color: 'rgba(0, 0, 0, .3)',
+}));
+
+const StyledTreeItemLabel = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'row',
+    padding: theme.spacing(1),
+    gap: theme.spacing(1),
 }));
 
 /**
@@ -83,7 +101,8 @@ export const OutlineMenu = observer((): JSX.Element => {
 
     const [expanded, setExpanded] = useState<string[]>([]);
     const [selected, setSelected] = useState<string[]>([]);
-
+    const [showSearch, setShowSearch] = useState<boolean>(false);
+    const [search, setSearch] = useState<string>('');
     /**
      * Render the block and it's children
      * @param id - id of the block to render
@@ -109,10 +128,39 @@ export const OutlineMenu = observer((): JSX.Element => {
             <TreeView.Item
                 key={block.id}
                 nodeId={block.id}
-                endIcon={<WidgetIcon />}
                 label={
-                    block.widget.charAt(0).toUpperCase() + block.widget.slice(1)
+                    <StyledTreeItemLabel>
+                        <StyledIcon>
+                            <WidgetIcon />
+                        </StyledIcon>
+                        <StyledSearchedLabel
+                            search={
+                                block.widget
+                                    .toLowerCase()
+                                    .indexOf(
+                                        search
+                                            ? search.toLowerCase()
+                                            : 'Not a widget',
+                                    ) > -1
+                            }
+                        >
+                            {block.widget.charAt(0).toUpperCase() +
+                                block.widget.slice(1)}
+                        </StyledSearchedLabel>
+                    </StyledTreeItemLabel>
                 }
+                onClick={(e: React.SyntheticEvent) => {
+                    e.stopPropagation();
+                    designer.setSelected(block.id);
+                }}
+                onMouseOver={(e: React.SyntheticEvent) => {
+                    e.stopPropagation();
+                    designer.setHovered(block.id);
+                }}
+                onMouseLeave={(e: React.SyntheticEvent) => {
+                    e.stopPropagation();
+                    designer.setHovered('');
+                }}
             >
                 {children.map((c) => {
                     return renderBlock(c);
@@ -121,10 +169,83 @@ export const OutlineMenu = observer((): JSX.Element => {
         );
     };
 
+    /**
+     * @returns List of pages
+     * TO-DO: Go from active page from wherever we decide to hold this in state
+     */
+    const pages = useMemo(() => {
+        const blocks = Object.entries(state.blocks);
+        // Filters out pages that are not children of a page
+        return blocks.filter((v) => v[1].widget === 'page' && !v[1].parent);
+    }, [designer.rendered]);
+
+    /**
+     * @returns Children of the Active Page
+     * TO-DO: Go off of Active_Page in depenedency array
+     */
+    const pageBlocks = useMemo(() => {
+        const renderedPage = state.blocks[designer.rendered];
+        const children = [];
+        if (renderedPage) {
+            for (const s in renderedPage.slots) {
+                children.push(...renderedPage.slots[s].children);
+            }
+        }
+
+        return children;
+    }, [designer.rendered, search]);
+
     return (
         <StyledMenu>
             <StyledMenuHeader>
+                <Typography variant="body1">Pages</Typography>
+            </StyledMenuHeader>
+            <div>
+                {pages.length && (
+                    <List>
+                        {pages.map((p: [string, Block]) => {
+                            return (
+                                <StyledListItemButton
+                                    key={p[0]}
+                                    dense={true}
+                                    selected={false}
+                                    hovered={true}
+                                >
+                                    {/* TO-DO: Display link of active page */}
+                                    <List.ItemText primary={p[1].id} />
+                                    <StyledIcon>
+                                        <Home />
+                                    </StyledIcon>
+                                </StyledListItemButton>
+                            );
+                        })}
+                    </List>
+                )}
+            </div>
+
+            <StyledMenuHeader>
                 <Typography variant="body1">Layers</Typography>
+                <StyledCollapseSearch>
+                    <Collapse orientation="horizontal" in={showSearch}>
+                        <TextField
+                            placeholder="Search"
+                            size="small"
+                            value={search}
+                            variant="outlined"
+                            onChange={(e) => setSearch(e.target.value)}
+                        />
+                    </Collapse>
+                    <IconButton
+                        color="default"
+                        size="small"
+                        onClick={() => {
+                            setShowSearch(!showSearch);
+                            setSearch('');
+                        }}
+                    >
+                        <SearchOutlined fontSize="medium" />
+                    </IconButton>
+                </StyledCollapseSearch>
             </StyledMenuHeader>
             <Divider />
             <StyledMenuScroll>
@@ -155,7 +276,10 @@ export const OutlineMenu = observer((): JSX.Element => {
                         </StyledIcon>
                     }
                 >
-                    {renderBlock(designer.rendered)}
+                    {pageBlocks.length &&
+                        pageBlocks.map((b: string) => {
+                            return renderBlock(b);
+                        })}
                 </TreeView>
             </StyledMenuScroll>
         </StyledMenu>
