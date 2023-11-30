e48a362854f1302ddbbc07053d4154fbbe89f692
Kyle Zifrony <kzifrony@deloitte.com>
2023-11-30 12:17:13 -0500

    fix: remove blocks package

--- packages/blocks/src/components/blocks/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './Blocks';
-export * from './Error';
-export * from './Loading';
-export * from './Unauthorized';
-export * from './Renderer';
-export * from './Slot';
--- packages/blocks/src/stores/state/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './state.store';
-export * from './state.actions';
-export * from './state.types';
