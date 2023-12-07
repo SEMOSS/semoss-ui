0c91f3bd13aca88e61185764a98ae0f552cd0b18
Cassandra Stone <casstone@deloitte.com>
2023-12-07 14:01:30 -0500

    feat: body block files

---
+++ packages/client/src/components/block-defaults/body-block/config.tsx
@@ -0,0 +1,73 @@
+import { BlockConfig } from '@/stores';
+import { BorderBottom } from '@mui/icons-material';
+
+import {
+    buildLayoutSection,
+    buildColorSection,
+    buildTypographySection,
+    buildDimensionsSection,
+} from '../block-defaults.shared';
+
+import { BodyBlockDef, BodyBlock } from './BodyBlock';
+import { BLOCK_TYPE_LAYOUT } from '../block-defaults.constants';
+import { SelectInputSettings } from '@/components/block-settings/shared/SelectInputSettings';
+
+// export the config for the block
+export const config: BlockConfig<BodyBlockDef> = {
+    widget: 'body',
+    type: BLOCK_TYPE_LAYOUT,
+    data: {
+        style: {
+            height: '80%',
+            width: '100%',
+        },
+    },
+    listeners: {},
+    slots: {
+        content: [],
+    },
+    render: BodyBlock,
+    icon: BorderBottom,
+    contentMenu: [],
+    styleMenu: [
+        buildDimensionsSection(),
+        buildLayoutSection(),
+        // root bodys don't get margin for spacing
+        {
+            name: 'Spacing',
+            children: [
+                {
+                    description: 'Padding',
+                    render: ({ id }) => (
+                        <SelectInputSettings
+                            id={id}
+                            path="style.padding"
+                            label="Padding"
+                            options={[
+                                {
+                                    value: '1rem',
+                                    display: 'Small',
+                                },
+                                {
+                                    value: '2rem',
+                                    display: 'Medium',
+                                },
+                                {
+                                    value: '3rem',
+                                    display: 'Large',
+                                },
+                                {
+                                    value: '4rem',
+                                    display: 'X-Large',
+                                },
+                            ]}
+                            allowUnset
+                        />
+                    ),
+                },
+            ],
+        },
+        buildColorSection(),
+        buildTypographySection(),
+    ],
+};
