1845b72e750a92eb10eaa6eba5f1c76d56af3c01
Allan Hillman <ahillman@deloitte.com>
2024-01-03 16:40:44 -0600

    fix: hide custom blocks section on UI

--- packages/client/src/components/block-defaults/block-defaults.constants.tsx
+++ packages/client/src/components/block-defaults/block-defaults.constants.tsx
@@ -5,7 +5,7 @@ export const BLOCK_TYPE_DATA = 'data';
 export const BLOCK_TYPE_INPUT = 'input';
 export const BLOCK_TYPE_ACTION = 'action';
 export const BLOCK_TYPE_CHART = 'chart';
-export const BLOCK_TYPE_CUSTOM = 'custom';
+// export const BLOCK_TYPE_CUSTOM = 'custom';
 
 export const BLOCK_TYPES = [
     BLOCK_TYPE_LAYOUT,
@@ -14,5 +14,5 @@ export const BLOCK_TYPES = [
     BLOCK_TYPE_DATA,
     BLOCK_TYPE_INPUT,
     BLOCK_TYPE_ACTION,
-    BLOCK_TYPE_CUSTOM,
+    // BLOCK_TYPE_CUSTOM,
 ];
