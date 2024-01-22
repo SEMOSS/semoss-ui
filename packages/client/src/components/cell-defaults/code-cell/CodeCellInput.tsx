ef060791acd94bdc1cce1d7846c7b968f2505bf0
Cassandra Stone <casstone@deloitte.com>
2024-01-22 13:08:22 -0500

    feat: dynamic monaco editor height for code cell

--- packages/client/src/components/cell-defaults/code-cell/CodeCellInput.tsx
+++ packages/client/src/components/cell-defaults/code-cell/CodeCellInput.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react';
 import Editor from '@monaco-editor/react';
 import { styled } from '@semoss/ui';
 
@@ -8,11 +9,15 @@ import { CodeCellDef } from './config';
 const StyledContent = styled('div')(() => ({
     position: 'relative',
     display: 'flex',
-    height: '150px',
     overflow: 'hidden',
 }));
 
+const EditorLineHeight = 19;
+
 export const CodeCellInput: CellComponent<CodeCellDef> = (props) => {
+    const editorRef = useRef(null);
+    const [editorHeight, setEditorHeight] = useState<number>(null);
+
     const { step } = props;
     const { state } = useBlocks();
 
@@ -25,11 +30,65 @@ export const CodeCellInput: CellComponent<CodeCellDef> = (props) => {
         }
     };
 
+    const handleMount = (editor, monaco) => {
+        // first time you set the height based on content Height
+        editorRef.current = editor;
+        const contentHeight = editor.getContentHeight();
+        setEditorHeight(contentHeight);
+        // update the action
+        editor.addAction({
+            id: 'run',
+            label: 'Run',
+            keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter],
+            run: (editor) => {
+                const newValue = editor.getValue();
+
+                // update with the new code
+                state.dispatch({
+                    message: ActionMessages.UPDATE_STEP,
+                    payload: {
+                        queryId: step.query.id,
+                        stepId: step.id,
+                        path: 'parameters.code',
+                        value: newValue,
+                    },
+                });
+
+                state.dispatch({
+                    message: ActionMessages.RUN_STEP,
+                    payload: {
+                        queryId: step.query.id,
+                        stepId: step.id,
+                    },
+                });
+            },
+        });
+    };
+
+    const handleChange = (newValue: string) => {
+        setEditorHeight(
+            editorRef.current.getModel().getLineCount() * EditorLineHeight,
+        );
+        if (step.isLoading) {
+            return;
+        }
+
+        state.dispatch({
+            message: ActionMessages.UPDATE_STEP,
+            payload: {
+                queryId: step.query.id,
+                stepId: step.id,
+                path: 'parameters.code',
+                value: newValue,
+            },
+        });
+    };
+
     return (
         <StyledContent>
             <Editor
                 width="100%"
-                height="100%"
+                height={editorHeight}
                 value={step.parameters.code}
                 language={getEditorLanguage(step.parameters.type)}
                 options={{
@@ -37,54 +96,11 @@ export const CodeCellInput: CellComponent<CodeCellDef> = (props) => {
                     readOnly: false,
                     minimap: { enabled: false },
                     automaticLayout: true,
+                    scrollBeyondLastLine: false,
+                    lineHeight: EditorLineHeight,
                 }}
-                onChange={(newValue) => {
-                    if (step.isLoading) {
-                        return;
-                    }
-
-                    state.dispatch({
-                        message: ActionMessages.UPDATE_STEP,
-                        payload: {
-                            queryId: step.query.id,
-                            stepId: step.id,
-                            path: 'parameters.code',
-                            value: newValue,
-                        },
-                    });
-                }}
-                onMount={(editor, monaco) => {
-                    // update the action
-                    editor.addAction({
-                        id: 'run',
-                        label: 'Run',
-                        keybindings: [
-                            monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter,
-                        ],
-                        run: (editor) => {
-                            const newValue = editor.getValue();
-
-                            // update with the new code
-                            state.dispatch({
-                                message: ActionMessages.UPDATE_STEP,
-                                payload: {
-                                    queryId: step.query.id,
-                                    stepId: step.id,
-                                    path: 'parameters.code',
-                                    value: newValue,
-                                },
-                            });
-
-                            state.dispatch({
-                                message: ActionMessages.RUN_STEP,
-                                payload: {
-                                    queryId: step.query.id,
-                                    stepId: step.id,
-                                },
-                            });
-                        },
-                    });
-                }}
+                onChange={handleChange}
+                onMount={handleMount}
             />
         </StyledContent>
     );
