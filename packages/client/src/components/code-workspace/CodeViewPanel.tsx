a8be0ff8cd7da779c9abf1522402c1a6fb02520b
Arash Afghahi <aafghahi@deloitte.com>
2024-01-29 12:47:00 -0500

    fix(client): add update code zip back

--- packages/client/src/components/code-workspace/CodeViewPanel.tsx
+++ packages/client/src/components/code-workspace/CodeViewPanel.tsx
@@ -1,30 +1,8 @@
 import React from 'react';
-import {
-    Button,
-    IconButton,
-    FileDropzone,
-    useNotification,
-    Typography,
-    Stack,
-    styled,
-} from '@semoss/ui';
-import {
-    Code,
-    CodeOff,
-    Download,
-    Settings,
-    PersonAdd,
-} from '@mui/icons-material';
-import { Controller, useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
+import { useNotification, styled } from '@semoss/ui';
 
 import { useWorkspace, useRootStore } from '@/hooks';
-import { SettingsContext } from '@/contexts';
 import { AppEditor } from '@/components/common';
-import { MembersTable, SettingsTiles } from '@/components/settings';
-
-import { AppSettings } from '../app/AppSettings';
-import { Card } from '@mui/material';
 
 const StyledContainer = styled('div')(({ theme }) => ({
     display: 'flex',
@@ -50,99 +28,18 @@ const StyledTopLeft = styled('div')(({ theme }) => ({
     width: '100%',
 }));
 
-const StyledTopLeftContent = styled('div')(({ theme }) => ({
-    display: 'flex',
-    width: '100%',
-    gap: theme.spacing(2),
-    padding: theme.spacing(2),
-}));
-
-type EditAppForm = {
-    PROJECT_UPLOAD: File;
-};
-
 interface CodeViewPanelProps {
     editorView: 'settings' | 'permissions' | 'code-editor';
     width: string;
 }
 
 export const CodeViewPanel = (props: CodeViewPanelProps) => {
-    const { editorView, width } = props;
+    const { width } = props;
 
-    const { monolithStore, configStore } = useRootStore();
+    const { monolithStore } = useRootStore();
     const notification = useNotification();
-    const navigate = useNavigate();
 
     const { workspace } = useWorkspace();
-
-    const { handleSubmit, control } = useForm<EditAppForm>({
-        defaultValues: {
-            PROJECT_UPLOAD: null,
-        },
-    });
-
-    /**
-     * Method that is called to create the app
-     */
-    const editApp = handleSubmit(async (data: EditAppForm) => {
-        // turn on loading
-        workspace.setLoading(true);
-
-        try {
-            const path = 'version/assets/';
-
-            // upnzip the file in the new app
-            await monolithStore.runQuery(
-                `DeleteAsset(filePath=["${path}"], space=["${workspace.appId}"]);`,
-            );
-
-            // upload the file
-            const upload = await monolithStore.uploadFile(
-                [data.PROJECT_UPLOAD],
-                configStore.store.insightID,
-                workspace.appId,
-                path,
-            );
-
-            // upnzip the file in the new app
-            await monolithStore.runQuery(
-                `UnzipFile(filePath=["${`${path}${upload[0].fileName}`}"], space=["${
-                    workspace.appId
-                }"]);`,
-            );
-
-            // Load the insight classes
-            await monolithStore.runQuery(
-                `ReloadInsightClasses('${workspace.appId}');`,
-            );
-
-            // set the app portal
-            await monolithStore.setProjectPortal(
-                false,
-                workspace.appId,
-                true,
-                'public',
-            );
-
-            // Publish the app the insight classes
-            await monolithStore.runQuery(
-                `PublishProject('${workspace.appId}', release=true);`,
-            );
-
-            // close it
-            // refreshApp();
-        } catch (e) {
-            console.error(e);
-
-            notification.add({
-                color: 'error',
-                message: e.message,
-            });
-        } finally {
-            // turn of loading
-            workspace.setLoading(false);
-        }
-    });
 
     /**
      * TODO Reusability
