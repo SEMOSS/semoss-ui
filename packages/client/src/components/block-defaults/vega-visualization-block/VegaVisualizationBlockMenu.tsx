eeeec96dfeab888b11a91a265fa4f1efbf7584b1
Baxter <johbaxter@deloitte.com>
2024-03-01 12:13:05 -0500

    fix(client): remove ai generation piece on charts other than general vega viz

--- packages/client/src/components/block-defaults/vega-visualization-block/VegaVisualizationBlockMenu.tsx
+++ packages/client/src/components/block-defaults/vega-visualization-block/VegaVisualizationBlockMenu.tsx
@@ -4,20 +4,24 @@ import {
     AIGenerationSettings,
     JsonSettings,
 } from '@/components/block-settings';
+import { useBlock } from '@/hooks';
 
 export const VegaVisualizationBlockMenu: BlockComponent = ({ id }) => {
+    const { data } = useBlock(id);
     return (
         <Stack padding={2} height="100%">
             <JsonSettings id={id} path="specJson" />
-            <AIGenerationSettings
-                id={id}
-                path="specJson"
-                appendPrompt={
-                    'Use vega lite version 5 and make the schema as simple as possible. Return the response as JSON. Ensure "data" is a top-level key in the JSON object.'
-                }
-                placeholder="Ex: Generate a bar graph."
-                valueAsObject
-            />
+            {!data.variation && (
+                <AIGenerationSettings
+                    id={id}
+                    path="specJson"
+                    appendPrompt={
+                        'Use vega lite version 5 and make the schema as simple as possible. Return the response as JSON. Ensure "data" is a top-level key in the JSON object.'
+                    }
+                    placeholder="Ex: Generate a bar graph."
+                    valueAsObject
+                />
+            )}
         </Stack>
     );
 };
