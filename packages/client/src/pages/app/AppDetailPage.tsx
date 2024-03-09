7ac645defaf1d74f1f2334117b1115c7bf165fde
Evan Walter <evwalter@deloitte.com>
2024-03-08 18:07:25 -0800

    feat: style the top buttons

--- packages/client/src/pages/app/AppDetailPage.tsx
+++ packages/client/src/pages/app/AppDetailPage.tsx
@@ -1,14 +1,53 @@
 import { useEffect, useState } from 'react';
 import { Link, useParams, useNavigate } from 'react-router-dom';
-import { observer } from 'mobx-react-lite';
-import { Breadcrumbs, useNotification } from '@semoss/ui';
-import { AppSettings } from '@/components/app';
-import { Sidebar, SidebarItem, SidebarText } from '@/components/common';
-import { LoadingScreen } from '@/components/ui';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import {
+    Breadcrumbs,
+    Button,
+    ButtonGroup,
+    IconButton,
+    styled,
+    useNotification,
+} from '@semoss/ui';
+import { SettingsTiles } from '@/components/settings/SettingsTiles';
+import { AppSettings } from '@/components/app/AppSettings';
 import { useRootStore } from '@/hooks';
-import { WorkspaceStore } from '@/stores';
 
-export const AppDetailPage = observer(() => {
+const TopButtonsContainer = styled('div')({
+    display: 'flex',
+    gap: '0.25rem',
+    marginLeft: 'auto',
+});
+
+const StyledTextButton = styled(Button)(({ theme }) => ({
+    fontWeight: 'bold',
+}));
+
+const StyledArrowDropDownIcon = styled(ArrowDropDownIcon)({
+    display: 'flex',
+    alignItems: 'center',
+});
+
+const Sidebar = styled('div')({
+    display: 'flex',
+    borderRight: 'solid',
+    flexDirection: 'column',
+    fontWeight: 'bold',
+    gap: '1rem',
+    overflow: 'scroll',
+});
+
+const SidebarLink = styled(Link)({
+    color: 'inherit',
+    fontWeight: 'bold',
+    textDecoration: 'none',
+    '&:visited': {
+        color: 'inherit',
+    },
+});
+
+export function AppDetailPage() {
     // App ID Needed for pixel calls
     const { appId } = useParams();
     const { configStore } = useRootStore();
@@ -16,73 +55,38 @@ export const AppDetailPage = observer(() => {
     const notification = useNotification();
     const navigate = useNavigate();
 
-    const [workspace, setWorkspace] = useState<WorkspaceStore>(undefined);
-
-    useEffect(() => {
-        // Clear out the old app
-        setWorkspace(undefined);
-
-        configStore
-            .createWorkspace(appId)
-            .then((loadedWorkspace) => {
-                setWorkspace(loadedWorkspace);
-            })
-            .catch((e) => {
-                notification.add({
-                    color: 'error',
-                    message: e.message,
-                });
-
-                navigate('/');
-            });
-    }, [appId]);
-
-    // Hide the screen while it loads
-    if (!workspace) {
-        return <LoadingScreen.Trigger description="Initializing app" />;
-    }
-
     return (
-        <div
-            style={{
-                width: '100%',
-                display: 'flex',
-                alignSelf: 'stretch',
-                flexDirection: 'column',
-                alignItems: 'flex-start',
-                gap: 2,
-            }}
-        >
+        <Sidebar>
             <Breadcrumbs>Breadcrumbs</Breadcrumbs>
+            <TopButtonsContainer>
+                <StyledTextButton variant="text">
+                    Change Access
+                </StyledTextButton>
+                <ButtonGroup>
+                    <Button variant="contained">Open</Button>
+                    <Button variant="contained">
+                        <StyledArrowDropDownIcon />
+                    </Button>
+                </ButtonGroup>
+                {/* <div style={{ width: '50rem' }}> */}
+                <IconButton>
+                    <MoreVertIcon />
+                </IconButton>
+                {/* </div> */}
+            </TopButtonsContainer>
             <div
                 style={{
                     display: 'flex',
-                    gap: 2,
-                    marginLeft: 'auto',
                 }}
             >
-                <pre>Change Access Button</pre>
-                <pre>Open Button</pre>
-            </div>
-            <div
-                style={{
-                    display: 'flex',
-                }}
-            >
-                <div
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        fontWeight: 'bold',
-                    }}
-                >
-                    <Link to="main-uses">Main Uses</Link>
-                    <Link to="tags">Tags</Link>
-                    <Link to="videos">Videos</Link>
-                    <Link to="dependencies">Dependencies</Link>
-                    <Link to="app-access">App Access</Link>
-                    <Link to="member-access">Member Access</Link>
-                </div>
+                <Sidebar>
+                    <SidebarLink to="#main-uses">Main Uses</SidebarLink>
+                    <SidebarLink to="#tags">Tags</SidebarLink>
+                    <SidebarLink to="#videos">Videos</SidebarLink>
+                    <SidebarLink to="#dependencies">Dependencies</SidebarLink>
+                    <SidebarLink to="#app-access">App Access</SidebarLink>
+                    <SidebarLink to="#member-access">Member Access</SidebarLink>
+                </Sidebar>
                 <div
                     style={{
                         display: 'flex',
@@ -96,67 +100,20 @@ export const AppDetailPage = observer(() => {
                     <pre id="#videos">Video</pre>
                     <pre id="#dependencies">Dependencies (reactor call)</pre>
                     <pre id="#member-access">Member Access</pre>
-                    <AppSettings id={appId} />
+                    {/* <AppSettings id={appId} /> */}
+                    {/* <SettingsTiles
+                        id={appId}
+                        mode={'app'}
+                        name={'app'}
+                        onDelete={() => {
+                            navigate('..', { relative: 'path' });
+                        }}
+                    /> */}
                     <pre id="#app-access">
                         App Access section (components from Settings)
                     </pre>
                 </div>
             </div>
-        </div>
+        </Sidebar>
     );
-});
-
-// import { useParams, useNavigate } from 'react-router-dom';
-// import { observer } from 'mobx-react-lite';
-// import { SettingsTiles } from '@/components/settings';
-// import { SettingsContext } from '@/contexts';
-// import { useRootStore, useWorkspace } from '@/hooks';
-// import {
-//     Container,
-//     styled,
-//     ToggleTabsGroup,
-//     useNotification,
-// } from '@semoss/ui';
-
-// export const AppDetailPage = observer(() => {
-//     // App ID Needed for pixel calls
-//     const { appId } = useParams();
-//     const { configStore } = useRootStore();
-//     const notification = useNotification();
-//     const navigate = useNavigate();
-//     const { workspace } = useWorkspace();
-
-//     return (
-//         <>
-//             <SettingsContext.Provider
-//                 value={{
-//                     adminMode: false,
-//                 }}
-//             >
-//                 <Container
-//                     maxWidth={'xl'}
-//                     sx={{
-//                         height: '100%',
-//                         display: 'flex',
-//                         flexDirection: 'column',
-//                         gap: '16px',
-//                         overflowX: 'hidden',
-//                         overflowY: 'auto',
-//                     }}
-//                 >
-//                     <div>AppDetailPage</div>
-//                     {workspace.role === 'OWNER' ? (
-//                         <SettingsTiles
-//                             mode={'app'}
-//                             name={'app'}
-//                             id={workspace.appId}
-//                             onDelete={() => {
-//                                 navigate('/settings/app');
-//                             }}
-//                         />
-//                     ) : null}
-//                 </Container>
-//             </SettingsContext.Provider>
-//         </>
-//     );
-// });
+}
