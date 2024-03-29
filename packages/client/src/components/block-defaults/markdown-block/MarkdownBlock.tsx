fbc36c06b6f4ce702e88cf3bb6cd2555aa2617f0
Cassandra Stone <casstone@deloitte.com>
2024-03-29 14:16:41 -0400

    feat: update react-markdown and add remark plugin

--- packages/client/src/components/block-defaults/markdown-block/MarkdownBlock.tsx
+++ packages/client/src/components/block-defaults/markdown-block/MarkdownBlock.tsx
@@ -1,6 +1,7 @@
 import { CSSProperties } from 'react';
 import { observer } from 'mobx-react-lite';
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import { useBlock } from '@/hooks';
 import { BlockDef, BlockComponent } from '@/stores';
 
@@ -23,7 +24,9 @@ export const MarkdownBlock: BlockComponent = observer(({ id }) => {
             }}
             {...attrs}
         >
-            <ReactMarkdown>{data.markdown}</ReactMarkdown>
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                {data.markdown}
+            </ReactMarkdown>
         </div>
     );
 });
