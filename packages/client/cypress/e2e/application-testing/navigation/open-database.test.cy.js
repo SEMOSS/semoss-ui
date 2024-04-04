8ba172a9a03b2578e496ebd3e0431f0d763c8a6f
Trevor Zylks <tzylks@deloitte.com>
2024-04-04 10:19:34 -0700

    feat(ui/client): added tests and removed unused test folders

---
+++ packages/client/cypress/e2e/application-testing/navigation/open-database.test.cy.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+describe('Login process', () => {
+    beforeEach(() => {
+        // Cypress starts out with a blank slate for each test
+        // so we must tell it to visit our website with the `cy.visit()` command.
+        // Since we want to visit the same URL at the start of all our tests,
+        // we include it in our beforeEach function so that it runs before each test
+        cy.visit(
+            'http://localhost:9090/semoss-ui/packages/client/dist/#/login',
+        );
+
+        cy.contains('Username');
+
+        cy.get('#mui-1').type(`${Cypress.env('user')}`);
+
+        cy.get('#mui-2').type(`${Cypress.env('pass')}`);
+
+        cy.get('.MuiButton-contained').parent().click();
+        cy.get('[data-testid="CloseIcon"]').click();
+    });
+    it('can open function catalog', () => {
+        cy.get('[data-testid="Database-icon"]').click();
+        cy.contains('Database Catalog').should('exist');
+    });
+});
