439ae20a2e84627e07c7673d3f3469644ed8084c
Evan Walter <evwalter@deloitte.com>
2024-04-05 17:03:30 -0700

    feat(client): start styling the modal

--- packages/client/src/pages/app/AppDetailPage.tsx
+++ packages/client/src/pages/app/AppDetailPage.tsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 // import { useParams, useNavigate } from 'react-router-dom';
 import { nanoid } from 'nanoid';
 import AssignmentIcon from '@mui/icons-material/Assignment';
+import CloseIcon from '@mui/icons-material/Close';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import HdrAutoIcon from '@mui/icons-material/HdrAuto';
@@ -464,6 +465,36 @@ function Sidebar({ refs }: SidebarProps) {
     );
 }
 
+const EditModalInnerContainer = styled('div')({
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '20px',
+});
+
+const ModalHeaderWrapper = styled('div')({
+    alignItems: 'center',
+    display: 'flex',
+    marginBottom: '1rem',
+    justifyContent: 'space-between',
+});
+
+const ModalHeading = styled(Typography)({
+    fontSize: 20,
+    fontWeight: 500,
+});
+
+const ModalFooter = styled('div')({
+    display: 'flex',
+    gap: '0.5rem',
+    marginLeft: 'auto',
+});
+
+const ModalSectionHeading = styled(Typography)({
+    fontSize: 16,
+    fontWeight: 500,
+    margin: '1rem 0 0.5rem 0',
+});
+
 interface EditDetailsModalProps {
     isOpen: boolean;
     onClose: () => void;
@@ -476,12 +507,33 @@ function EditDetailsModal({
     runSetMainUses,
 }: EditDetailsModalProps) {
     return (
-        <Modal open={isOpen} fullWidth>
-            <Typography variant="h3">Edit App Details</Typography>
-            <Button onClick={() => runSetMainUses()}>
-                <pre>set test main uses</pre>
-            </Button>
-            <Button onClick={() => onClose()}>Cancel</Button>
+        <Modal open={true} fullWidth>
+            <EditModalInnerContainer>
+                <ModalHeaderWrapper>
+                    <ModalHeading variant="h2">Edit App Details</ModalHeading>
+                    <IconButton onClick={() => onClose()}>
+                        <CloseIcon />
+                    </IconButton>
+                </ModalHeaderWrapper>
+
+                <ModalSectionHeading variant="h3">
+                    Main Uses
+                </ModalSectionHeading>
+                <Button onClick={() => runSetMainUses()}>
+                    <pre>set test main uses</pre>
+                </Button>
+
+                <ModalSectionHeading variant="h3">Tags</ModalSectionHeading>
+
+                <ModalFooter>
+                    <Button onClick={() => onClose()} variant="text">
+                        Cancel
+                    </Button>
+                    <Button onClick={null} variant="contained">
+                        Save
+                    </Button>
+                </ModalFooter>
+            </EditModalInnerContainer>
         </Modal>
     );
 }
