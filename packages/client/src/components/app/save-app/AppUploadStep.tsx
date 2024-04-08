8b5c07b221c51d6860d1ebe612e2c273f1637880
Hock <ghock@deloitte.com>
2024-04-08 12:01:20 -0400

    feat(app modal): updates add app modal

--- packages/client/src/components/app/save-app/AppUploadStep.tsx
+++ packages/client/src/components/app/save-app/AppUploadStep.tsx
@@ -1,25 +1,73 @@
 import { FileDropzone } from '@semoss/ui';
 import { Control, Controller } from 'react-hook-form';
-import { ADD_APP_FORM_FIELD_UPLOAD } from './save-app.constants';
+import {
+    ADD_APP_FORM_FIELD_UPLOAD,
+    ADD_APP_FORM_FIELD_TYPE,
+} from './save-app.constants';
+import { KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material';
+import { Stack, Accordion, Select } from '@semoss/ui';
+import { Dispatch, SetStateAction, useState } from 'react';
+import { AddAppFormStep } from './AddAppModal';
 
-export const AppUploadStep = (props: { control: Control<any, any> }) => {
+export const AppUploadStep = (props: {
+    control: Control<any, any>;
+    setAddAppFormSteps: Dispatch<SetStateAction<AddAppFormStep[]>>;
+    defaultAddAppFormStep: AddAppFormStep[];
+    assetAddAppFormStep: AddAppFormStep[];
+}) => {
+    const {
+        control,
+        setAddAppFormSteps,
+        defaultAddAppFormStep,
+        assetAddAppFormStep,
+    } = props;
+
     return (
-        <Controller
-            name={ADD_APP_FORM_FIELD_UPLOAD}
-            control={props.control}
-            rules={{ required: true }}
-            render={({ field }) => {
-                return (
-                    <FileDropzone
-                        multiple={false}
-                        value={field.value}
-                        onChange={(newValues) => {
-                            field.onChange(newValues);
-                        }}
-                        extensions={['.zip']}
-                    />
-                );
-            }}
-        />
+        <Stack direction="column">
+            <Controller
+                name={ADD_APP_FORM_FIELD_UPLOAD}
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => {
+                    return (
+                        <FileDropzone
+                            multiple={false}
+                            value={field.value}
+                            onChange={(newValues) => {
+                                field.onChange(newValues);
+                            }}
+                            extensions={['.zip']}
+                        />
+                    );
+                }}
+            />
+            <Controller
+                name={ADD_APP_FORM_FIELD_TYPE}
+                control={control}
+                render={({ field }) => {
+                    return (
+                        <Select
+                            label="Folder Type"
+                            value={field.value}
+                            defaultValue="App Zip"
+                            onChange={(value) => {
+                                field.onChange(value);
+                                setAddAppFormSteps(
+                                    value.target.value === 'App Zip'
+                                        ? defaultAddAppFormStep
+                                        : assetAddAppFormStep,
+                                );
+                            }}
+                        >
+                            {['App Zip', 'Project Zip'].map((option, idx) => (
+                                <Select.Item key={idx} value={option}>
+                                    {option}
+                                </Select.Item>
+                            ))}
+                        </Select>
+                    );
+                }}
+            />
+        </Stack>
     );
 };
--- packages/client/src/components/app/save-app/save-app.constants.ts
+++ packages/client/src/components/app/save-app/save-app.constants.ts
@@ -3,3 +3,4 @@ export const ADD_APP_FORM_FIELD_NAME = 'name';
 export const ADD_APP_FORM_FIELD_DESCRIPTION = 'description';
 export const ADD_APP_FORM_FIELD_TAGS = 'tags';
 export const ADD_APP_FORM_FIELD_IS_GLOBAL = 'global';
+export const ADD_APP_FORM_FIELD_TYPE = 'type';
