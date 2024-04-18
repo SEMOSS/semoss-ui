07f675720cb52d899c7bd302bea6795d8bb43996
Baxter <johbaxter@deloitte.com>
2024-04-18 11:44:47 -0400

    fix(client): png issue

---
+++ packages/client/src/components/app/app.images.ts
@@ -0,0 +1,34 @@
+//Blocks
+import UPDATED_BLOCKS from '@/assets/img/updated_blocks.png';
+
+//Code
+import UPDATED_CODE from '@/assets/img/updated_code.png';
+
+//Insight
+import UPDATED_INSIGHT from '@/assets/img/updated_insight.png';
+
+//Default
+import UPDATED_FORMS from '@/assets/img/updated_forms.png';
+
+export const APP_IMAGES = {
+    BLOCKS: {
+        image: UPDATED_BLOCKS,
+    },
+    CODE: {
+        image: UPDATED_CODE,
+    },
+    INSIGHT: {
+        image: UPDATED_INSIGHT,
+    },
+    '': {
+        image: UPDATED_FORMS,
+    },
+    SYSTEM: {
+        bi: {
+            image: UPDATED_BLOCKS,
+        },
+        terminal: {
+            image: UPDATED_CODE,
+        },
+    },
+};
