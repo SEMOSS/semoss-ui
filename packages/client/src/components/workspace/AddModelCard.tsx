5fb394347ccb1049bea1adc3ab5fa7022852a2ea
Katie Gorbell <kgorbell@deloitte.com>
2024-04-26 10:35:06 -0500

    style(client): styles updated for addModelCard

--- packages/client/src/components/workspace/AddModelCard.tsx
+++ packages/client/src/components/workspace/AddModelCard.tsx
@@ -1,15 +1,43 @@
 import { Add } from '@mui/icons-material';
-import { Card, IconButton, Typography } from '@semoss/ui';
+import { styled, Card, IconButton, Typography } from '@semoss/ui';
+
+const StyledCard = styled(Card)(({ theme }) => ({
+    width: '362px',
+    height: '116px',
+    display: 'flex',
+    justifyContent: 'center',
+}));
+
+const StyledContent = styled(Card.Content)(({ theme }) => ({
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 0,
+    ':last-child': {
+        paddingBottom: 0,
+    },
+}));
+
+const StyledIconButton = styled(IconButton)(({ theme }) => ({
+    backgroundColor: theme.palette.primary.selected,
+    '&:hover': {
+        backgroundColor: theme.palette.primary.selected,
+    },
+}));
 
 export const AddModelCard = () => {
     return (
-        <Card>
-            <div>
-                <IconButton color="secondary">
+        <StyledCard>
+            <StyledContent>
+                <StyledIconButton color="primary">
                     <Add color="inherit" />
-                </IconButton>
-                <Typography variant="body2">Add Model</Typography>
-            </div>
-        </Card>
+                </StyledIconButton>
+                <Typography variant="body2" color="primary">
+                    Add Model
+                </Typography>
+            </StyledContent>
+        </StyledCard>
     );
 };
