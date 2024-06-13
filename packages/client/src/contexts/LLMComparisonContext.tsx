b42954439de9e89bd5d55614ecb1885e1cf95a1f
Katie Gorbell <kgorbell@deloitte.com>
2024-06-13 15:55:20 -0500

    feat(client): removed selectedVariants, and updated types for variants and models

--- packages/client/src/contexts/LLMComparisonContext.tsx
+++ packages/client/src/contexts/LLMComparisonContext.tsx
@@ -1,48 +1,24 @@
 import { createContext } from 'react';
-
-export interface ModelVariant {
-    alias: string;
-    value: string;
-    database_name: string;
-    database_subtype: string;
-    database_type: string;
-
-    name?: string;
-    icon?: string;
-    topP?: number;
-    temperature?: number;
-    length?: number;
-}
-
-export interface DefaultModelVariant extends ModelVariant {
-    to: string;
-    type: string;
-}
+import { TypeVariant, TypeLlmConfig } from '@/components/workspace';
 
 export type LLMComparisonContextType = {
     /** the variants we want to test on this app */
-    variants: (ModelVariant | null)[][];
-
-    /** selected variants for comparison */
-    selectedVariants: (ModelVariant | null)[][];
+    variants: TypeVariant[];
 
     /** default variant in app */
-    defaultVariant: DefaultModelVariant[];
+    defaultVariant: TypeVariant;
 
     /** Adds Variant to context */
-    addNewVariant: (index: number | (ModelVariant | null)[]) => void;
+    addNewVariant: (index: number | TypeVariant) => void;
 
     /** Delete Variant set in context */
     deleteVariant: (index: number) => void;
-
-    /** Sets the selected variants */
-    setSelectedVariants: (variants: ModelVariant[][]) => void;
 
     /** Swap function of variant model */
     swapVariantModel: (
         variantIndex: number,
         modelIndex: number,
-        model: ModelVariant,
+        model: TypeLlmConfig,
     ) => void;
 };
 
