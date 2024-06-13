ac50f72d5a000a4305133bd99fc59f939cbdccc8
Katie Gorbell <kgorbell@deloitte.com>
2024-06-13 17:15:16 -0500

    feat(client): users can tab between the variants and A/B testing tabs in the testing panel

--- packages/client/src/components/workspace/llm-compare/ABTestingPanel.tsx
+++ packages/client/src/components/workspace/llm-compare/ABTestingPanel.tsx
@@ -1,13 +1,28 @@
-import { ToggleTabsGroup, Typography } from '@semoss/ui';
+import { useLLMComparison } from '@/hooks';
+import { Fragment } from 'react';
+import { PlayArrow } from '@mui/icons-material';
+import { styled, ToggleTabsGroup, Typography, Button } from '@semoss/ui';
 import { useState } from 'react';
+import { ModelVariant } from './ModelVariant';
+
+const StyledABTestingPanel = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: theme.spacing(2),
+}));
 
 type Mode = 'variants' | 'abTesting';
 
 export const ABTestingPanel = () => {
     const [mode, setMode] = useState<Mode>('variants');
+    const { defaultVariant, variants } = useLLMComparison();
+
+    const handleRerunVariants = () => {
+        // TODO: reruns and saves the selected variants/edits to generated the report
+    };
 
     return (
-        <div>
+        <StyledABTestingPanel>
             <Typography variant="h6">Evaluate</Typography>
 
             <ToggleTabsGroup
@@ -18,13 +33,46 @@ export const ABTestingPanel = () => {
                 <ToggleTabsGroup.Item label="A/B Testing" value="abTesting" />
             </ToggleTabsGroup>
 
-            {mode === 'variants' && <div>{/* Variants */}</div>}
+            {mode === 'variants' && (
+                <Fragment>
+                    <Button
+                        startIcon={<PlayArrow />}
+                        color="primary"
+                        variant="outlined"
+                        onClick={handleRerunVariants}
+                    >
+                        Rerun
+                    </Button>
+
+                    <ModelVariant
+                        index={-1}
+                        isDefault={true}
+                        variant={defaultVariant}
+                        orientation="column"
+                        hideVariantActions={true}
+                        cardProps={{ size: 'small' }}
+                    />
+
+                    {variants.map((value, idx) => {
+                        return (
+                            <ModelVariant
+                                index={idx}
+                                variant={value}
+                                orientation="column"
+                                hideVariantActions={true}
+                                key={idx}
+                                cardProps={{ size: 'small' }}
+                            />
+                        );
+                    })}
+                </Fragment>
+            )}
 
             {mode === 'abTesting' && (
                 <div>
                     <Typography variant="body1">Select Variants</Typography>
                 </div>
             )}
-        </div>
+        </StyledABTestingPanel>
     );
 };
