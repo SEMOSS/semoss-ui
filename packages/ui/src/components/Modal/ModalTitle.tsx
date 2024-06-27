511e03df87f6723032c9a0ab81ba828817ee0ac8
Katie Gorbell <kgorbell@deloitte.com>
2024-06-27 10:13:21 -0500

    style(ui): modal title padding updated

--- packages/ui/src/components/Modal/ModalTitle.tsx
+++ packages/ui/src/components/Modal/ModalTitle.tsx
@@ -1,17 +1,7 @@
 import { DialogTitle as MuiModalTitle, SxProps, styled } from "@mui/material";
-import { IconButton } from "../../index";
-import { Close } from "@mui/icons-material";
 
-const StyledModalTitle = styled(MuiModalTitle, {
-    shouldForwardProp: (prop) => prop !== "isCloseable",
-})<{ isCloseable?: boolean }>(({ theme, isCloseable }) => ({
+const StyledModalTitle = styled(MuiModalTitle)(({ theme }) => ({
     padding: theme.spacing(2),
-
-    ...(isCloseable && {
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-    }),
 }));
 
 export interface ModalTitleProps {
@@ -24,31 +14,14 @@ export interface ModalTitleProps {
      * The system prop that allows defining system overrides as well as additional CSS styles.
      */
     sx?: SxProps;
-
-    /**
-     * Provides the user with an option to to close the modal via a close icon
-     */
-    onClose?: () => void;
 }
 
 export const ModalTitle = (props: ModalTitleProps) => {
-    const { sx, children, onClose, ...rest } = props;
+    const { sx, children, ...rest } = props;
 
-    if (!!onClose) {
-        return (
-            <StyledModalTitle sx={sx} {...rest} isCloseable={true}>
-                <div>{children}</div>
-
-                <IconButton onClick={onClose}>
-                    <Close />
-                </IconButton>
-            </StyledModalTitle>
-        );
-    } else {
-        return (
-            <StyledModalTitle sx={sx} {...rest}>
-                {children}
-            </StyledModalTitle>
-        );
-    }
+    return (
+        <StyledModalTitle sx={sx} {...rest}>
+            {children}
+        </StyledModalTitle>
+    );
 };
