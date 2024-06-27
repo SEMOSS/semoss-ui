18b08cdf7df6def74db2eb8b1421052ba50968ce
Katie Gorbell <kgorbell@deloitte.com>
2024-06-27 17:06:31 -0500

    feat(client): users can toggle between sub menus of the LLM Comparison block

--- packages/client/src/components/block-defaults/llm-comparison-block/LLMComparisonMenu.tsx
+++ packages/client/src/components/block-defaults/llm-comparison-block/LLMComparisonMenu.tsx
@@ -1,6 +1,42 @@
 import { BlockComponent } from '@/stores';
 import { useBlock } from '@/hooks';
+import { styled, ToggleTabsGroup, Stack } from '@semoss/ui';
+import { useState } from 'react';
+
+const StyledMenu = styled('div')(({ theme }) => ({
+    padding: theme.spacing(2),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: theme.spacing(1),
+}));
+
+type Mode = 'configure' | 'settings';
 
 export const LLMComparisonMenu: BlockComponent = ({ id }) => {
-    return <div></div>;
+    const [mode, setMode] = useState<Mode>('configure');
+
+    return (
+        <StyledMenu>
+            <ToggleTabsGroup
+                value={mode}
+                onChange={(e, val) => setMode(val as Mode)}
+            >
+                <ToggleTabsGroup.Item label="Configure" value="configure" />
+                <ToggleTabsGroup.Item label="Settings" value="settings" />
+            </ToggleTabsGroup>
+
+            {mode === 'configure' && (
+                <Stack direction="column">
+                    <div>Configure Menu</div>
+                </Stack>
+            )}
+
+            {mode === 'settings' && (
+                <Stack direction="column">
+                    <div>Settings Menu</div>
+                </Stack>
+            )}
+        </StyledMenu>
+    );
 };
