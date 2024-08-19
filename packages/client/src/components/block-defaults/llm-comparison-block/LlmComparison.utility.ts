80d679803c71f6984c9ff12af6a520b482ba40b8
Katie Gorbell <kgorbell@deloitte.com>
2024-08-19 16:58:09 -0500

    refactor(client): cleaned up fake/hard coded data for llm comparison

--- packages/client/src/components/block-defaults/llm-comparison-block/LlmComparison.utility.ts
+++ packages/client/src/components/block-defaults/llm-comparison-block/LlmComparison.utility.ts
@@ -57,53 +57,9 @@ export const LlmComparisonFormDefaultValues: TypeLlmComparisonForm = {
     defaultVariant: {
         name: 'default',
         selected: true,
-        models: [
-            {
-                alias: 'llm1',
-                value: 'f7cc988b-8c74-406e-87e3-ba30ca42c6fd',
-                database_name: 'openAI test',
-                database_subtype: 'OPEN_AI',
-                database_type: 'MODEL',
-                topP: 0.2,
-                temperature: 0.7,
-                length: 690,
-            },
-        ],
+        models: [],
     },
-    variants: [
-        {
-            name: 'A',
-            selected: false,
-            models: [
-                {
-                    alias: 'llm Z',
-                    value: '123',
-                    database_name: 'AIC',
-                    database_subtype: 'OPEN_AI',
-                    database_type: 'MODEL',
-                    topP: 0.6,
-                    temperature: 1,
-                    length: 210,
-                },
-            ],
-        },
-        {
-            name: 'B',
-            selected: false,
-            models: [
-                {
-                    alias: 'llm W',
-                    value: '321',
-                    database_name: 'AIC',
-                    database_subtype: 'OPEN_AI',
-                    database_type: 'MODEL',
-                    topP: 0.0,
-                    temperature: 0.3,
-                    length: 865,
-                },
-            ],
-        },
-    ],
+    variants: [],
     designerView: 'allVariants',
     editorVariantIndex: null,
     editorModelIndex: null,
