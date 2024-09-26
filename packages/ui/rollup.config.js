01be2668b6705c22e65cccb11c22af1e5dbab355
Baxter <johbaxter@deloitte.com>
2024-09-26 16:35:10 -0400

    fix(ui): push rollup config back to component lib

---
+++ packages/ui/rollup.config.js
@@ -0,0 +1,39 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+
+import bundleSize from "rollup-plugin-bundle-size";
+import postcss from "rollup-plugin-postcss";
+import del from "rollup-plugin-delete";
+
+import packageJson from "./package.json";
+
+const config = [
+    // Outputs separate declarations files and builds
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: packageJson.main,
+                format: "cjs",
+                sourcemap: true,
+            },
+            {
+                file: packageJson.module,
+                format: "esm",
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            del({ targets: "dist" }),
+            resolve(),
+            commonjs(),
+            typescript({ tsconfig: "./tsconfig.json" }),
+            postcss(),
+            bundleSize(),
+        ],
+        external: ["react", "react-dom"],
+    },
+];
+
+export default config;
