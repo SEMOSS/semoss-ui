fad5fcc27a1504bc21f5863ff2612c5293874288
Katie Gorbell <kgorbell@deloitte.com>
2024-10-04 17:00:58 -0500

    feat(client): llmCellSelect displays llm cells that are variables and sets the variableId in data

--- packages/client/src/components/block-defaults/llm-comparison-block/LLMCellSelect.tsx
+++ packages/client/src/components/block-defaults/llm-comparison-block/LLMCellSelect.tsx
@@ -1,22 +1,8 @@
 import { BaseSettingSection } from '@/components/block-settings/BaseSettingSection';
 import { useBlocks, useBlockSettings } from '@/hooks';
 import { Autocomplete, TextField } from '@mui/material';
-import { styled } from '@semoss/ui';
-import { computed } from 'mobx';
 import { observer } from 'mobx-react-lite';
-import { useRef, useState, useEffect, useMemo } from 'react';
-import { getValueByPath } from '@/utility';
-
-const GroupHeader = styled('div')(({ theme }) => ({
-    position: 'sticky',
-    top: '-8px',
-    padding: '4px 10px',
-    backgroundColor: theme.palette.background.default,
-}));
-
-const GroupItems = styled('ul')(({ theme }) => ({
-    padding: 0,
-}));
+import { useRef, useState, useEffect } from 'react';
 
 interface LLMCellSelectProps {
     // Id of the block
@@ -27,56 +13,32 @@ export const LLMCellSelect = observer(({ id }: LLMCellSelectProps) => {
     const { data, setData } = useBlockSettings(id);
     const { state } = useBlocks();
     const [llmCells, setLlmCells] = useState([]);
-    const [value, setValue] = useState({ queryId: '', cellId: '' });
+    const [value, setValue] = useState('');
     const timeoutRef = useRef<ReturnType<typeof setTimeout>>(null);
 
-    // Get the value of the input
-    const computedValue = useMemo(() => {
-        return computed(() => {
-            if (!data) {
-                return { queryId: '', cellId: '' };
-            }
-
-            const modelledValue = { queryId: '', cellId: '' };
-            const queryId = getValueByPath(data, 'queryId');
-            if (typeof queryId === 'string') {
-                modelledValue.queryId = queryId;
-            }
-            const cellId = getValueByPath(data, 'cellId');
-            if (typeof cellId === 'string') {
-                modelledValue.cellId = cellId;
-            }
-            return modelledValue;
-        });
-    }, [data.cellId, data.queryId]).get();
-
-    // Update the value whenever the computed one changes
+    // update the input value when the variableId is changed
     useEffect(() => {
-        setValue(computedValue);
-    }, [computedValue]);
+        if (typeof data.variableId === 'string') {
+            setValue(data.variableId);
+        }
+    }, [data.variableId]);
 
-    // Filter cells that are not an LLM Cell
+    // Build input options by filtering variables to only be LLM cells
     useEffect(() => {
-        let options = [];
-        Object.keys(state.queries).forEach((name) => {
-            const query = state.queries[name];
-            const filteredCells = Object.values(query.cells).filter(
-                (cell) => cell.widget === 'llm',
-            );
-            const modelledCells = filteredCells.map((cell) => {
-                return {
-                    queryId: query.id,
-                    cellId: cell.id,
-                };
-            });
-            options = [...options, ...modelledCells];
+        const cellOptions = Object.keys(state.variables).filter((key) => {
+            const isCell = state.variables[key].type === 'cell';
+            if (isCell) {
+                const query = state.getQuery(state.variables[key].to);
+                const cell = query.cells[state.variables[key].cellId];
+                return cell?.widget === 'llm';
+            } else {
+                return false;
+            }
         });
-        setLlmCells(options);
-    }, [state.queries]);
+        setLlmCells(cellOptions);
+    }, [state.variables]);
 
-    const onChange = (val) => {
-        setValue(val.cellId);
-
+    const onChange = (val: string) => {
         // clear out the old timeout
         if (timeoutRef.current) {
             clearTimeout(timeoutRef.current);
@@ -85,8 +47,7 @@ export const LLMCellSelect = observer(({ id }: LLMCellSelectProps) => {
 
         timeoutRef.current = setTimeout(() => {
             try {
-                setData('queryId', val.queryId);
-                setData('cellId', val.cellId);
+                setData('variableId', val);
             } catch (e) {
                 console.log(e);
             }
@@ -94,29 +55,20 @@ export const LLMCellSelect = observer(({ id }: LLMCellSelectProps) => {
     };
 
     return (
-        <BaseSettingSection label="LLM Cell">
+        <BaseSettingSection label="Variable">
             <Autocomplete
                 fullWidth
                 disableClearable
                 size="small"
                 value={value}
                 options={llmCells}
-                getOptionLabel={(cell) => cell.cellId || ''}
-                isOptionEqualToValue={(option, val) => {
-                    return option.cellId === val.cellId;
-                }}
-                groupBy={(cell) => cell.queryId}
+                getOptionLabel={(cell) => cell || ''}
                 onChange={(_, newValue) => onChange(newValue)}
                 renderInput={(params) => (
-                    <TextField {...params} placeholder="Cell" />
-                )}
-                renderGroup={(params) => (
-                    <li key={params.key}>
-                        <GroupHeader>{params.group}</GroupHeader>
-                        <GroupItems>{params.children}</GroupItems>
-                    </li>
+                    <TextField {...params} placeholder="Variable" />
                 )}
             />
+            <div></div>
         </BaseSettingSection>
     );
 });
