81680aa39a1aa4764fd8f8b17368b8c408bd2749
soumya pratik <soumyapratik018@gmail.com>
2024-10-10 11:14:22 +0530

    feat(client): fixed typo

--- packages/client/src/components/block-defaults/container-block/config.tsx
+++ packages/client/src/components/block-defaults/container-block/config.tsx
@@ -27,7 +27,6 @@ export const config: BlockConfig<ContainerBlockDef> = {
             flexWrap: 'wrap',
         },
         subcontainer: 2,
-        id: '',
     },
     listeners: {},
     slots: {
