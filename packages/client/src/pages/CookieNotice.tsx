47f637ec2ebdf60bcf07d03984120443fafb0e9c
Katie Gorbell <kgorbell@deloitte.com>
2024-11-01 16:25:52 -0500

    feat(client): cookie and privacy notice pages use the theme to set their content

--- packages/client/src/pages/CookieNotice.tsx
+++ packages/client/src/pages/CookieNotice.tsx
@@ -1,10 +1,28 @@
 import { useRootStore } from '@/hooks';
 import { observer } from 'mobx-react-lite';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const CookieNotice = observer(() => {
     const { configStore } = useRootStore();
     const [pageBody, setPageBody] = useState('');
+
+    console.log('COOKIE NOTICE', pageBody);
+
+    useEffect(() => {
+        const theme = configStore.store.config.theme;
+        if (theme && theme['THEME_MAP']) {
+            try {
+                const map = JSON.parse(theme['THEME_MAP'] as string);
+
+                const cookiePolicyNoticePage = map['cookiePolicyNoticePage']
+                    ? map['cookiePolicyNoticePage']
+                    : '';
+                setPageBody(cookiePolicyNoticePage);
+            } catch {
+                console.error('Unable to parse theme for Cookie Notice');
+            }
+        }
+    }, [Object.keys(configStore.store.config).length]);
 
     return (
         <div>
--- packages/client/src/pages/PrivacyNotice.tsx
+++ packages/client/src/pages/PrivacyNotice.tsx
@@ -1,13 +1,32 @@
 import { useRootStore } from '@/hooks';
 import { observer } from 'mobx-react-lite';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const PrivacyNotice = observer(() => {
     const { configStore } = useRootStore();
     const [pageBody, setPageBody] = useState('');
 
+    console.log('PRIVACYNOTICE', pageBody);
+
+    useEffect(() => {
+        const theme = configStore.store.config.theme;
+        if (theme && theme['THEME_MAP']) {
+            try {
+                const map = JSON.parse(theme['THEME_MAP'] as string);
+
+                const privacyNoticePage = map['privacyNoticePage']
+                    ? map['privacyNoticePage']
+                    : '';
+                setPageBody(privacyNoticePage);
+            } catch {
+                console.error('Unable to parse theme for Privacy Notice');
+            }
+        }
+    }, [Object.keys(configStore.store.config).length]);
+
     return (
         <div>
+            PRIVACY
             <div dangerouslySetInnerHTML={{ __html: pageBody }} />
         </div>
     );
