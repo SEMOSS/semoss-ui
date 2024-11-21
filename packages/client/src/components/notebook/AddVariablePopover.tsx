e92876c5516baff944f1151c57933e93bf1092f9
soumya pratik <soumyapratik018@gmail.com>
2024-11-21 14:29:02 +0530

    feat(client): code fix

--- packages/client/src/components/notebook/AddVariablePopover.tsx
+++ packages/client/src/components/notebook/AddVariablePopover.tsx
@@ -606,7 +606,7 @@ export const AddVariablePopover = observer((props: AddVariablePopoverProps) => {
             setVariableType(variable.type);
             setIsInput(variable.isInput);
             setIsOutput(variable.isOutput);
-            setVariablePointer(`${variable.to}.${variable.cellId}`);
+
             if (
                 variable.type !== 'query' &&
                 variable.type !== 'block' &&
@@ -631,6 +631,12 @@ export const AddVariablePopover = observer((props: AddVariablePopoverProps) => {
                     if (variableEngine) {
                         setEngine(variableEngine);
                     }
+                }
+            } else {
+                if (variable.type === 'cell') {
+                    setVariablePointer(`${variable.to}.${variable.cellId}`);
+                } else {
+                    setVariablePointer(variable.to);
                 }
             }
         }
