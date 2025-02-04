b151840a613d8fb9cd97402dd283cc3973bade06
Baxter <johbaxter@deloitte.com>
2025-02-04 15:54:36 -0500

    fix(client): fix flash on cookies and attention modal

--- packages/client/src/pages/PlatformMessages.tsx
+++ packages/client/src/pages/PlatformMessages.tsx
@@ -1,125 +1,86 @@
 import { observer } from 'mobx-react-lite';
-import React, { useEffect } from 'react';
-
+import React, { useEffect, useState } from 'react';
 import { useRootStore } from '@/hooks/';
 import { Button, Modal } from '@semoss/ui';
-import { useState, useMemo } from 'react';
 import { WelcomeModal } from '@/components/welcome';
 import { cookieName } from '@/components/cookies';
 
 interface PlatformMessagesProps {
-    /**
-     * children
-     */
     children: React.ReactNode;
-
-    /**
-     * Does the user need app assistance at this point of time (welcome-tour)
-     */
     platformAssist?: boolean;
 }
 
 export const PlatformMessages = observer((props: PlatformMessagesProps) => {
     const { children, platformAssist } = props;
     const { configStore } = useRootStore();
-    const [acceptedTerms, setAcceptedTerms] = useState(false);
+    const [acceptedTerms, setAcceptedTerms] = useState<boolean | null>(null);
+    const [terms, setTerms] = useState({ header: '', text: '' });
 
-    /**
-     * CHECKS IF USER HAS ACCEPTED TERMS
-     */
     useEffect(() => {
         if (configStore.store.userEpoch) {
             const key = `smss--terms--${configStore.store.userEpoch}`;
-
             const item = localStorage.getItem(key);
-
             if (item) {
-                // try to get the state
                 const d = JSON.parse(item);
-
                 setAcceptedTerms(d.state);
+            } else {
+                setAcceptedTerms(false);
             }
         }
-    }, [configStore.store.userEpoch]);
 
-    const acceptTerms = () => {
-        if (configStore.store.userEpoch) {
-            const key = `smss--terms--${configStore.store.userEpoch}`;
-            // save cache
-            localStorage.setItem(
-                key,
-                JSON.stringify({
-                    state: true,
-                }),
-            );
-        }
-        setAcceptedTerms(true);
-    };
-
-    const TERMS = useMemo(() => {
         const theme = configStore.store.config['theme'];
         try {
             if (theme && theme['THEME_MAP']) {
                 const themeMap = JSON.parse(theme['THEME_MAP'] as string);
-                return {
-                    header: themeMap['termsHeaderReact']
-                        ? themeMap['termsHeaderReact']
-                        : 'Attention',
-                    text: themeMap['termsReact'] ? themeMap['termsReact'] : '',
-                };
+                setTerms({
+                    header: themeMap['termsHeaderReact'] || 'Attention',
+                    text: themeMap['termsReact'] || '',
+                });
             }
-            return {
-                header: '',
-                text: '',
-            };
         } catch {
-            return {
-                header: '',
-                text: '',
-            };
+            console.error('Error parsing theme data');
         }
-    }, []);
+    }, [configStore.store.userEpoch, configStore.store.config]);
+
+    const acceptTerms = () => {
+        if (configStore.store.userEpoch) {
+            const key = `smss--terms--${configStore.store.userEpoch}`;
+            localStorage.setItem(key, JSON.stringify({ state: true }));
+        }
+        setAcceptedTerms(true);
+    };
 
     const addressedCookies = localStorage.getItem(cookieName);
 
+    if (acceptedTerms === null) {
+        return <>{children}</>;
+    }
+
     return (
         <>
-            {/* APP CODE */}
             {children}
-
-            {/* ---------------------------------------- */}
-            {/* ALL NOTICES FOR USER BELOW */}
-            {/* ---------------------------------------- */}
-            {TERMS.header && TERMS.text && (
-                <Modal open={!acceptedTerms}>
+            {!acceptedTerms && terms.header && terms.text && (
+                <Modal open={true}>
                     <Modal.Title sx={{ paddingBottom: '0' }}>
                         <div
                             id="attention-modal-header"
-                            dangerouslySetInnerHTML={{
-                                __html: TERMS.header,
-                            }}
-                        ></div>
+                            dangerouslySetInnerHTML={{ __html: terms.header }}
+                        />
                     </Modal.Title>
                     <Modal.Content>
                         <div
                             id="attention-modal-body"
-                            dangerouslySetInnerHTML={{
-                                __html: TERMS.text,
-                            }}
-                        ></div>
+                            dangerouslySetInnerHTML={{ __html: terms.text }}
+                        />
                     </Modal.Content>
                     <Modal.Actions>
-                        <Button
-                            variant="contained"
-                            onClick={() => {
-                                acceptTerms();
-                            }}
-                        >
+                        <Button variant="contained" onClick={acceptTerms}>
                             Accept
                         </Button>
                     </Modal.Actions>
                 </Modal>
             )}
+
             {acceptedTerms && addressedCookies && platformAssist && (
                 <WelcomeModal />
             )}
