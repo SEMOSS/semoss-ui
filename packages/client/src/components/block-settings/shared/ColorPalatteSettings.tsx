76c4f86f2ef83c977409113725058a2520a7c52d
Bannaarisamy Shanmugham <bannaarisamy.shanmugham@kanini.com>
2025-02-07 19:31:42 +0530

    build(colorpalate): color palatte basic design and bar colour change functionality

--- packages/client/src/components/block-settings/shared/ColorPalatteSettings.tsx
+++ packages/client/src/components/block-settings/shared/ColorPalatteSettings.tsx
@@ -6,9 +6,19 @@ import { useBlockSettings, useBlocks } from '@/hooks';
 import { getValueByPath } from '@/utility';
 import { Block, BlockDef, QueryState } from '@/stores';
 import { DefaultBlocks } from '@/components/block-defaults';
-import { Box, Button, Popover, Stack, styled } from '@semoss/ui';
+import {
+    Box,
+    Button,
+    IconButton,
+    InputAdornment,
+    Popover,
+    Stack,
+    styled,
+    TextField,
+} from '@semoss/ui';
 import { SketchPicker } from 'react-color';
-import './ColorPalette.css';
+import { OutlinedInput } from '@mui/material';
+import EChartsReact from 'echarts-for-react';
 
 interface ColorPalatteSettingProps<D extends BlockDef = BlockDef> {
     /**
@@ -63,6 +73,29 @@ export const ColorPalatteSettings = observer<ColorPalatteSettingProps>(
         const [showPopover, setShowPopover] =
             useState<HTMLButtonElement | null>(null);
         const [color, setColor] = useState('#000000');
+        const { data, setData } = useBlockSettings<any>(id);
+        const [value, setValue] = useState(data.option);
+        const pathVal = 'option';
+        const timeoutRef = useRef<ReturnType<typeof setTimeout>>(null);
+        // const barChartColour = getValueByPath(data.option,'series.0');
+        // console.log(barChartColour, 'barChartColour');
+        const computedValue = useMemo(() => {
+            return computed(() => {
+                if (!data) {
+                    return '';
+                }
+
+                const v = getValueByPath(data, pathVal);
+                if (typeof v === 'undefined') {
+                    return '';
+                } else if (typeof v === 'string') {
+                    return v;
+                }
+
+                return JSON.stringify(v, null, 2);
+            });
+        }, [data, pathVal]).get();
+
         function handleClick(event: MouseEvent<HTMLButtonElement>) {
             setShowPopover(event.currentTarget);
         }
@@ -70,19 +103,88 @@ export const ColorPalatteSettings = observer<ColorPalatteSettingProps>(
             setShowPopover(null);
         }
         function handleColorChange(e) {
-            console.log(e.rgb);
-            setColor(e.rgb);
+            console.log(e);
+            setColor(e.hex);
+        }
+        useEffect(() => {
+            setValue(computedValue);
+        }, [computedValue]);
+        useEffect(() => {
+            let option = typeof value === 'string' ? JSON.parse(value) : value;
+            // let customPath = 'series.0.itemStyle.color';
+            // let customPathArray = customPath.split('.');
+
+            if (
+                option.hasOwnProperty('series') &&
+                option['series'].length > 0
+            ) {
+                let seriesIndex = option['series'].findIndex(
+                    (item) => item.type === 'bar',
+                );
+                option['series'][seriesIndex] = {
+                    ...option['series'][seriesIndex],
+                    ['itemStyle']: {
+                        color: color,
+                    },
+                };
+            }
+            runStateUpdateCustom(option);
+        }, [color]);
+        function runStateUpdateCustom(option) {
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
+                timeoutRef.current = null;
+            }
+            timeoutRef.current = setTimeout(() => {
+                try {
+                    setData(pathVal, option as PathValue<any, typeof pathVal>);
+                } catch (e) {
+                    console.log(e);
+                }
+            }, 300);
         }
         const open = Boolean(showPopover);
+        console.log(showPopover, 'showpopover');
         return (
             <div>
-                <Button
-                    aria-describedby={id}
-                    variant="outlined"
-                    onClick={handleClick}
-                >
-                    Click Me
-                </Button>
+                <label htmlFor="outlined-adornment-password">
+                    Select Colour
+                </label>
+                <OutlinedInput
+                    id="outlined-adornment-password"
+                    placeholder="Select Colour"
+                    aria-label="Select Colour"
+                    type={'text'}
+                    value={color}
+                    style={{ width: '100%' }}
+                    onChange={(e) => {
+                        setColor(e.target.value);
+                    }}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label={'select colour'}
+                                onClick={(e: MouseEvent<HTMLButtonElement>) => {
+                                    if (showPopover) setShowPopover(null);
+                                    else setShowPopover(e.currentTarget);
+                                }}
+                                edge="end"
+                            >
+                                <span
+                                    style={{
+                                        backgroundColor: color,
+                                        width: '33px',
+                                        height: '33px',
+                                        borderRadius: '20%',
+                                        display: 'block',
+                                        border: '1px solid #000',
+                                    }}
+                                ></span>
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                    label="Select Colour"
+                />
                 <Popover
                     id={id}
                     open={open}
