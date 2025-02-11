b28f3d4f651b922feca71d7c7562026217c26375
joshromero_deloitte <joshromero@deloitte.com>
2025-02-11 15:53:49 -0500

    fix(client): resolve update bug for total user count based on search term

--- packages/client/src/stores/monolith/monolith.store.ts
+++ packages/client/src/stores/monolith/monolith.store.ts
@@ -2696,7 +2696,7 @@ export class MonolithStore {
 
         const finalResponse = {
             users: response.data,
-            totalUsers: count.data,
+            totalUsers: searchTerm !== '' ? response.data.length : count.data,
         };
 
         return finalResponse;
