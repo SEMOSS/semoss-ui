e9c0b224f3dc6d460c9fa15d11c734674286fab7
Paulson Robert <paulson.robert@kanini.com>
2025-02-17 14:36:04 +0530

    feat(client): accordion block image

--- packages/client/src/components/designer/menu/default-menu.ts
+++ packages/client/src/components/designer/menu/default-menu.ts
@@ -32,7 +32,7 @@ import BLOCK_RADIO from '@/assets/img/BLOCK_RADIO.png';
 import HTML_BLOCK from '@/assets/img/HTML_BLOCK_SM.png';
 import BLOCK_MODAL from '@/assets/img/BLOCK_MODAL.png';
 import BLOCK_THEME from '@/assets/img/BLOCK_THEME.png';
-import BLOCK_ACCORDION from '@/assets/img/BLOCK_MODAL.png';
+import BLOCK_ACCORDION from '@/assets/img/BLOCK_ACCORDION.png';
 
 const SECTION_ELEMENT = 'Element';
 const SECTION_INPUT = 'Input';
