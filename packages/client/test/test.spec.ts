de7caa9f46236465211aeb1d1b411ad0baff9f03
Rose Memis <memisgulden@yahoo.com>
2025-03-02 20:04:45 -0500

    test(client): testing pre push changes

--- packages/client/test/test.spec.ts
+++ packages/client/test/test.spec.ts
@@ -1,7 +1,7 @@
 // Simple test to validate that vitest is working
 import { describe, it, expect } from 'vitest';
 
-describe('vitest setup check 2', () => {
+describe('vitest setup check', () => {
     it('should run a basic test', () => {
         expect(1 + 1).toBe(2);
     });
