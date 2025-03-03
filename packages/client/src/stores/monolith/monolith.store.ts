e0f8b72d43e12ca3eca56c4e685177fa34254957
Maher Khalil <themaherkhalil@gmail.com>
2025-03-03 13:54:10 -0500

    fix(client): moving logic to interceptor for all GET requests

--- packages/client/src/stores/monolith/monolith.store.ts
+++ packages/client/src/stores/monolith/monolith.store.ts
@@ -2676,16 +2676,6 @@ export class MonolithStore {
                     offset: offset,
                     limit: limit,
                 },
-                paramsSerializer: (params) => {
-                    return Object.keys(params)
-                        .map(
-                            (key) =>
-                                `${encodeURIComponent(
-                                    key,
-                                )}=${encodeURIComponent(params[key])}`,
-                        )
-                        .join('&');
-                },
             })
             .catch((error) => {
                 throw Error(error);
