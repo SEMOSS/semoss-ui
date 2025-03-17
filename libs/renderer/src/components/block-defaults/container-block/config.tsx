9ca671c8c094f50ae49180d8c4a2354dc29cf248
Gowrishankar-Palanisamy <gowrishankar.mcet@gmail.com>
2025-03-17 19:48:53 +0530

    feat(container settings): updated the settings with position

--- libs/renderer/src/components/block-defaults/container-block/config.tsx
+++ libs/renderer/src/components/block-defaults/container-block/config.tsx
@@ -7,6 +7,7 @@ import {
     buildDimensionsSection,
     buildBorderSection,
     buildColorSection,
+    buildPositionSection,
 } from "../block-defaults.shared";
 
 import { ContainerBlockDef, ContainerBlock } from "./ContainerBlock";
@@ -35,6 +36,7 @@ export const config: BlockConfig<ContainerBlockDef> = {
     contentMenu: [],
     styleMenu: [
         buildLayoutSection(),
+        buildPositionSection(),
         buildSpacingSection(),
         buildDimensionsSection(),
         buildColorSection(),
