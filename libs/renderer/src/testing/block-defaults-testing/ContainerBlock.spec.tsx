6323a4b02f9e5c1c3e64d0f1165c181f25fdd72a
Robert Nubla <rnubla97@gmail.com>
2025-03-21 10:57:23 -0400

    feat(client): add information on readme for testing; difference betwen it vs test. Update ContainerBlock test to better align with english semantics

--- libs/renderer/src/testing/block-defaults-testing/ContainerBlock.spec.tsx
+++ libs/renderer/src/testing/block-defaults-testing/ContainerBlock.spec.tsx
@@ -4,42 +4,51 @@ import { render, screen } from "../utils";
 import { useBlock } from "@/hooks";
 
 const blocks = {
-    "test-container": {
-        data: {
-            style: {
-                display: "flex",
-                flexDirection: "column",
-                padding: "4px",
-                gap: "8px",
-                flexWrap: "wrap",
-            },
-        },
-        id: "test-container",
-        widget: "container",
-        slots: {
-            children: {
-                children: [],
-                name: "",
-            },
-        },
-        listeners: {
-            onChange: [],
-        },
-    },
+	"test-container": {
+		data: {
+			style: {
+				display: "flex",
+				flexDirection: "column",
+				padding: "4px",
+				gap: "8px",
+				flexWrap: "wrap",
+			},
+		},
+		id: "test-container",
+		widget: "container",
+		slots: {
+			children: {
+				children: [],
+				name: "",
+			},
+		},
+		listeners: {
+			onChange: [],
+		},
+	},
 };
 
-describe("container block", () => {
-    test("Show container", async () => {
-        const { container } = render(
-            // passing the data-testid property does not work; use the querySelector instead since data-block is a custom attribute
-            <ContainerBlock data-testid="containerID" id="test-container" />,
-            {
-                blocks: blocks,
-            },
-        );
-        console.log({ container });
+describe("Container Block", () => {
+	it(" Should render the Container Block", async () => {
+		const { container } = render(
+			// passing the data-testid property does not work; use the querySelector instead since data-block is a custom attribute
+			<ContainerBlock data-testid="containerID" id="test-container" />,
+			{
+				blocks: blocks,
+			},
+		);
+		const exist = container.querySelector("[data-block='test-container']");
+		expect(exist).toBeInTheDocument();
+	});
 
-        const exist = container.querySelector("[data-block='test-container']");
-        expect(exist).toBeInTheDocument();
-    });
+	it("Should render the Container's default Slot text", async () => {
+		render(
+			// passing the data-testid property does not work; use the querySelector instead since data-block is a custom attribute
+			<ContainerBlock data-testid="containerID" id="test-container" />,
+			{
+				blocks: blocks,
+			},
+		);
+		expect(screen.getByText("Add Content")).toBeInTheDocument();
+	});
 });
