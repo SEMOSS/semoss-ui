c968fb79281c80d1fb06b6d6a9e10640eded9487
Rose Memis <memisgulden@yahoo.com>
2025-04-07 14:24:18 -0400

    fix(env): fixing vitest config files

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
