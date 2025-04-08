17eaf0c8f4caf307d600d771999f297f83bbc196
Rose Memis <memisgulden@yahoo.com>
2025-04-08 13:54:55 -0400

    test(env): fixing project level scripts

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,4 +1,3 @@
-/// <reference types='vitest' />
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
