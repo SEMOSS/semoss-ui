ab61b487229e2ef03b8fc0aaf930783714aeb148
Leung <alaleung@deloitte.com>
2025-04-18 13:49:01 -0500

    test(ui): add check box block test

---
+++ libs/renderer/src/testing/block-defaults/Checkbox.spec.tsx
@@ -0,0 +1,120 @@
+import { render, screen } from "../utils";
+import { expect } from "vitest";
+import "@testing-library/jest-dom";
+
+import { CheckboxBlock } from "../../components/block-defaults/checkbox-block/CheckboxBlock";
+
+const blocks = {
+    checkbox: {
+        data: {
+            style: {
+                display: "flex",
+                flexDirection: "column",
+                padding: "4px",
+                gap: "8px",
+                flexWrap: "wrap",
+            },
+            value: false,
+            show: true,
+        },
+        id: "checkbox",
+        widget: "checkbox",
+        slots: {
+            children: {
+                children: [],
+                name: "",
+            },
+        },
+        listeners: {
+            onChange: [],
+        },
+    },
+
+    checkbox2: {
+        data: {
+            style: {
+                display: "flex",
+                flexDirection: "column",
+                padding: "4px",
+                gap: "8px",
+                flexWrap: "wrap",
+            },
+            value: true,
+            show: true,
+        },
+        id: "checkbox2",
+        widget: "checkbox",
+        slots: {
+            children: {
+                children: [],
+                name: "",
+            },
+        },
+        listeners: {
+            onChange: [],
+        },
+    },
+
+    checkbox3: {
+        data: {
+            style: {
+                display: "flex",
+                flexDirection: "column",
+                padding: "4px",
+                gap: "8px",
+                flexWrap: "wrap",
+            },
+            disabled: true,
+            value: true,
+            show: true,
+        },
+        id: "checkbox3",
+        widget: "checkbox",
+        slots: {
+            children: {
+                children: [],
+                name: "",
+            },
+        },
+        listeners: {
+            onChange: [],
+        },
+    },
+};
+
+describe("Checkbox block", () => {
+    it("renders correctly with mocked provider", async () => {
+        const { container } = render(<CheckboxBlock id="checkbox" />, {
+            blocks: blocks,
+        });
+
+        const element = container.querySelector("[data-block='checkbox']");
+
+        expect(element).toBeInTheDocument();
+        expect(element.querySelector(".Mui-checked")).toBeNull();
+        expect(element.querySelector(".Mui-disabled")).toBeNull();
+    });
+
+    it("renders checkbox checked", async () => {
+        const { container } = render(<CheckboxBlock id="checkbox2" />, {
+            blocks: blocks,
+        });
+
+        const element = container.querySelector("[data-block='checkbox2']");
+
+        expect(element).toBeInTheDocument();
+        expect(element.querySelector(".Mui-checked")).toBeInTheDocument();
+    });
+
+    it("renders checkbox disabled", async () => {
+        const { container } = render(<CheckboxBlock id="checkbox3" />, {
+            blocks: blocks,
+        });
+
+        const element = container.querySelector("[data-block='checkbox3']");
+
+        expect(element).toBeInTheDocument();
+        expect(element.querySelector(".Mui-checked")).toBeInTheDocument();
+        expect(element.querySelector(".Mui-disabled")).toBeInTheDocument();
+    });
+});
