d4ab42b6ace66add0b74ee045829ce1f2db83602
Leung <alaleung@deloitte.com>
2025-04-23 09:23:35 -0500

    test(ui): use relative imports, add click test for toggle button

--- libs/renderer/src/testing/block-defaults/ToggleButtonBlock.spec.tsx
+++ libs/renderer/src/testing/block-defaults/ToggleButtonBlock.spec.tsx
@@ -1,8 +1,7 @@
-import { render, screen } from "../utils";
+import { fireEvent, render, screen } from "../utils";
 import { expect } from "vitest";
 import "@testing-library/jest-dom";
-
-import { ToggleButtonBlock } from "../../components/block-defaults/toggle-button-block/ToggleButtonBlock";
+import { ToggleButtonBlock } from "@/components/block-defaults/toggle-button-block/ToggleButtonBlock";
 
 const blocks = {
     "toggle-button": {
@@ -226,5 +225,26 @@ describe("toggle button block", () => {
         expect(offElement.getAttribute("aria-pressed")).toBe("true");
         expect(onElement.classList.contains("Mui-disabled")).toBeTruthy;
         expect(offElement.classList.contains("Mui-disabled")).toBeTruthy;
+    });
+
+    it("clicks and toggles", async () => {
+        const { container } = render(<ToggleButtonBlock id="toggle-button" />, {
+            blocks: blocks,
+        });
+
+        const element = container.querySelector("[data-block='toggle-button']");
+        const elementList = container.querySelectorAll("[type='button']");
+        const onElement = container.querySelector("[value='on']");
+        const offElement = container.querySelector("[value='off']");
+
+        expect(element).toBeInTheDocument();
+        expect(elementList.length).toBe(2);
+        expect(screen.getByText("on")).toBeInTheDocument();
+        expect(screen.getByText("off")).toBeInTheDocument();
+        expect(onElement.getAttribute("aria-pressed")).toBe("false");
+        expect(offElement.getAttribute("aria-pressed")).toBe("false");
+        fireEvent.click(onElement);
+        expect(onElement.getAttribute("aria-pressed")).toBe("true");
+        expect(offElement.getAttribute("aria-pressed")).toBe("false");
     });
 });
