1a9249200bb43f89643683cb6400ee8e083d31e7
Ishumita Mohan <ismohan@deloitte.com>
2025-04-25 16:11:04 -0500

    reverted changes in add team modal

--- packages/client/src/components/teams/AddTeamModal.tsx
+++ packages/client/src/components/teams/AddTeamModal.tsx
@@ -153,36 +153,6 @@ export const AddTeamModal = (props: AddTeamModalProps) => {
             description: 'Directly manage users in the team',
             isOauth: false,
         },
-        {
-            provider: 'adfs',
-            name: 'ADFS',
-            description: '',
-            isOauth: false,
-        },
-        {
-            provider: 'dropbox',
-            name: 'Dropbox',
-            description: '',
-            isOauth: false,
-        },
-        {
-            provider: 'Generic',
-            name: 'Generic',
-            description: 'Specific implementation of OAuth',
-            isOauth: false,
-        },
-        {
-            provider: 'github',
-            name: 'Github',
-            description: '',
-            isOauth: false,
-        },
-        {
-            provider: 'gitlab',
-            name: 'Gitlab',
-            description: '',
-            isOauth: false,
-        },
         ...configStore.store.config.availableProviders,
     ] as {
         provider: string;
