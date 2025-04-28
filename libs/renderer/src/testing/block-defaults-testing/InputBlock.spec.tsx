346fbdf4411729146363f698e3e6e19373c9a51f
mezzet_deloitte <mezzet@deloitte.com>
2025-04-28 01:05:59 -0400

    test(ui): input block tests

---
+++ libs/renderer/src/testing/block-defaults-testing/InputBlock.spec.tsx
@@ -0,0 +1,186 @@
+import { render, screen } from "../utils/index";
+import { expect, test } from "vitest";
+import { waitFor } from "@testing-library/react";
+import { CSSProperties } from "react";
+import "@testing-library/jest-dom";
+
+import { InputBlock } from "../../components/block-defaults/input-block/InputBlock";
+
+const blocks = {
+    "string-input": {
+        data: {
+            style: {},
+            label: "Example Input",
+            value: "Example",
+            type: "text",
+            rows: 1,
+            multiline: false,
+            disabled: false,
+            required: false,
+            loading: false,
+        },
+        id: "string-input",
+        widget: "input",
+        slots: {},
+        listeners: {
+            onChange: [],
+        },
+    },
+
+    "multiLine-input": {
+        data: {
+            style: {},
+            label: "Example Input",
+            value: "Example #1\nExample #2\nExample #3\n",
+            type: "text",
+            rows: 3,
+            multiline: true,
+            disabled: false,
+            required: false,
+            loading: false,
+        },
+        id: "multiLine-input",
+        widget: "input",
+        slots: {},
+        listeners: {
+            onChange: [],
+        },
+    },
+
+    "rowValue-input": {
+        data: {
+            style: {},
+            label: "Example Input",
+            value: "Example #1\nExample #2\nExample #3\n",
+            type: "text",
+            rows: 1,
+            multiline: true,
+            disabled: false,
+            required: false,
+            loading: false,
+        },
+        id: "rowValue-input",
+        widget: "input",
+        slots: {},
+        listeners: {
+            onChange: [],
+        },
+    },
+
+    "number-input": {
+        data: {
+            style: {},
+            label: "Example Input",
+            value: 1,
+            type: "number",
+            rows: 1,
+            multiline: false,
+            disabled: false,
+            required: false,
+            loading: false,
+        },
+        id: "number-input",
+        widget: "input",
+        slots: {},
+        listeners: {
+            onChange: [],
+        },
+    },
+    "rowType-input": {
+        data: {
+            style: {},
+            label: "Example Input",
+            value: 1,
+            type: "number",
+            rows: 1,
+            multiline: false,
+            disabled: false,
+            required: false,
+            loading: false,
+        },
+        id: "rowType-input",
+        widget: "input",
+        slots: {},
+        listeners: {
+            onChange: [],
+        },
+    },
+};
+
+describe("input block", () => {
+    test("renders correctly with mocked provider", async () => {
+        const { container } = render(<InputBlock id="string-input" />, {
+            blocks: blocks,
+        });
+
+        const element = container.querySelector("[data-block='string-input']");
+        //console.log(container.innerHTML);
+        expect(element).toBeInTheDocument();
+    });
+
+    test("renders correct label, type, and rows", async () => {
+        const { container } = render(<InputBlock id="string-input" />, {
+            blocks: blocks,
+        });
+
+        const element = container.querySelector("input");
+        const label = screen.getByLabelText("Example Input");
+
+        expect(label).toBeTruthy();
+        expect(element).toHaveAttribute("rows", "1");
+        expect(element).toHaveAttribute("type", "text");
+        expect(element).toHaveAttribute("value", "Example");
+    });
+
+    test("renders correct rows and value with multiline input", async () => {
+        const { container } = render(<InputBlock id="multiLine-input" />, {
+            blocks: blocks,
+        });
+
+        console.log(container.innerHTML);
+
+        const element = container.querySelector("input");
+        const rows = container.querySelector("textarea");
+        const label = screen.getByLabelText("Example Input");
+
+        expect(label).toBeTruthy();
+        expect(rows).toHaveAttribute("rows", "3");
+        // expect(element).toHaveAttribute(
+        //     "value",
+        //     "Example #1\nExample #2\nExample #3\n",
+        // );
+    });
+
+    test("does not render if values greater than row number", async () => {
+        const { container } = render(<InputBlock id="rowValue-input" />, {
+            blocks: blocks,
+        });
+
+        const element = container.querySelector("input");
+        console.log(container.innerHTML);
+
+        expect(element).toHaveAttribute("rows", "1");
+        expect(element).not.toHaveAttribute(
+            "value",
+            "Example #1\nExample#2\nExample#3\n",
+        );
+    });
+
+    test("renders input with number type", async () => {
+        const { container } = render(<InputBlock id="number-input" />, {
+            blocks: blocks,
+        });
+
+        const element = container.querySelector("input");
+        const label = screen.getByLabelText("Example Input");
+
+        expect(label).toBeTruthy();
+        expect(element).toHaveAttribute("rows", "1");
+        expect(element).toHaveAttribute("type", "number");
+        expect(element).toHaveAttribute("value", "1");
+    });
+
+    // type and value mismatch
+    // input overflows container
+    // onChange
+});
