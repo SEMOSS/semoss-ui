fa99cffc6c326974fe203953e995f077859247bd
Robert Nubla <rnubla97@gmail.com>
2025-04-28 14:44:50 -0400

    test(renderer): initial audio block test file

---
+++ libs/renderer/src/testing/block-defaults/AudioBlock.spec.tsx
@@ -0,0 +1,22 @@
+const blocks = {
+	"audio-player": {
+		data: {
+			autoplay: false,
+			controls: true,
+			label: "Audio Player",
+			loop: false,
+			show: "true",
+			source:
+				"https://upload.wikimedia.org/wikipedia/commons/6/65/Star_Spangled_Banner_instrumental.ogg",
+		},
+		id: "audio-player",
+		widget: "audio-player",
+		slots: {},
+	},
+};
+
+describe("Audio Block", async() => {
+    it("Shoulder render the audio block", async() => {
+        
+    })
+})
