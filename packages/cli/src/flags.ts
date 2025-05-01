3b7610736d4b059917f5169daf6918539a630ff9
Hock <ghock@deloitte.com>
2025-05-01 15:56:09 -0400

    feat(client): attemmpts to run pixel having environment issue

--- packages/cli/src/flags.ts
+++ packages/cli/src/flags.ts
@@ -5,3 +5,9 @@ export const FORCE = Flags.boolean({ char: 'f' });
 
 // flag with a value (-n, --name=VALUE)
 export const NAME = Flags.string({ char: 'n', description: 'name to print' });
+
+
+export const ENV = Flags.string({
+    char: 'e',
+    description: 'Path to the environment variables. Default is .env',
+});
