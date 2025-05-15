332fe86b9ae918be5818ded1e6b79761608da49d
Leung <alaleung@deloitte.com>
2025-05-15 00:45:16 -0500

    test(ui): change package.json for renderer and client to use v8 for code coverage

--- libs/renderer/vite.config.ts
+++ libs/renderer/vite.config.ts
@@ -1,22 +1,22 @@
 /// <reference types='vitest/config' />
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
-import { resolve } from 'node:path';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
+import { nxCopyAssetsPlugin } from "@nx/vite/plugins/nx-copy-assets.plugin";
+import { resolve } from "node:path";
 
 export default defineConfig({
     root: __dirname,
-    cacheDir: '../../node_modules/.vite/packages/renderer',
+    cacheDir: "../../node_modules/.vite/packages/renderer",
 
     plugins: [
         react({ include: /\.(js|jsx|ts|tsx)$/ }),
         nxViteTsPaths(),
-        nxCopyAssetsPlugin(['*.md']),
+        nxCopyAssetsPlugin(["*.md"]),
     ],
 
     resolve: {
-        alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
+        alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
     },
 
     // Uncomment this if you are using workers.
@@ -25,28 +25,28 @@ export default defineConfig({
     // },
 
     test: {
-        name: 'renderer',
+        name: "renderer",
         watch: false,
         globals: true,
-        environment: 'jsdom',
-        include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        environment: "jsdom",
+        include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
         deps: {
             // Required for vitest-canvas-mock
-            inline: ['vitest-canvas-mock'],
+            inline: ["vitest-canvas-mock"],
         },
-        reporters: ['default'],
+        reporters: ["default"],
         coverage: {
-            provider: 'v8',
-            reportsDirectory: '../../coverage/packages/renderer',
+            provider: "v8",
+            reportsDirectory: "../../coverage/packages/renderer",
         },
         environmentOptions: {
             jsdom: {
-                resources: 'usable',
+                resources: "usable",
             },
         },
         cache: {
-            dir: '../../node_modules/.vitest',
+            dir: "../../node_modules/.vitest",
         },
-        setupFiles: ['./vitest.setup.ts'],
+        setupFiles: ["./vitest.setup.ts"],
     },
 });
