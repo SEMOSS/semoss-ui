48c966d3d92378a5e470d437a4155ce4c83aee3b
SindhuHarika <sindhuharikakande@gmail.com>
2025-05-19 19:30:22 +0530

    feat(client): additional changes

--- packages/client/src/pages/jobs/JobsPage.tsx
+++ packages/client/src/pages/jobs/JobsPage.tsx
@@ -171,7 +171,7 @@ export function JobsPage() {
                     color: 'success',
                     message:
                         jobId.length > 1 && jobGroup.length > 1
-                            ? `Sucessfully deleted all selected files`
+                            ? `Successfully deleted all selected files`
                             : `Successfully deleted ${type}`,
                 });
                 jobId.length > 1 && jobGroup.length > 1
