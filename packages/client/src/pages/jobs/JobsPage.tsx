52040ab20923c84db3043a0d3fe6399c578785a0
SindhuHarika <sindhuharikakande@gmail.com>
2025-06-03 18:25:48 +0530

    feat(client): review comment changes

--- packages/client/src/pages/jobs/JobsPage.tsx
+++ packages/client/src/pages/jobs/JobsPage.tsx
@@ -155,9 +155,12 @@ export function JobsPage() {
     const deleteJob = (jobId: string[], jobGroup: string[]) => {
         let pixel;
         if (jobId.length > 1 && jobGroup.length > 1) {
-            const jobIdStr = `"${jobId.join(' , ')}"`;
-            const jobGroupStr = `"${jobGroup.join(' , ')}"`;
-            pixel = `META | RemoveJobFromDB(jobId=[${jobIdStr}], jobGroup=[${jobGroupStr}]) `;
+            // const jobIdStr = `"${jobId.join(' , ')}"`;
+            // const jobGroupStr = `"${jobGroup.join(' , ')}"`;
+            // pixel = `META | RemoveJobFromDB(jobId=[${jobIdStr}], jobGroup=[${jobGroupStr}]) `;
+            pixel = `META | RemoveJobFromDB(jobId=${JSON.stringify(
+                jobId,
+            )}, jobGroup=${JSON.stringify(jobGroup)}) `;
         } else {
             pixel = 'META | RemoveJobFromDB(';
             pixel += 'jobId=["' + jobId[0] + '"], ';
