f4bcc8347d50958afe2a9dcd6d4c9abb4c484f4a
Rose Memis <memisgulden@yahoo.com>
2025-06-03 21:27:24 -0400

    fix(client): linted only staged

--- lint-staged.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    '{client,ui}/**/*.{ts,tsx,jsx,js}': files => {
-        return `nx affected --target=typecheck --files=${files.join(',')}`;
-    },
-    '{client,ui}/**/*.{js,ts,jsx,tsx,json}': [
-        files => `nx affected:lint --files=${files.join(',')}`,
-        files => `nx format:write --files=${files.join(',')}`,
-    ],
-};
---
+++ lintstagedrc.js
@@ -0,0 +1,43 @@
+const { execSync } = require('child_process');
+
+module.exports = {
+    '**/*.{ts,tsx,jsx,js}': files => {
+        return `nx affected --target=typecheck --files=${files.join(',')}`;
+    },
+    '**/*.{ts,tsx,jsx,js,json}': [
+        files => `nx affected:lint --files=${files.join(',')}`,
+        files => `nx format:write --files=${files.join(',')}`,
+    ],
+};
+
+// module.exports = async ( stagedFiles ) => {
+//     //files to lint
+//     const tsFiles = stagedFiles.filter((f) => 
+//         /\.(ts|tsx|js|jsx)$/.test(f)
+//     );
+
+//     console.log("ts files")
+
+//     const prettierCmds = stagedFiles.map((file) => `prettier --write ${file}`);
+
+//     if(tsFiles.length === 0) return prettierCmds;
+
+//     //get the all of the nx projects
+//     const graphRaw = execSync('npx nx show projects --json').toString();
+//     const projectGraph = JSON.parse(graphRaw);
+
+//     //affected projects
+//     const affectedRaw = execSync(`npx nx print-affected --type=lint --files=${tsFiles.join(',')}`);
+//     const affected = JSON.parse(affectedRaw);
+//     const affectedProjects = affected.projects ?? [];
+
+//     //only return projects with a lint target
+//     const lintableProjects = affectedProjects.filter((p) => {
+//         const targets = projectGraph[p]?.targets ?? {};
+//         return targets.hasOwnProperty('lint');
+//     });
+
+//     const lintCmds = lintableProjects.map((project) => `npx nx lint ${project}`);
+
+//     return [...lintCmds, ...prettierCmds];
+// };
