eac6f9a5620641497067c9abd21c967921b50992
Rose Memis <memisgulden@yahoo.com>
2025-06-04 10:09:17 -0400

    fix(client): removing unused linting code

--- lintstagedrc.js
+++ lintstagedrc.js
@@ -1,5 +1,3 @@
-const { execSync } = require('child_process');
-
 module.exports = {
     '{apps,libs,tools}/**/*.{ts,tsx,jsx,js}': files => {
         return `nx affected --target=typecheck --files=${files.join(',')}`;
@@ -10,34 +8,3 @@ module.exports = {
     ],
 };
 
-// module.exports = async ( stagedFiles ) => {
-//     //files to lint
-//     const tsFiles = stagedFiles.filter((f) => 
-//         /\.(ts|tsx|js|jsx)$/.test(f)
-//     );
-
-//     console.log("ts files")
-
-//     const prettierCmds = stagedFiles.map((file) => `prettier --write ${file}`);
-
-//     if(tsFiles.length === 0) return prettierCmds;
-
-//     //get the all of the nx projects
-//     const graphRaw = execSync('npx nx show projects --json').toString();
-//     const projectGraph = JSON.parse(graphRaw);
-
-//     //affected projects
-//     const affectedRaw = execSync(`npx nx print-affected --type=lint --files=${tsFiles.join(',')}`);
-//     const affected = JSON.parse(affectedRaw);
-//     const affectedProjects = affected.projects ?? [];
-
-//     //only return projects with a lint target
-//     const lintableProjects = affectedProjects.filter((p) => {
-//         const targets = projectGraph[p]?.targets ?? {};
-//         return targets.hasOwnProperty('lint');
-//     });
-
-//     const lintCmds = lintableProjects.map((project) => `npx nx lint ${project}`);
-
-//     return [...lintCmds, ...prettierCmds];
-// };
