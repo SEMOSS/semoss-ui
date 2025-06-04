fe6addacad137c667fc135e4c102961545bcd8f2
Rose Memis <memisgulden@yahoo.com>
2025-06-04 11:36:40 -0400

    fix(client): lint staged rules to proper libs

--- lintstagedrc.js
+++ lintstagedrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-    '{apps,libs,tools}/**/*.{ts,tsx,jsx,js}': files => {
+    '{libs,packages}/**/*.{ts,tsx,jsx,js}': files => {
         return `nx affected --target=typecheck --files=${files.join(',')}`;
     },
-    '{apps,libs,tools}/**/*.{ts,tsx,jsx,js,json}': [
+    '{libs,packages}/**/*.{ts,tsx,jsx,js,json}': [
         files => `nx affected:lint --files=${files.join(',')}`,
         files => `nx format:write --files=${files.join(',')}`,
     ],
