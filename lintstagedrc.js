05b36713a9aafc9bb5cfe3400c796f8ffe858575
Rose Memis <memisgulden@yahoo.com>
2025-06-09 11:17:37 -0400

    test(client): try to add mui/material import

--- lintstagedrc.js
+++ lintstagedrc.js
@@ -1,8 +1,6 @@
 module.exports = {
     '{*,libs,packages}/**/*.{ts,tsx,jsx,js,json}': [
-        files => `nx affected --target=eslint:lint --files=${files.join(',')}`,
-        console.log('Staged files: ', files)
-        // files => `nx format:write --files=${files.join(',')}`,
+        files => `npx eslint ${files}`,
     ],
 };
 
