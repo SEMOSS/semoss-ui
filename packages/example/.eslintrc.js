d4102d5ce47083b3c6410fae3549006b4d807816
Rose Memis <memisgulden@yahoo.com>
2025-06-11 17:09:19 -0400

    fix(environment): adding additional workspace dependencies

--- packages/example/.eslintrc.js
+++ packages/example/.eslintrc.js
@@ -5,29 +5,8 @@ module.exports = {
         node: true,
     },
     extends: [
+        '../../.eslintrc.json',
         'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:react/jsx-runtime',
         // 'plugin:@typescript-eslint/recommended',
-        'prettier',
-        'plugin:prettier/recommended',
     ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-        },
-    },
-    plugins: ['react'],
-    rules: {
-        'react/prop-types': 0,
-        'no-debugger': 2,
-    },
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
 };
