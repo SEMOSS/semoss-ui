963df3e9c3fdb7baa039e14441676c67fd69ab98
Robert Nubla <rnubla97@gmail.com>
2025-07-07 13:40:07 -0400

    test(renderer): current work in progress for migrating from 9 to 10

--- libs/renderer/src/store/state/migration/migrate__1_0_0_alpha_9__to___1_0_0_alpha_10_.ts
+++ libs/renderer/src/store/state/migration/migrate__1_0_0_alpha_9__to___1_0_0_alpha_10_.ts
@@ -12,48 +12,113 @@ const config: Migration = {
     run: (state) => {
         const newState = { ...state };
 
-        // check each cell, convert each cell to a 
-        Object.values(state.queries).forEach((q) => {
-            q.cells.forEach((c) => {
-                let found = false
-                Object.values(newState.variables).forEach((variable) => {
-                    if(variable.to === q.id) {
-                        if (c.id === variable.cellId) {
-                            found = true
-                        }
-                    }
-                })
+        // check each cell, convert each cell to a
+        for (const q of Object.values(state.queries)) {
+            for (const c of q.cells) {
+                let found = false;
 
+                for (const variable of Object.values(newState.variables)) {
+                    if (variable.to === q.id) {
+                        if (c.id === variable.cellId) {
+                            found = true;
+                        }
+                    } else {
+                        found = false;
+                    }
+                }
                 // create a variable for cells that arent already there
-                if(!found) {
+                // console.log({ found, query: q.id, cell: c.id });
+                if (!found) {
+                    console.log({ found, query: q.id, cell: c.id });
                     const variableConfig = {
                         type: "cell",
                         to: q.id,
-                        cellId: c.id
-                    }
+                        cellId: c.id,
+                    };
 
                     // EDGE CASE: Check if someone coincidentally named there variable nbName--cellId
-                    let baseKey = `${q.id}--${c.id}`
+                    const baseKey = `${q.id}--${c.id}`;
                     let variableKey = baseKey;
                     let attemptCount = 0;
-                    
-                    while(newState.variables[variableKey]) {
-                        const randomSuffix = Math.floor(Math.random() * 100000)
-                        variableKey = `${baseKey}--${randomSuffix}`
 
-                        attemptCount++
+                    console.log({
+                        beforeVariableKey: newState.variables[variableKey],
+                        variables: newState.variables,
+                    });
 
-                        if(attemptCount > 100) {
-                            throw new Error(`Failed to generate unique id for variable pointing to -->  sheet: ${q.id} cell:${c.id}`)
+                    while (newState.variables[variableKey]) {
+                        const randomSuffix = Math.floor(Math.random() * 100000);
+                        variableKey = `${baseKey}--${randomSuffix}`;
+                        console.log({ variableKey });
+
+                        attemptCount++;
+
+                        if (attemptCount > 100) {
+                            throw new Error(
+                                `Failed to generate unique id for variable pointing to -->  sheet: ${q.id} cell:${c.id}`,
+                            );
                         }
-                        
-                    } 
-                    newState.variables[`${q.id}--${c.id}`] = variableConfig
-                }
-                
-            })
+                    }
 
-        })
+                    newState.variables[`${q.id}--${c.id}`] = variableConfig;
+                    console.log({
+                        after: newState.variables[`${q.id}--${c.id}`],
+                    });
+                }
+            }
+        }
+        // Object.values(state.queries).forEach((q) => {
+        //     q.cells.forEach((c) => {
+        //         let found = false;
+        //         Object.values(newState.variables).forEach((variable) => {
+        //             if (variable.to === q.id) {
+        //                 if (c.id === variable.cellId) {
+        //                     found = true;
+        //                 }
+        //             }
+        //         });
+
+        //         // create a variable for cells that arent already there
+        //         // console.log({ found, query: q.id, cell: c.id });
+        //         if (!found) {
+        //             console.log({ found, query: q.id, cell: c.id });
+        //             const variableConfig = {
+        //                 type: "cell",
+        //                 to: q.id,
+        //                 cellId: c.id,
+        //             };
+
+        //             // EDGE CASE: Check if someone coincidentally named there variable nbName--cellId
+        //             const baseKey = `${q.id}--${c.id}`;
+        //             let variableKey = baseKey;
+        //             let attemptCount = 0;
+
+        //             console.log({
+        //                 beforeVariableKey: newState.variables[variableKey],
+        //                 variables: newState.variables,
+        //             });
+
+        //             while (newState.variables[variableKey]) {
+        //                 const randomSuffix = Math.floor(Math.random() * 100000);
+        //                 variableKey = `${baseKey}--${randomSuffix}`;
+        //                 console.log({ variableKey });
+
+        //                 attemptCount++;
+
+        //                 if (attemptCount > 100) {
+        //                     throw new Error(
+        //                         `Failed to generate unique id for variable pointing to -->  sheet: ${q.id} cell:${c.id}`,
+        //                     );
+        //                 }
+        //             }
+
+        //             newState.variables[`${q.id}--${c.id}`] = variableConfig;
+        //             console.log({
+        //                 after: newState.variables[`${q.id}--${c.id}`],
+        //             });
+        //         }
+        //     });
+        // });
 
         return newState;
     },
