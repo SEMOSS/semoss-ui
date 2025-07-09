b5d0dc05c967812d4df46e1eb803a576b05a1e09
Neel <neelneelneel@users.noreply.github.com>
2025-07-09 12:59:10 -0400

    fix: WIP updating build

--- libs/renderer/rollup.config.js
+++ libs/renderer/rollup.config.js
@@ -3,6 +3,10 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
+import image from "@rollup/plugin-image";
+import json from '@rollup/plugin-json';
+
+import packageJson from "./package.json" with { type: "json" };
 
 import del from "rollup-plugin-delete";
 
@@ -11,7 +15,6 @@ const isProduction = process.env.NODE_ENV === "production";
 export default defineConfig({
     input: {
         index: "src/index.ts",
-        "js-frameworks/react/index": "src/js-frameworks/react/index.ts",
     },
     output: {
         dir: "dist",
@@ -23,12 +26,14 @@ export default defineConfig({
         del({ targets: "dist" }),
         resolve(),
         commonjs(),
+        image(),
+        json(),
         typescript({
             tsconfig: "./tsconfig.json",
         }),
         isProduction && terser(),
     ],
-    external: ["react"],
+    external: [...Object.keys(packageJson.peerDependencies)],
     watch: {
         clearScreen: false,
     },
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -3,10 +3,15 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
     plugins: [react({ include: /\.(js|jsx|ts|tsx)$/ })],
     resolve: {
         alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
+    },
+    build: {
+        minify: isProduction,
     },
     // test: {
     //     name: 'client',
