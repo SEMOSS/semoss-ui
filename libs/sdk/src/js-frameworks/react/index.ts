8c98e8af186a42e8977ebe1f8b530c5ebd0b5d42
Baxter <johbaxter@deloitte.com>
2025-07-21 15:52:15 -0400

    refactor(client/renderer): use debounce fns from sdk

--- libs/sdk/src/js-frameworks/react/index.ts
+++ libs/sdk/src/js-frameworks/react/index.ts
@@ -3,4 +3,5 @@ export { InsightProvider } from "./InsightProvider";
 
 export { useInsight } from "./useInsight";
 export { useDebounced as debounced } from "./useDebounced";
+export { useDebouncedValue } from "./useDebouncedValue";
 export { usePixel } from "./usePixel";
