5c55a4e33e13c9305c4638ba27f488d25adca4e8
Baxter <johbaxter@deloitte.com>
2025-07-21 12:05:20 -0400

    feat(sdk/client): add useDebounced hook in sdk and implement on catalog/spotlight search

--- libs/sdk/src/js-frameworks/react/index.ts
+++ libs/sdk/src/js-frameworks/react/index.ts
@@ -2,4 +2,5 @@ export * from "../../";
 export { InsightProvider } from "./InsightProvider";
 
 export { useInsight } from "./useInsight";
+export { useDebounced as debounced } from "./useDebounced";
 export { usePixel } from "./usePixel";
