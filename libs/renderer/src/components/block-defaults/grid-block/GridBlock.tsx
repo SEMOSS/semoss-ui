3f5e4270c9d825f3b8efb78e459bab1933ed0c31
Paulson Robert <paulson.robert@kanini.com>
2025-07-23 19:52:54 +0530

    feat(renderer): 1575 pagination in data grid bug fix

--- libs/renderer/src/components/block-defaults/grid-block/GridBlock.tsx
+++ libs/renderer/src/components/block-defaults/grid-block/GridBlock.tsx
@@ -428,7 +428,7 @@ export const GridBlock: BlockComponent = observer(({ id }) => {
                     rowCount={frame.count}
                     paginationModel={paginationModel}
                     onPaginationModelChange={handlePaginationModalChange}
-                    pageSizeOptions={[10, 50, 100, 500]}
+                    pageSizeOptions={[10, 50, 100]}
                     getRowHeight={getRowHeight}
                     columnHeaderHeight={50}
                     disableColumnMenu
