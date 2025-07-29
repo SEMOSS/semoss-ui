1ec0d2e825f459fd5569e4ff9c4e1f354eb87ac7
Bannaarisamy Shanmugham <bannaarisamy.shanmugham@kanini.com>
2025-07-29 13:17:31 +0530

    build(landingpage): ui inconsistencies fix

--- packages/client/src/components/landing/FanFavoritesSection.tsx
+++ packages/client/src/components/landing/FanFavoritesSection.tsx
@@ -1,85 +1,85 @@
-import React from 'react';
+import React from "react";
 
-import { Typography, Stack } from '@semoss/ui';
+import { Typography, Stack } from "@semoss/ui";
 
-import { AppMetadata, AppTileCard } from '../app';
+import { AppMetadata, AppTileCard } from "../app";
 
 const BUSINESS_INTELLIGENCE_APP: AppMetadata = {
-    project_id: '',
-    project_name: 'BI',
-    project_type: '',
-    project_cost: '',
-    project_global: '',
-    project_catalog_name: '',
-    project_created_by: 'SYSTEM',
-    project_created_by_type: '',
-    project_date_created: '',
-    project_date_last_edited: '',
-    project_has_portal: false,
-    project_portal_name: '',
-    project_portal_published_date: '',
-    project_published_user: '',
-    project_published_user_type: '',
-    project_reactors_compiled_date: '',
-    project_reactors_compiled_user: '',
-    project_reactors_compiled_user_type: '',
-    project_favorite: '',
-    user_permission: null,
-    group_permission: '',
-    tag: [],
-    description: 'Develop dashboards and visualizations to view data',
+  project_id: "",
+  project_name: "BI",
+  project_type: "",
+  project_cost: "",
+  project_global: "",
+  project_catalog_name: "",
+  project_created_by: "SYSTEM",
+  project_created_by_type: "",
+  project_date_created: "",
+  project_date_last_edited: "",
+  project_has_portal: false,
+  project_portal_name: "",
+  project_portal_published_date: "",
+  project_published_user: "",
+  project_published_user_type: "",
+  project_reactors_compiled_date: "",
+  project_reactors_compiled_user: "",
+  project_reactors_compiled_user_type: "",
+  project_favorite: "",
+  user_permission: null,
+  group_permission: "",
+  tag: [],
+  description: "Develop dashboards and visualizations to view data",
 };
 
 const TERMINAL_APP: AppMetadata = {
-    project_id: '',
-    project_name: 'Terminal',
-    project_type: '',
-    project_cost: '',
-    project_global: '',
-    project_catalog_name: '',
-    project_created_by: 'SYSTEM',
-    project_created_by_type: '',
-    project_date_created: '',
-    project_date_last_edited: '',
-    project_has_portal: false,
-    project_portal_name: '',
-    project_portal_published_date: '',
-    project_published_user: '',
-    project_published_user_type: '',
-    project_reactors_compiled_date: '',
-    project_reactors_compiled_user: '',
-    project_reactors_compiled_user_type: '',
-    project_favorite: '',
-    user_permission: null,
-    group_permission: '',
-    tag: [],
-    description: 'Execute commands and see a response',
+  project_id: "",
+  project_name: "Terminal",
+  project_type: "",
+  project_cost: "",
+  project_global: "",
+  project_catalog_name: "",
+  project_created_by: "SYSTEM",
+  project_created_by_type: "",
+  project_date_created: "",
+  project_date_last_edited: "",
+  project_has_portal: false,
+  project_portal_name: "",
+  project_portal_published_date: "",
+  project_published_user: "",
+  project_published_user_type: "",
+  project_reactors_compiled_date: "",
+  project_reactors_compiled_user: "",
+  project_reactors_compiled_user_type: "",
+  project_favorite: "",
+  user_permission: null,
+  group_permission: "",
+  tag: [],
+  description: "Execute commands and see a response",
 };
 
 export const FanFavoritesSection = () => {
-    return (
-        <Stack gap={1}>
-            <Typography variant={'h6'}>Try these fan favorites</Typography>
-            <Stack direction={'row'} gap={3}>
-                <AppTileCard
-                    app={BUSINESS_INTELLIGENCE_APP}
-                    background="#BADEFF"
-                    href="../../legacy/dist/"
-                    systemApp={true}
-                    appType={'BI'}
-                    isLoading={false}
-                    showSkeleton={false}
-                />
-                <AppTileCard
-                    app={TERMINAL_APP}
-                    background="#BADEFF"
-                    href="../../legacy/dist/#!/embed-terminal"
-                    systemApp={true}
-                    appType={'TERMINAL'}
-                    isLoading={false}
-                    showSkeleton={false}
-                />
-            </Stack>
-        </Stack>
-    );
+  return (
+    <Stack gap={1}>
+      <Typography variant={"body1"}>Try these fan favorites</Typography>
+      <Stack direction={"row"} gap={3}>
+        <AppTileCard
+          app={BUSINESS_INTELLIGENCE_APP}
+          background="#BADEFF"
+          href="../../legacy/dist/"
+          systemApp={true}
+          appType={"BI"}
+          isLoading={false}
+          showSkeleton={false}
+        />
+        <AppTileCard
+          app={TERMINAL_APP}
+          background="#BADEFF"
+          href="../../legacy/dist/#!/embed-terminal"
+          systemApp={true}
+          appType={"TERMINAL"}
+          isLoading={false}
+          showSkeleton={false}
+        />
+      </Stack>
+    </Stack>
+  );
 };
--- packages/client/src/pages/LandingPage.tsx
+++ packages/client/src/pages/LandingPage.tsx
@@ -1,72 +1,66 @@
-import { observer } from 'mobx-react-lite';
+import { observer } from "mobx-react-lite";
 import {
-    styled,
-    Typography,
-    Switch,
-    ToggleButtonGroup,
-    ToggleButton,
-} from '@semoss/ui';
+  styled,
+  Typography,
+  Switch,
+  ToggleButtonGroup,
+  ToggleButton,
+} from "@semoss/ui";
 
-import { useCacheState, usePage } from '@/hooks';
-import { BusinessUserScreen, DeveloperUserScreen } from '@/components/landing';
-import { NavbarLeft, NavbarRight, NavbarHeader } from '../components/shared';
+import { useCacheState, usePage } from "@/hooks";
+import { BusinessUserScreen, DeveloperUserScreen } from "@/components/landing";
+import { NavbarLeft, NavbarRight, NavbarHeader } from "../components/shared";
 
 const StyledAppBuilder = styled(Typography)(({ theme }) => ({
-    color: 'var(--Text-Secondary, #666)',
-    fontFeatureSettings: "'liga' off, 'clig' off",
-    display: 'flex',
-    /* Typography/Caption */
-    fontFamily: 'Roboto',
-    fontSize: '12px',
-    fontStyle: 'normal',
-    fontWeight: '400',
-    lineHeight: '166%' /* 19.92px */,
-    letterSpacing: '0.4px',
-    flexDirection: 'column',
-    alignItems: 'flex-end',
-    flexGrow: 1,
+  color: "var(--Text-Secondary, #666)",
+  fontFeatureSettings: "'liga' off, 'clig' off",
+  display: "flex",
+  /* Typography/Caption */
+  fontFamily: "Roboto",
+  fontSize: "12px",
+  fontStyle: "normal",
+  fontWeight: "400",
+  lineHeight: "166%" /* 19.92px */,
+  letterSpacing: "0.4px",
+  flexDirection: "column",
+  alignItems: "flex-end",
+  flexGrow: 1,
+  alignSelf: "center",
 }));
 
 const StyledSwitch = styled(Switch)(({ theme }) => ({
-    display: 'flex',
-    // width: '3.125rem',
-    padding: '2px 0px',
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    gap: '10px',
+  display: "flex",
+  // width: '3.125rem',
+  padding: "2px 0px",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  gap: "10px",
 }));
 
 export const LandingPage: React.FC = observer(() => {
-    const [devMode, setDevMode] = useCacheState(false, `landing--devMode`);
+  const [devMode, setDevMode] = useCacheState(false, `landing--devMode`);
 
-    // setup the page
-    usePage({
-        showNavbarSearch: devMode,
-    });
+  // setup the page
+  usePage({
+    showNavbarSearch: devMode,
+  });
 
-    return (
-        <>
-            <NavbarLeft>
-                <NavbarHeader />
-            </NavbarLeft>
-            <NavbarRight>
-                <ToggleButtonGroup
-                    size="small"
-                    color={'primary'}
-                    value={devMode ? 'build' : ''}
-                >
-                    <ToggleButton
-                        size="small"
-                        value={'build'}
-                        onClick={() => {
-                            setDevMode(!devMode);
-                        }}
-                    >
-                        Build
-                    </ToggleButton>
-                </ToggleButtonGroup>
-            </NavbarRight>
-            {devMode ? <DeveloperUserScreen /> : <BusinessUserScreen />}
-        </>
-    );
+  return (
+    <>
+      <NavbarLeft>
+        <NavbarHeader />
+      </NavbarLeft>
+      <NavbarRight>
+        <StyledAppBuilder variant="caption">App Builder</StyledAppBuilder>
+        <StyledSwitch
+          checked={devMode}
+          size={"small"}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setDevMode(e.target.checked);
+          }}
+        />
+      </NavbarRight>
+      {devMode ? <DeveloperUserScreen /> : <BusinessUserScreen />}
+    </>
+  );
 });
