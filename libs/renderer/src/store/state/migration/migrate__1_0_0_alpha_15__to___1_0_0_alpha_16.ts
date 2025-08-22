fea2d8942e8804a15e25800f7fbd0a48876a08b1
Manikandan Velayutham <manikandan.velayutham@kanini.com>
2025-08-22 19:03:33 +0530

    Update migrate__1_0_0_alpha_15__to___1_0_0_alpha_16.ts

--- libs/renderer/src/store/state/migration/migrate__1_0_0_alpha_15__to___1_0_0_alpha_16.ts
+++ libs/renderer/src/store/state/migration/migrate__1_0_0_alpha_15__to___1_0_0_alpha_16.ts
@@ -3,7 +3,7 @@ import type { Migration, MigrationState } from "./migration.types";
 const migrate__1_0_0_alpha_15__to_1_0_0_alpha_16: Migration = {
 	versionFrom: "1.0.0-alpha.15",
 	versionTo: "1.0.0-alpha.16",
-	// This function performs a migration on the state by updating button blocks
+	// This function performs a migration on the state by updating image block data
 	async run(state: MigrationState): Promise<MigrationState> {
 		const newState: MigrationState = { ...state };
 		// Ensure that all legacy image blocks have the 'unavailable' and 'placeholderText' fields
