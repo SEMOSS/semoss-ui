abba41494748ac417b989be75e5d53ef1333ccd0
Manikandan Velayutham <manikandan.velayutham@kanini.com>
2025-09-03 22:07:29 +0530

    feat(client): update image block settings to pull from app assets (#1466)
    * #1368 image block implementation
    
    * #1368 display base64 image implemented
    
    * feat: tooltip changes
    
    * feat: optimize and split components
    
    * feat: image settings block moved to client from lib
    
    * feat: deleted unwanted changes
    
    * feat: removed unwanted reference
    
    * feat: code optimized
    
    * feat: image block updated with latest dev
    
    * refactor: migration function included for image block config
    
    * Update migrate__1_0_0_alpha_15__to___1_0_0_alpha_16.ts

--- packages/client/src/components/blocks-workspace/blocks/block-settings/image-block/config.tsx
+++ packages/client/src/components/blocks-workspace/blocks/block-settings/image-block/config.tsx
@@ -6,7 +6,6 @@ import {
 } from "@mui/icons-material";
 import {
 	ButtonGroupSettings,
-	InputSettings,
 	SelectInputSettings,
 } from "../../settings";
 import { BLOCK_TYPE_DISPLAY } from "../block-defaults.constants";
@@ -16,127 +15,116 @@ import {
 	buildShowField,
 } from "../block-defaults.shared";
 import type { BlockSettingsConfig } from "../settings.types";
+import GeneralSettings from './GeneralSettings';
 
 export const config: BlockSettingsConfig = {
-	type: BLOCK_TYPE_DISPLAY,
-	icon: PanoramaOutlined,
-	contentMenu: [
-		{
-			name: "General",
-			children: [
-				{
-					description: "Image Source",
-					render: ({ id }) => (
-						<InputSettings id={id} label="Image URL" path="src" />
-					),
-				},
-				{
-					description: "Description",
-					render: ({ id }) => (
-						<InputSettings
-							id={id}
-							label="Description"
-							path="title"
-						/>
-					),
-				},
-			],
-		},
-		{
-			name: "Conditional",
-			children: [...buildShowField()],
-		},
-		{
-			name: "Pre Process",
-			children: [...buildListener("preProcess")],
-		},
-	],
-	styleMenu: [
-		{
-			name: "Layout",
-			children: [
-				{
-					description: "Ratio",
-					render: ({ id }) => (
-						<ButtonGroupSettings
-							id={id}
-							path="style.backgroundSize"
-							label="Ratio"
-							options={[
-								{
-									value: "100% 100%",
-									icon: FitScreen,
-									title: "fit",
-									isDefault: false,
-								},
-								{
-									value: "cover",
-									icon: AspectRatio,
-									title: "cover",
-									isDefault: false,
-								},
-								{
-									value: "contain",
-									icon: ImageAspectRatio,
-									title: "contain",
-									isDefault: true,
-								},
-							]}
-						/>
-					),
-				},
-				{
-					description: "Position",
-					render: ({ id }) => (
-						<SelectInputSettings
-							id={id}
-							path="style.backgroundPosition"
-							label="Position"
-							allowUnset
-							allowCustomInput
-							options={[
-								{
-									value: "top left",
-									display: "Top left",
-								},
-								{
-									value: "top center",
-									display: "Top Center",
-								},
-								{
-									value: "top right",
-									display: "Top Right",
-								},
-								{
-									value: "center left",
-									display: "Center Left",
-								},
-								{
-									value: "center center",
-									display: "Center",
-								},
-								{
-									value: "center right",
-									display: "Center Right",
-								},
-								{
-									value: "bottom left",
-									display: "Bottom Left",
-								},
-								{
-									value: "bottom center",
-									display: "Bottom Center",
-								},
-								{
-									value: "bottom right",
-									display: "Bottom Right",
-								},
-							]}
-						/>
-					),
-				},
-			],
-		},
-		buildDimensionsSection(),
-	],
+    type: BLOCK_TYPE_DISPLAY,
+    icon: PanoramaOutlined,
+    contentMenu: [
+        {
+            name: 'General',
+            children: [
+                {
+                    description: '',
+                    render: ({ id }) => <GeneralSettings id={id} />,
+                },
+            ],
+        },
+        {
+            name: 'Conditional',
+            children: [...buildShowField()],
+        },
+        {
+            name: 'Pre Process',
+            children: [...buildListener('preProcess')],
+        },
+    ],
+    styleMenu: [
+        {
+            name: 'Layout',
+            children: [
+                {
+                    description: 'Ratio',
+                    render: ({ id }) => (
+                        <ButtonGroupSettings
+                            id={id}
+                            path="style.backgroundSize"
+                            label="Ratio"
+                            options={[
+                                {
+                                    value: '100% 100%',
+                                    icon: FitScreen,
+                                    title: 'fit',
+                                    isDefault: false,
+                                },
+                                {
+                                    value: 'cover',
+                                    icon: AspectRatio,
+                                    title: 'cover',
+                                    isDefault: false,
+                                },
+                                {
+                                    value: 'contain',
+                                    icon: ImageAspectRatio,
+                                    title: 'contain',
+                                    isDefault: true,
+                                },
+                            ]}
+                        />
+                    ),
+                },
+                {
+                    description: 'Position',
+                    render: ({ id }) => (
+                        <SelectInputSettings
+                            id={id}
+                            path="style.backgroundPosition"
+                            label="Position"
+                            allowUnset
+                            allowCustomInput
+                            options={[
+                                {
+                                    value: 'top left',
+                                    display: 'Top left',
+                                },
+                                {
+                                    value: 'top center',
+                                    display: 'Top Center',
+                                },
+                                {
+                                    value: 'top right',
+                                    display: 'Top Right',
+                                },
+                                {
+                                    value: 'center left',
+                                    display: 'Center Left',
+                                },
+                                {
+                                    value: 'center center',
+                                    display: 'Center',
+                                },
+                                {
+                                    value: 'center right',
+                                    display: 'Center Right',
+                                },
+                                {
+                                    value: 'bottom left',
+                                    display: 'Bottom Left',
+                                },
+                                {
+                                    value: 'bottom center',
+                                    display: 'Bottom Center',
+                                },
+                                {
+                                    value: 'bottom right',
+                                    display: 'Bottom Right',
+                                },
+                            ]}
+                        />
+                    ),
+                },
+            ],
+        },
+        buildDimensionsSection(),
+    ],
 };
