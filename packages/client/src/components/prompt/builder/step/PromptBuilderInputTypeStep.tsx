e2a889941a5ed26646c443d216833b44391388ae
aditiiisinhaa <130389159+aditiiisinhaa@users.noreply.github.com>
2025-09-17 20:17:28 +0530

    feat(client): agent builder user dropdown list

--- packages/client/src/components/prompt/builder/step/PromptBuilderInputTypeStep.tsx
+++ packages/client/src/components/prompt/builder/step/PromptBuilderInputTypeStep.tsx
@@ -117,7 +117,7 @@ export const PromptBuilderInputTypeStep = (props: {
 	const setInputType = (
 		inputTokenIndex: number,
 		inputType: string,
-		inputTypeMeta: string | null,
+		inputTypeMeta: any,
 	) => {
 		const inputTypesDup = {
 			...inputTypes,
