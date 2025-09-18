9c2bd081a7bf1189deb9f2ed23d27c586f6dc54e
SuryaNarayanan-Kgs <suryagang97@gmail.com>
2025-09-18 14:38:08 +0530

    feat(client): added vector pagecontet page

--- packages/client/src/pages/import/ImportPage.tsx
+++ packages/client/src/pages/import/ImportPage.tsx
@@ -3,35 +3,38 @@ import { DatabasePageContent } from "../CatalogueEngines/DatabaseEngine/Database
 import { FunctionPageContent } from "../CatalogueEngines/FunctionEngine/FunctionPageContent";
 import { ModelPageContent } from "../CatalogueEngines/ModelEngine/ModelPageContent";
 import { StoragePageContent } from "../CatalogueEngines/StorageEngine/StoragePageContent";
+import { VectorPageContent } from "../CatalogueEngines/VectorEngine/vectorPageContent";
 import { ImportLayout } from "./ImportLayout";
 import { ImportPageContent } from "./ImportPageContent";
 
 /** TODO: Refactor */
 interface ImportPageProps {
-  /**
-   * Name of the section
-   */
-  name: string;
+	/**
+	 * Name of the section
+	 */
+	name: string;
 
-  /**
-   * What engine are you importing
-   */
-  type: ENGINE_TYPES;
+	/**
+	 * What engine are you importing
+	 */
+	type: ENGINE_TYPES;
 }
 export const ImportPage: React.FC<ImportPageProps> = ({ name, type }) => {
-  return (
-    <ImportLayout>
-      { type === "MODEL" ? (
-        <ModelPageContent name={name} />
-      ) : type === "DATABASE" ? (
-        <DatabasePageContent name={name} />
-      ) : type === "STORAGE" ? (
-        <StoragePageContent name={name} />
-      ) : type === "FUNCTION" ? (
-        <FunctionPageContent name={name} />
-      ) : (
-        <ImportPageContent name={name} type={type} />
-      )}
-    </ImportLayout>
-  );
+	return (
+		<ImportLayout>
+			{type === "VECTOR" ? (
+				<VectorPageContent name={name} />
+			) : type === "MODEL" ? (
+				<ModelPageContent name={name} />
+			) : type === "DATABASE" ? (
+				<DatabasePageContent name={name} />
+			) : type === "STORAGE" ? (
+				<StoragePageContent name={name} />
+			) : type === "FUNCTION" ? (
+				<FunctionPageContent name={name} />
+			) : (
+				<ImportPageContent name={name} type={type} />
+			)}
+		</ImportLayout>
+	);
 };
