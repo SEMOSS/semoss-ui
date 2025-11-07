b782f38077c3a3fbe184da33afa2c549654e26f6
Maher Khalil <themaherkhalil@gmail.com>
2025-11-07 08:25:01 -0500

    feat(client): changing log timestamp location

--- packages/client/src/components/logs/AuditLogsDetailDrawer.tsx
+++ packages/client/src/components/logs/AuditLogsDetailDrawer.tsx
@@ -520,21 +520,6 @@ export const AuditLogsDetailDrawer = (props) => {
 						</SummaryItem>
 						<SummaryItem>
 							<SummaryLabel variant="caption">
-								Log TimeStamp
-							</SummaryLabel>
-							<SummaryValue variant="body2">
-								{
-									TimeDateFormatter(logDetails.logTimestamp)
-										.time
-								}{" "}
-								{
-									TimeDateFormatter(logDetails.logTimestamp)
-										.date
-								}
-							</SummaryValue>
-						</SummaryItem>
-						<SummaryItem>
-							<SummaryLabel variant="caption">
 								User Id
 							</SummaryLabel>
 							<SummaryValue variant="body2">
@@ -547,6 +532,21 @@ export const AuditLogsDetailDrawer = (props) => {
 							</SummaryLabel>
 							<SummaryValue variant="body2">
 								{logDetails.sessionId}
+							</SummaryValue>
+						</SummaryItem>
+						<SummaryItem>
+							<SummaryLabel variant="caption">
+								Log Timestamp
+							</SummaryLabel>
+							<SummaryValue variant="body2">
+								{
+									TimeDateFormatter(logDetails.logTimestamp)
+										.time
+								}{" "}
+								{
+									TimeDateFormatter(logDetails.logTimestamp)
+										.date
+								}
 							</SummaryValue>
 						</SummaryItem>
 					</SummaryGrid>
