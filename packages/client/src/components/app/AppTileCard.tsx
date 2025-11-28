008bf5ae884acefe2c6dcc7ae2b866049fc4ed89
Kirthikakumar-K <kirthika.kumar@kanini.com>
2025-11-28 18:56:32 +0530

    bug(client): cosmetic fix

--- packages/client/src/components/app/AppTileCard.tsx
+++ packages/client/src/components/app/AppTileCard.tsx
@@ -62,18 +62,6 @@ const StyledOverlayContent = styled("div")(({ theme }) => ({
 	paddingRight: theme.spacing(2),
 }));
 
-// const StyledTileCardMedia = styled(Card.Media)(({ theme }) => ({
-// 	display: "flex",
-// 	alignItems: "flex-start",
-// 	gap: theme.spacing(1.25),
-// 	alignSelf: "stretch",
-// 	overflowClipMargin: "content-box",
-// 	overflow: "clip",
-// 	objectFit: "cover",
-// 	width: "100%",
-// 	height: "77px",
-// }));
-
 const StyledPublishedByContainer = styled("div")(({ theme }) => ({
 	display: "flex",
 	justifyContent: "flex-start",
