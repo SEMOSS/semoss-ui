2810e6bbfbc8be4b8569581492cbf1606e54ead5
Van Buren <tevanburen@deloitte.com>
2025-12-10 16:50:14 -0500

    feat: add AI title

--- packages/playground/src/components/message/error-message.tsx
+++ packages/playground/src/components/message/error-message.tsx
@@ -1,4 +1,4 @@
-import { CircleAlert } from "lucide-react";
+import { CircleAlert, MessageCircleIcon } from "lucide-react";
 import { Alert, AlertTitle, Button } from "@semoss/ui/next";
 
 export interface ErrorMessageProps {
@@ -12,27 +12,33 @@ export interface ErrorMessageProps {
  */
 export const ErrorMessage = ({ onTryAgain }: ErrorMessageProps) => {
 	return (
-		<Alert
-			variant="destructive"
-			className="flex items-center justify-between"
-		>
-			<div className="flex items-center gap-2">
-				<CircleAlert className="aspect-square size-4" />
-				<AlertTitle>
-					Unable to process request. Please check your connection,
-					copy your message, and refresh.
-				</AlertTitle>
+		<div className="group mb-0 flex w-full flex-col gap-4 overflow-hidden">
+			<div className="group flex flex-row items-center gap-2">
+				<MessageCircleIcon className="size-4" />
+				<span className="mr-0.5 font-medium text-base">AI</span>
 			</div>
-			{onTryAgain && (
-				<Button
-					onClick={onTryAgain}
-					size="sm"
-					variant="destructive"
-					className="flex h-6 items-center justify-center gap-2 rounded-md bg-custom-background-dark-input-30! px-3 py-2 text-base-foreground! hover:bg-custom-background-dark-input-30! hover:text-base-foreground!"
-				>
-					Try again
-				</Button>
-			)}
-		</Alert>
+			<Alert
+				variant="destructive"
+				className="flex items-center justify-between"
+			>
+				<div className="flex items-center gap-2">
+					<CircleAlert className="aspect-square size-4" />
+					<AlertTitle>
+						Unable to process request. Please check your connection,
+						copy your message, and refresh.
+					</AlertTitle>
+				</div>
+				{onTryAgain && (
+					<Button
+						onClick={onTryAgain}
+						size="sm"
+						variant="destructive"
+						className="flex h-6 items-center justify-center gap-2 rounded-md bg-custom-background-dark-input-30! px-3 py-2 text-base-foreground! hover:bg-custom-background-dark-input-30! hover:text-base-foreground!"
+					>
+						Try again
+					</Button>
+				)}
+			</Alert>
+		</div>
 	);
 };
