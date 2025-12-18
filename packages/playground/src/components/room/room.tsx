73351fee4e835df2452ada1b3cc9e0c08382fe18
Van Buren <tevanburen@deloitte.com>
2025-12-18 09:43:19 -0500

    fix: disable when error

--- packages/playground/src/components/room/room.tsx
+++ packages/playground/src/components/room/room.tsx
@@ -137,7 +137,7 @@ export const Room: React.FC<RoomProps> = observer(({ room }) => {
 		};
 	}, [room]);
 
-	// console.log(room.error);
+	const isDisabled = Boolean(room.error) || room.mode === "executing";
 
 	return (
 		<div className="flex h-full w-full flex-col bg-secondary-background transition-all duration-200 ease-in-out">
@@ -217,7 +217,7 @@ export const Room: React.FC<RoomProps> = observer(({ room }) => {
 			<div className="mx-auto w-full max-w-4xl shrink-0 p-4">
 				<RoomInput
 					isLoading={room.isLoading}
-					isDisabled={room.mode === "executing"}
+					isDisabled={isDisabled}
 					minRows={3}
 					maxRows={8}
 					configuration={<RoomConfigurationButton room={room} />}
