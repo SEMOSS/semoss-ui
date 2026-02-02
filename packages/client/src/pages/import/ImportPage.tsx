7efab3239fd1491e65be439e7c4b9485b9c8b808
Naveen Ramasamy <naveen.ramasamy@kanini.com>
2026-02-02 20:02:52 +0530

    feat(merge dev): merge dev

--- packages/client/src/pages/import/ImportPage.tsx
+++ packages/client/src/pages/import/ImportPage.tsx
@@ -1,9 +1,7 @@
 import { useMemo } from "react";
 import { DatabasePageContent } from "@/components/import/database/DatabasePageContent";
 import { FunctionImport } from "@/components/import/function/FunctionImport";
-import {
-	GuardrailImport,
-} from "@/components/import/guardrail/GuardrailImport";
+import { GuardrailImport } from "@/components/import/guardrail/GuardrailImport";
 import { StorageImport } from "@/components/import/storage/StorageImport";
 import { VectorImport } from "@/components/import/vector/VectorImport";
 import type { ENGINE_TYPES } from "@/types";
