b8cdecc07fb2ab6014a562ba53110c5c9e35b148
Kirthika K <154126558+KirthikaKumar-K@users.noreply.github.com>
2026-02-04 15:15:42 +0530

    Reorder import statements in MembersAddOverlayUser

--- packages/client/src/components/settings/MembersAddOverlayUser.tsx
+++ packages/client/src/components/settings/MembersAddOverlayUser.tsx
@@ -1,5 +1,5 @@
-import { Avatar, Stack, styled, Typography } from "@semoss/ui";
 import { extractInitials } from "@/utility/general";
+import { Avatar, Stack, styled, Typography } from "@semoss/ui";
 
 const StyledUser = styled(Stack)(({ theme }) => ({
 	paddingTop: theme.spacing(1),
