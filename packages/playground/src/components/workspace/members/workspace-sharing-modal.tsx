564fc3b076669c9faca092cdb0448bba3c5b9d70
Thomas Van Buren <tevanburen@deloitte.com>
2026-02-19 14:12:22 -0500

    Fix: searching for users (#2725)
    * fix: search

--- packages/playground/src/components/workspace/members/workspace-sharing-modal.tsx
+++ packages/playground/src/components/workspace/members/workspace-sharing-modal.tsx
@@ -286,7 +286,7 @@ export const WorkspaceSharingModal = ({
 
 													return (
 														<CommandItem
-															key={user.id}
+															key={`${user.id}-${user.email}`} // Use both ID and email for uniqueness
 															value={user.id}
 															onSelect={() =>
 																handleAddUser(
@@ -360,7 +360,7 @@ export const WorkspaceSharingModal = ({
 								<div>
 									{Object.values(pendingUsers).map((user) => (
 										<WorkspaceMemberRow
-											key={user.id}
+											key={`${user.id}-${user.email}`} // Use both ID and email for uniqueness
 											member={user}
 											currentUserId=""
 											activeUserPermission={
