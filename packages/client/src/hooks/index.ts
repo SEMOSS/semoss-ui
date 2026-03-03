a1fe337cff181b9ab9472a7051459ddaaeadbb02
Maher Khalil <themaherkhalil@gmail.com>
2026-03-03 18:56:43 -0500

    feat(client): pagination hook for tables, css fixes

--- packages/client/src/hooks/index.ts
+++ packages/client/src/hooks/index.ts
@@ -1,37 +1,39 @@
-import { useAPI } from './useAPI';
-import { useCacheState } from './useCacheState';
-import { useEngine } from './useEngine';
-import { useLLM } from './useLLM';
-import { useMetamodel } from './useMetamodel';
-import { useRootStore } from './useRootStore';
-import { useSettings } from './useSettings';
-import { usePixel } from './usePixel';
-import { useDesigner } from './useDesigner';
-import { useStepper } from './useStepper';
-import { useWorkspace } from './useWorkspace';
-import { usePage } from './usePage';
-import { useDatabaseStructure } from './useDatabaseStructure';
-import { useQueryExecution } from './useDatabaseQueryExecution';
-import { useQueryEditor } from './useDatabaseQueryEditor';
-import { useQueryResults } from './useDatabaseQueryResults';
-import { useBlockSettings } from './useBlockSettings';
+import { useAPI } from "./useAPI";
+import { useBlockSettings } from "./useBlockSettings";
+import { useCacheState } from "./useCacheState";
+import { useQueryEditor } from "./useDatabaseQueryEditor";
+import { useQueryExecution } from "./useDatabaseQueryExecution";
+import { useQueryResults } from "./useDatabaseQueryResults";
+import { useDatabaseStructure } from "./useDatabaseStructure";
+import { useDesigner } from "./useDesigner";
+import { useEngine } from "./useEngine";
+import { useLLM } from "./useLLM";
+import { useMetamodel } from "./useMetamodel";
+import { usePage } from "./usePage";
+import { usePixel } from "./usePixel";
+import { useRootStore } from "./useRootStore";
+import { useServerPagination } from "./useServerPagination";
+import { useSettings } from "./useSettings";
+import { useStepper } from "./useStepper";
+import { useWorkspace } from "./useWorkspace";
 
 export {
-    useAPI,
-    useBlockSettings,
-    useCacheState,
-    useEngine,
-    useLLM,
-    useMetamodel,
-    useRootStore,
-    useSettings,
-    usePixel,
-    useDesigner,
-    useStepper,
-    useWorkspace,
-    usePage,
-    useDatabaseStructure,
-    useQueryExecution,
-    useQueryEditor,
-    useQueryResults,
+	useAPI,
+	useBlockSettings,
+	useCacheState,
+	useDatabaseStructure,
+	useDesigner,
+	useEngine,
+	useLLM,
+	useMetamodel,
+	usePage,
+	usePixel,
+	useQueryEditor,
+	useQueryExecution,
+	useQueryResults,
+	useRootStore,
+	useServerPagination,
+	useSettings,
+	useStepper,
+	useWorkspace,
 };
