0d5910557ac6ec7450205c3ae2c4653f0071cd10
Srujan Keskar <srujankeskar15496@gmail.com>
2026-03-04 16:16:42 +0530

    updating materia ui to semoss/ui

--- packages/client/src/components/prompt/builder/step/PromptBuilderInputTypeStep.tsx
+++ packages/client/src/components/prompt/builder/step/PromptBuilderInputTypeStep.tsx
@@ -1,13 +1,11 @@
-import {
-	Dialog,
-	DialogActions,
-	DialogContent,
-	DialogTitle,
-} from "@mui/material";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import {
 	Box,
 	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogTitle,
 	Grid,
 	Paper,
 	Stack,
@@ -15,7 +13,11 @@ import {
 	Typography,
 } from "@semoss/ui";
 import { usePixel, useRootStore } from "@/hooks";
-import { INPUT_TYPE_TEXT, TOKEN_TYPE_INPUT, TOKEN_TYPE_TEXT } from "../../prompt.constants";
+import {
+	INPUT_TYPE_TEXT,
+	TOKEN_TYPE_INPUT,
+	TOKEN_TYPE_TEXT,
+} from "../../prompt.constants";
 import type { Builder, Token } from "../../prompt.types";
 import { PromptBuilderInputTypeSelection } from "./PromptBuilderInputTypeSelection";
 
@@ -35,25 +37,25 @@ export const StyledStack = styled(Stack)(({ theme }) => ({
 export const PromptBuilderInputTypeStep = (props: {
 	builder: Builder;
 	setBuilderValue: (builderStepKey: string, value: object | string) => void;
-    modelId: string;
-    setLLMResponse: (response: string) => void;
-    setLLMTokens: (tokens: Token[]) => void;
-    isLLMVersionSelected: boolean;
-    setIsLLMVersionSelected: (value: boolean) => void;
+	modelId: string;
+	setLLMResponse: (response: string) => void;
+	setLLMTokens: (tokens: Token[]) => void;
+	isLLMVersionSelected: boolean;
+	setIsLLMVersionSelected: (value: boolean) => void;
 }) => {
 	const builderInputTypes = props.builder.inputTypes.value;
 
 	const { monolithStore } = useRootStore();
-    const [inputTokens, setInputTokens] = useState<Token[]>([]);
+	const [inputTokens, setInputTokens] = useState<Token[]>([]);
 	const [inputTypes, setInputTypes] = useState({});
 
-    const originalPrompt = props.builder.context.value as string;
-    const SelectedKeys = inputTokens.map((token) => token.key);
-    const [isInitialRender, setIsInitialRender] = useState(true); // State to track the initial render
-    const [isPopupOpen, setIsPopupOpen] = useState(false); // State to track the popup open/close
-    const [promptWithInputs, setPromptWithInputs] = useState(''); // State to hold prompt with inputs
-    const [LllmPromptResponse, setLllmPromptResponse] = useState<string>(''); // State to hold LLM response
-    const [isLLMVersionLoading, setIsLLMVersionLoading] = useState(true); // State to track LLM version loading for button
+	const originalPrompt = props.builder.context.value as string;
+	const SelectedKeys = inputTokens.map((token) => token.key);
+	const [isInitialRender, setIsInitialRender] = useState(true); // State to track the initial render
+	const [isPopupOpen, setIsPopupOpen] = useState(false); // State to track the popup open/close
+	const [promptWithInputs, setPromptWithInputs] = useState(""); // State to hold prompt with inputs
+	const [LllmPromptResponse, setLllmPromptResponse] = useState<string>(""); // State to hold LLM response
+	const [isLLMVersionLoading, setIsLLMVersionLoading] = useState(true); // State to track LLM version loading for button
 
 	const [cfgLibraryVectorDbs, setCfgLibraryVectorDbs] = useState({
 		loading: false,
@@ -146,7 +148,7 @@ export const PromptBuilderInputTypeStep = (props: {
 		const inputTypesDup = {
 			...inputTypes,
 			[inputTokenIndex]: {
-                ...inputTypes[inputTokenIndex],
+				...inputTypes[inputTokenIndex],
 				type: inputType,
 				meta: inputTypeMeta,
 			},
@@ -156,98 +158,101 @@ export const PromptBuilderInputTypeStep = (props: {
 		props.setBuilderValue("inputTypes", inputTypesDup);
 	};
 
-    const setInputLabel = (inputTokenIndex: number, inputLabel: string) => {
-        setInputTypes((prevInputTypes) => {
-            const inputTypesDup = {
-                ...prevInputTypes,
-                [inputTokenIndex]: {
-                    ...prevInputTypes[inputTokenIndex],
-                    label: inputLabel,
-                },
-            };
-            props.setBuilderValue('inputTypes', inputTypesDup);
-            return inputTypesDup;
-        });
-    };
+	const setInputLabel = (inputTokenIndex: number, inputLabel: string) => {
+		setInputTypes((prevInputTypes) => {
+			const inputTypesDup = {
+				...prevInputTypes,
+				[inputTokenIndex]: {
+					...prevInputTypes[inputTokenIndex],
+					label: inputLabel,
+				},
+			};
+			props.setBuilderValue("inputTypes", inputTypesDup);
+			return inputTypesDup;
+		});
+	};
 
-    const generalizedPrompt = `Rephrase the following input for better clarity and readability while maintaining 
+	const generalizedPrompt = `Rephrase the following input for better clarity and readability while maintaining 
     its original intent. Replace each word inside {} with a broad and appropriate category label that represents 
     its meaning. Use sequential numbering only when multiple items belong to the same broad category. 
     Ensure the transformation enhances the prompt but does not introduce, remove, or alter details beyond 
     necessary refinements. Provide only the improved version without extra context or explanations.`;
 
-    // Call the LLM API to get the LLM version of the prompt
-    const promptGenerationLLMCall = async () => {
-        let prompt = originalPrompt;
-        const inputTokenValue = SelectedKeys;
-        inputTokenValue.forEach((token) => {
-            const regex = new RegExp(`\\b${token}\\b`, 'g');
-            prompt = prompt.replace(regex, `{${token}}`);
-        });
-        setPromptWithInputs(prompt);
-        setIsLLMVersionLoading(true);
-        const LLMresponse = await monolithStore.runQuery(
-            `LLM(engine="${props.builder.model.value}", command=["<encode>${generalizedPrompt}\n\n User Input: ${prompt}</encode>"])`,
-        );
-        setIsLLMVersionLoading(false);
-        setLllmPromptResponse(LLMresponse.pixelReturn[0].output.response);
-    };
+	// Call the LLM API to get the LLM version of the prompt
+	const promptGenerationLLMCall = async () => {
+		let prompt = originalPrompt;
+		const inputTokenValue = SelectedKeys;
+		inputTokenValue.forEach((token) => {
+			const regex = new RegExp(`\\b${token}\\b`, "g");
+			prompt = prompt.replace(regex, `{${token}}`);
+		});
+		setPromptWithInputs(prompt);
+		setIsLLMVersionLoading(true);
+		const LLMresponse = await monolithStore.runQuery(
+			`LLM(engine="${props.builder.model.value}", command=["<encode>${generalizedPrompt}\n\n User Input: ${prompt}</encode>"])`,
+		);
+		setIsLLMVersionLoading(false);
+		setLllmPromptResponse(LLMresponse.pixelReturn[0].output.response);
+	};
 
-    // Call the promptGenerationLLMCall function only on the initial render
-    useEffect(() => {
-        if (isInitialRender && inputTokens.length > 0) {
-            promptGenerationLLMCall();
-            setIsPopupOpen(true);
-            setIsInitialRender(false); // Setting to false after the initial render
-        }
-    }, [isInitialRender, inputTokens]);
+	// Call the promptGenerationLLMCall function only on the initial render
+	useEffect(() => {
+		if (isInitialRender && inputTokens.length > 0) {
+			promptGenerationLLMCall();
+			setIsPopupOpen(true);
+			setIsInitialRender(false); // Setting to false after the initial render
+		}
+	}, [isInitialRender, inputTokens]);
 
-    const extractCategorisedLabels = (text: string): string[] => {
-        const regex = /\{([^}]+)\}/g;
-        const matches = [];
-        let match;
-        // Executes a search on a string using a regular expression pattern, and returns an array containing the results of that search.
-        while ((match = regex.exec(text)) !== null) {
-            matches.push(match[1]);
-        }
-        return matches;
-    };
+	const extractCategorisedLabels = (text: string): string[] => {
+		const regex = /\{([^}]+)\}/g;
+		const matches = [];
+		let match;
+		// Executes a search on a string using a regular expression pattern, and returns an array containing the results of that search.
+		while ((match = regex.exec(text)) !== null) {
+			matches.push(match[1]);
+		}
+		return matches;
+	};
 
-    const handleLLMResponse = useCallback((LLMResponse: string) => {
-        const tokens = LLMResponse.split(/(\s+|\{[^}]+\})/g)
-            .filter(Boolean)
-            .map((part, index) => {
-                const isInputToken = part.startsWith('{') && part.endsWith('}');
-                const key = isInputToken ? part.slice(1, -1) : part;
-                return {
-                    index: index,
-                    key: key,
-                    display: key,
-                    type: isInputToken ? TOKEN_TYPE_INPUT : TOKEN_TYPE_TEXT,
-                    isHiddenPhraseInputToken: false,
-                    linkedInputToken: undefined,
-                };
-            });
-        props.setLLMResponse(LLMResponse);
-        props.setLLMTokens(tokens);
-    }, [props.setLLMResponse, props.setLLMTokens]);
+	const handleLLMResponse = useCallback(
+		(LLMResponse: string) => {
+			const tokens = LLMResponse.split(/(\s+|\{[^}]+\})/g)
+				.filter(Boolean)
+				.map((part, index) => {
+					const isInputToken =
+						part.startsWith("{") && part.endsWith("}");
+					const key = isInputToken ? part.slice(1, -1) : part;
+					return {
+						index: index,
+						key: key,
+						display: key,
+						type: isInputToken ? TOKEN_TYPE_INPUT : TOKEN_TYPE_TEXT,
+						isHiddenPhraseInputToken: false,
+						linkedInputToken: undefined,
+					};
+				});
+			props.setLLMResponse(LLMResponse);
+			props.setLLMTokens(tokens);
+		},
+		[props.setLLMResponse, props.setLLMTokens],
+	);
 
-    // Call handleLLMResponse with the LLM response when the LLM version is selected to set the builder values
-    useEffect(() => {
-        if (props.isLLMVersionSelected) {
-            handleLLMResponse(LllmPromptResponse);
-        }
-    }, [LllmPromptResponse, props.isLLMVersionSelected, handleLLMResponse]);
+	// Call handleLLMResponse with the LLM response when the LLM version is selected to set the builder values
+	useEffect(() => {
+		if (props.isLLMVersionSelected) {
+			handleLLMResponse(LllmPromptResponse);
+		}
+	}, [LllmPromptResponse, props.isLLMVersionSelected, handleLLMResponse]);
 
-    // Extract the words in braces from the LLM response
-    const wordsInBraces = extractCategorisedLabels(LllmPromptResponse);
+	// Extract the words in braces from the LLM response
+	const wordsInBraces = extractCategorisedLabels(LllmPromptResponse);
 
-    // Function to handle the LLM version
-    const handleLLMVersionSelection = () => {
-        props.setIsLLMVersionSelected(true);
-        setIsPopupOpen(false);
-    };
-
+	// Function to handle the LLM version
+	const handleLLMVersionSelection = () => {
+		props.setIsLLMVersionSelected(true);
+		setIsPopupOpen(false);
+	};
 
 	return (
 		<StyledStepPaper elevation={2} square>
@@ -258,52 +263,52 @@ export const PromptBuilderInputTypeStep = (props: {
 					inputs.
 				</Typography>
 			</StyledBox>
-            <Dialog
-                open={isPopupOpen}
-                onClose={() => setIsPopupOpen(false)}
-                maxWidth="md"
-                fullWidth
-            >
-                <DialogTitle id="alert-dialog-title">
-                    Prompt Selection
-                </DialogTitle>
-                <DialogContent>
-                    <Grid container spacing={2}>
-                        <Grid item xs={6}>
-                            <Typography variant="h6">User Prompt</Typography>
-                            <Typography variant="body2">
-                                {promptWithInputs}
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Typography variant="h6">
-                                LLM Suggested Prompt
-                            </Typography>
-                            <Typography variant="body2">
-                                {LllmPromptResponse}
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                </DialogContent>
-                <DialogActions>
-                    <Button
-                        onClick={() => {
-                            props.setIsLLMVersionSelected(false);
-                            setIsPopupOpen(false);
-                        }}
-                        color="primary"
-                    >
-                        User Version
-                    </Button>
-                    <Button
-                        disabled={isLLMVersionLoading}
-                        onClick={handleLLMVersionSelection}
-                        color="primary"
-                    >
-                        LLM Version
-                    </Button>
-                </DialogActions>
-            </Dialog>
+			<Dialog
+				open={isPopupOpen}
+				onClose={() => setIsPopupOpen(false)}
+				maxWidth="md"
+				fullWidth
+			>
+				<DialogTitle id="alert-dialog-title">
+					Prompt Selection
+				</DialogTitle>
+				<DialogContent>
+					<Grid container spacing={2}>
+						<Grid item xs={6}>
+							<Typography variant="h6">User Prompt</Typography>
+							<Typography variant="body2">
+								{promptWithInputs}
+							</Typography>
+						</Grid>
+						<Grid item xs={6}>
+							<Typography variant="h6">
+								LLM Suggested Prompt
+							</Typography>
+							<Typography variant="body2">
+								{LllmPromptResponse}
+							</Typography>
+						</Grid>
+					</Grid>
+				</DialogContent>
+				<DialogActions>
+					<Button
+						onClick={() => {
+							props.setIsLLMVersionSelected(false);
+							setIsPopupOpen(false);
+						}}
+						color="primary"
+					>
+						User Version
+					</Button>
+					<Button
+						disabled={isLLMVersionLoading}
+						onClick={handleLLMVersionSelection}
+						color="primary"
+					>
+						LLM Version
+					</Button>
+				</DialogActions>
+			</Dialog>
 			<StyledStack spacing={3}>
 				{Array.from(inputTokens, (inputToken: Token, index) => (
 					<PromptBuilderInputTypeSelection
@@ -315,18 +320,18 @@ export const PromptBuilderInputTypeStep = (props: {
 						cfgLibraryVectorDbs={cfgLibraryVectorDbs}
 						cfgLibraryDatabases={cfgLibraryDatabases}
 						setInputType={setInputType}
-                        setInputLabel={setInputLabel}
-                        userPrompt={props.builder.context.value as string}
-                        modelId={props.builder.model.value as string}
-                        setBuilderValue={(key, value) =>
-                            props.setBuilderValue(key, value as string)
-                        } // Cast value to string
-                        LlmInputLabel={
-                            props.isLLMVersionSelected
-                                ? wordsInBraces[index]
-                                : ''
-                        } // Pass the label to the child
-                        isLLMVersionSelected={props.isLLMVersionSelected}
+						setInputLabel={setInputLabel}
+						userPrompt={props.builder.context.value as string}
+						modelId={props.builder.model.value as string}
+						setBuilderValue={(key, value) =>
+							props.setBuilderValue(key, value as string)
+						} // Cast value to string
+						LlmInputLabel={
+							props.isLLMVersionSelected
+								? wordsInBraces[index]
+								: ""
+						} // Pass the label to the child
+						isLLMVersionSelected={props.isLLMVersionSelected}
 					/>
 				))}
 			</StyledStack>
