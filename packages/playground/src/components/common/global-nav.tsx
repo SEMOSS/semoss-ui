c48722f36a57ed879c77488b01ea2dcc59cbe949
Sunny <samgill3@deloitte.com>
2026-03-20 16:00:51 -0400

    Corrected local time for sidebar chat history, fixed the category in which day/week/month the chat should fall under

--- packages/playground/src/components/common/global-nav.tsx
+++ packages/playground/src/components/common/global-nav.tsx
@@ -67,7 +67,9 @@ export const GlobalNav = observer(() => {
 		t("buckets.favorites"),
 		t("buckets.today"),
 		t("buckets.yesterday"),
+		t("buckets.fewDaysAgo"),
 		t("buckets.lastWeek"),
+		t("buckets.thisMonth"),
 		t("buckets.lastMonth"),
 		t("buckets.older"),
 	] as const;
@@ -203,7 +205,7 @@ export const GlobalNav = observer(() => {
 	 */
 	const bucketedRooms = getRooms.data.reduce(
 		(acc, val) => {
-			const d = dayjs(val.DATE_CREATED);
+			const d = dayjs(val.DATE_CREATED + "Z");
 
 			// Pinned rooms only go in Favorites bucket
 			if (val.PINNED) {
@@ -216,9 +218,13 @@ export const GlobalNav = observer(() => {
 				acc[t("buckets.today")].push(val);
 			} else if (systemDate.subtract(1, "day").isSame(d, "day")) {
 				acc[t("buckets.yesterday")].push(val);
-			} else if (systemDate.isSame(d, "week")) {
+			} else if (d.isAfter(systemDate.subtract(3, "day"))) {
+				acc[t("buckets.fewDaysAgo")].push(val);
+			} else if (d.isAfter(systemDate.subtract(7, "day"))) {
 				acc[t("buckets.lastWeek")].push(val);
 			} else if (systemDate.isSame(d, "month")) {
+				acc[t("buckets.thisMonth")].push(val);
+			} else if (systemDate.subtract(1, "month").isSame(d, "month")) {
 				acc[t("buckets.lastMonth")].push(val);
 			} else {
 				acc[t("buckets.older")].push(val);
@@ -230,7 +236,9 @@ export const GlobalNav = observer(() => {
 			[t("buckets.favorites")]: [],
 			[t("buckets.today")]: [],
 			[t("buckets.yesterday")]: [],
+			[t("buckets.fewDaysAgo")]: [],
 			[t("buckets.lastWeek")]: [],
+			[t("buckets.thisMonth")]: [],
 			[t("buckets.lastMonth")]: [],
 			[t("buckets.older")]: [],
 		} as Record<string, typeof getRooms.data>,
@@ -410,7 +418,7 @@ export const GlobalNav = observer(() => {
 											t("messages.untitled");
 										const date = root.theme.sidebar
 											.chatHistoryDate
-											? new Date(room.DATE_CREATED).toLocaleString(undefined, {
+											? new Date(room.DATE_CREATED + 'Z').toLocaleString(undefined, {
 													month: "numeric",
 													day: "numeric",
 													year: "numeric",
