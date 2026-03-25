e09210d7f639f7bebfec2679689d824b82166c4d
Sunny <samgill3@deloitte.com>
2026-03-25 14:06:55 -0400

    Added sidebar open by default logic to use themedb, expandedByDefault needs to be added in the sidebar in themedb and set to true

--- libs/shared/src/types.ts
+++ libs/shared/src/types.ts
@@ -61,6 +61,7 @@ export interface ThemeMap {
 		 * Content to show in the sidebar
 		 */
 		sidebar: {
+			expandedByDefault?: boolean;
 			chatHistoryDate: boolean;
 			headerItems: {
 				name: string;
