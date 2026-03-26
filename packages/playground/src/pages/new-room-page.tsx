f189d4964f5ddb5302f3b6f089ed56e7d6d01d47
Yifan Zhang <Yifan.Zhang@fda.hhs.gov>
2026-03-26 08:22:27 -0500

    fix: add name behind weclome in new room

--- packages/playground/src/pages/new-room-page.tsx
+++ packages/playground/src/pages/new-room-page.tsx
@@ -314,7 +314,7 @@ export const NewRoomPage = observer(() => {
 						) : (
 							<div className="mx-auto flex max-w-xl flex-col items-center gap-3">
 								<div className="text-center font-semibold text-4xl text-foreground leading-normal">
-									{t("room:welcome")}
+									{t("room:welcome")}, {chat.user.name}
 								</div>
 								{root.theme.description ? (
 									<div className="text-center text-muted-foreground text-sm leading-normal">
