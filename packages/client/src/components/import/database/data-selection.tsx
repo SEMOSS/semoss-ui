49952033ac31ccbf2ee2ed8afe155029ca28b253
Thomas Van Buren <tevanburen@deloitte.com>
2026-04-02 11:38:26 -0400

    Tvb lint (#2962)
    * feat(playground): iframe hooks
    
    * feat(playground): configurable iframe
    
    * feat(playground): embed routes to external app with hooks
    
    * fix(playground): remove dead hooks
    
    * local theme overrides backend
    
    * caught up with create-app-iframe-embed-preload
    
    * trying to optimize latency
    
    * change gitignore
    
    * removed package app and related
    
    * fix: restore package.json
    
    * fix: restore gitignore
    
    * fix(playground): lint
    
    * refactor(playground): set room as null in existing effect
    
    * feat(playground): update deep merge for theme
    
    * feat(playground): simplify embed logic
    
    * feat(playground): store embed map in chat store
    
    * feat(playground): set up listener on embed page
    
    * feat(playground): revert
    
    ---------
    
    Co-authored-by: ckelly17 <conor.richard.kelly@gmail.com>
    Co-authored-by: Eugene <94133195+eukim1707@users.noreply.github.com>
    Co-authored-by: Kim <Eugene.Kim1@fda.gov>

--- packages/client/src/components/import/database/data-selection.tsx
+++ packages/client/src/components/import/database/data-selection.tsx
@@ -1,5 +1,5 @@
-/** biome-ignore-all lint/a11y/noStaticElementInteractions: <explanation> */
-/** biome-ignore-all lint/a11y/useKeyWithClickEvents: <explanation> */
+/** biome-ignore-all lint/a11y/noStaticElementInteractions: TODO */
+/** biome-ignore-all lint/a11y/useKeyWithClickEvents: TODO */
 import {
 	ChevronDown,
 	ChevronUp,
