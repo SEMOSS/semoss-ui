7c444d9f16b7b96507a1fe7debbd4c5253a27ca2
Naveen Ramasamy <naveen.ramasamy@kanini.com>
2026-04-07 14:47:58 +0530

    feat(Changelog): changelog

--- packages/client/src/components/shared/LogoutPopover.tsx
+++ packages/client/src/components/shared/LogoutPopover.tsx
@@ -25,7 +25,6 @@ export const LogoutPopover: React.FC<LogoutPopoverProps> = (props) => {
 	const { configStore } = useRootStore();
 	const [loggingOut, setLoggingOut] = useState(false);
 	const [open, setOpen] = useState(false);
-	console.log("LogoutPopover rendered, loggingOut:", loggingOut);
 
 	const handleOpenChange = (next: boolean) => {
 		setOpen(next);
