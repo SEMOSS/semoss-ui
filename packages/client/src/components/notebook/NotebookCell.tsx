4be3fbf210c09a472cbb9677de2a2f170eef63d9
Paulson Robert <paulson.robert@kanini.com>
2026-04-07 11:48:21 +0530

    2487 MCP for engines (#2774)
    * feat(client): made mcp json editor into shared components
    
    * feat(client): include shared source to client
    
    * feat(client): kebab casing filenames
    
    * feat(client): make and edit mcp icon conditional changes
    
    * feat(clinet): ui changes as per UI audit
    
    * feat(client): mcp file refactor
    
    * feat(client): cosmetic changes
    
    * feat(client/shared): mcp generation for engines feature added with revert option
    
    * feat(client):  mcp related changes
    
    * feat(client/shared): optimized shared components and made mcp editor shared comp
    
    * feat(client): using shared component
    
    * feat(client): single mcp component
    
    * feat(client): re-written logic for new flow
    
    * feat(client): MCP chip FE addition
    
    * feat(client): added jsdocs for the functions
    
    * feat(client): removed all revert MCp changes
    
    * feat(client): removed generate mcp btn for gaurdrail catalog
    
    * feat(client): optimized mcp tool file opening
    
    * feat(client): mcp tag addition from FE
    
    * feat(client): navigation path char fix
    
    * feat(client): auto refresh on mcp generation fix
    
    * feat(client): file exp refresh fix
    
    ---------
    
    Co-authored-by: Kirthikakumar-K <kirthika.kumar@kanini.com>
    Co-authored-by: Kirthika K <154126558+KirthikaKumar-K@users.noreply.github.com>

--- packages/client/src/components/notebook/NotebookCell.tsx
+++ packages/client/src/components/notebook/NotebookCell.tsx
@@ -11,9 +11,9 @@ import {
 	Pending,
 	PlayArrowRounded,
 	PlayCircle,
-	SmartToy,
 	SwapHoriz,
 } from "@mui/icons-material";
+import { HammerIcon } from "lucide-react";
 import { observer } from "mobx-react-lite";
 import { createElement, useEffect, useMemo, useRef, useState } from "react";
 import {
@@ -772,7 +772,7 @@ export const NotebookCell = observer(
 											{cell.widget === "mcp-tool" ? (
 												<SwapHoriz />
 											) : (
-												<SmartToy />
+												<HammerIcon size={16}/>
 											)}
 										</StyledButtonLabel>
 									</StyledButtonGroupButton>
