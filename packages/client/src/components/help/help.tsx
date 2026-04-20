238975d4036673564ea641e6f1936dfdd8636e76
Gowrishankar <104352891+Gowrishankar-Palanisamy@users.noreply.github.com>
2026-04-20 23:37:13 +0530

    fix(client): filter out tutorial link on theme (#3035)

--- packages/client/src/components/help/help.tsx
+++ packages/client/src/components/help/help.tsx
@@ -12,7 +12,13 @@ import { useRootStore } from "@/hooks";
 export const Help = observer((): JSX.Element => {
 	const { configStore } = useRootStore();
 
-	if (configStore.theme.helpBannerOrder.length === 0) {
+    let themeMap;
+
+    const parsedThemeMap = JSON.parse(configStore.store.config.theme.THEME_MAP);
+
+    themeMap = typeof parsedThemeMap === "string" ? JSON.parse(parsedThemeMap) : parsedThemeMap;
+
+	if (themeMap.helpBannerOrder.length === 0) {
 		return null;
 	}
 
@@ -30,8 +36,10 @@ export const Help = observer((): JSX.Element => {
 					</Button>
 				</DropdownMenuTrigger>
 				<DropdownMenuContent align="end" side="top" sideOffset={8}>
-					{configStore.theme.helpBannerOrder.map((key) => {
-						const v = configStore.theme.helpBannerValues[key];
+					{themeMap.helpBannerOrder
+                        .filter((key) => key !== "tutorials") // Filter out the "tutorials" value
+                        .map((key) => {
+						const v = themeMap.helpBannerValues[key];
 
 						if (v) {
 							return (
